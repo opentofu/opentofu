846babf1ff9e467702de0a0e475cb059b555a1fb
Kuba Martin <jakub.wit.martin@gmail.com>
2023-08-21 17:01:11 +0200

    Revert "Add support for  `~/.opentf.d` (#22)"
    This reverts commit e650bab8d7b0d8bc1f3f7c67e6710c2eafc64fee.

--- internal/command/cliconfig/config_windows.go
+++ internal/command/cliconfig/config_windows.go
@@ -7,7 +7,6 @@
 package cliconfig
 
 import (
-	"os"
 	"path/filepath"
 	"syscall"
 	"unsafe"
@@ -38,18 +37,10 @@ func configDir() (string, error) {
 		return "", err
 	}
 
-	newConfigDir := filepath.Join(dir, "opentf.d")
-	legacyConfigDir := filepath.Join(dir, "terraform.d")
-
-	return getNewOrLegacyPath(newConfigDir, legacyConfigDir)
+	return filepath.Join(dir, "terraform.d"), nil
 }
 
 func homeDir() (string, error) {
-	// For unit-testing purposes.
-	if home := os.Getenv("OPENTF_TEST_HOME"); home != "" {
-		return home, nil
-	}
-
 	b := make([]uint16, syscall.MAX_PATH)
 
 	// See: http://msdn.microsoft.com/en-us/library/windows/desktop/bb762181(v=vs.85).aspx
