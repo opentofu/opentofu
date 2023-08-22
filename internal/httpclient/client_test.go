3c4bef34b8a3e367a24327809d1fa6c10f2ee142
Yaron Yarimi <yaron.yarimi@env0.com>
2023-08-22 17:29:42 +0300

    Rename user agent header from to include OpenTF

--- internal/httpclient/client_test.go
+++ internal/httpclient/client_test.go
@@ -30,7 +30,7 @@ func TestNew_userAgent(t *testing.T) {
 		request  func(c *http.Client) error
 	}{
 		{
-			fmt.Sprintf("Terraform/%s", version.Version),
+			fmt.Sprintf("OpenTF/%s", version.Version),
 			func(c *http.Client) error {
 				_, err := c.Get(ts.URL)
 				return err
