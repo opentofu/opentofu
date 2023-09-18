9da0bc3aeba7fb6e21606c8bf3c6556a2afbae05
Lars Lehtonen <lars.lehtonen@gmail.com>
2023-09-18 13:09:10 -0700

    internal/cloud: wrap formatted error
    Signed-off-by: Lars Lehtonen <lars.lehtonen@gmail.com>

--- internal/cloud/backend_taskStages.go
+++ internal/cloud/backend_taskStages.go
@@ -177,9 +177,7 @@ func (b *Cloud) processStageOverrides(context *IntegrationContext, output Integr
 	runUrl := fmt.Sprintf(taskStageHeader, b.hostname, b.organization, context.Op.Workspace, context.Run.ID)
 	err := b.confirm(context.StopContext, context.Op, opts, context.Run, "override")
 	if err != nil && err != errRunOverridden {
-		return false, fmt.Errorf(
-			fmt.Sprintf("Failed to override: %s\n%s\n", err, runUrl),
-		)
+		return false, fmt.Errorf("Failed to override: %w\n%s\n", err, runUrl)
 	}
 
 	if err != errRunOverridden {
