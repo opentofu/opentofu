6f3213c39865754e938373fc8634c371fb52925c
Dmitry Kisler <admin@dkisler.com>
2023-09-26 15:16:24 +0200

    fix: https://github.com/opentofu/opentofu/pull/576#discussion_r1337086717
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- internal/command/jsonchecks/checks.go
+++ internal/command/jsonchecks/checks.go
@@ -70,7 +70,7 @@ func MarshalCheckStates(results *states.CheckResults) []byte {
 // checkResultStatic is the container for the static, configuration-driven
 // idea of "checkable object" -- a resource block with conditions, for example --
 // which ensures that we can always say _something_ about each checkable
-// object in the configuration even if OpenTofOpenTofu Core encountered an error
+// object in the configuration even if OpenTofu Core encountered an error
 // before being able to determine the dynamic instances of the checkable object.
 type checkResultStatic struct {
 	// Address is the address of the checkable object this result relates to.
