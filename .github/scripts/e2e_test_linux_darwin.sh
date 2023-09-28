8f8e25c6eeabcf93b21621a71f4a0a669dca0702
Elbaz <eranelbaz97+github@gmail.com>
2023-09-28 11:24:30 +0300

    Fix build workflow is failing (#608)

--- .github/scripts/e2e_test_linux_darwin.sh
+++ .github/scripts/e2e_test_linux_darwin.sh
@@ -7,12 +7,12 @@ set -uo pipefail
 if [[ $arch == 'arm' || $arch == 'arm64' ]]
 then
     export DIR=$(mktemp -d)
-    unzip -d $DIR "${e2e_cache_path}/opentofu-e2etest_${os}_${arch}.zip"
-    unzip -d $DIR "./opentofu_${version}_${os}_${arch}.zip"
+    unzip -d $DIR "${e2e_cache_path}/tofu-e2etest_${os}_${arch}.zip"
+    unzip -d $DIR "./tofu_${version}_${os}_${arch}.zip"
     sudo chmod +x $DIR/e2etest
     docker run --platform=linux/arm64 -v $DIR:/src -w /src arm64v8/alpine ./e2etest -test.v
 else
-    unzip "${e2e_cache_path}/opentofu-e2etest_${os}_${arch}.zip"
-    unzip "./opentofu_${version}_${os}_${arch}.zip"
+    unzip "${e2e_cache_path}/tofu-e2etest_${os}_${arch}.zip"
+    unzip "./tofu_${version}_${os}_${arch}.zip"
     TF_ACC=1 ./e2etest -test.v
 fi
