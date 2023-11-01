a5cc4ff09db3d24aeddea8ff17e85c724b60805f
igor-savchenko <igor@scalr.com>
2023-11-01 11:19:33 -0700

    Backend/Remote: Update useragent from go-tfe to opentofu

--- internal/backend/remote/backend.go
+++ internal/backend/remote/backend.go
@@ -334,8 +334,12 @@ func (b *Remote) Configure(obj cty.Value) tfdiags.Diagnostics {
 	// Set the version header to the current version.
 	cfg.Headers.Set(tfversion.Header, tfversion.Version)
 
+	// Update user-agent from go-tfe to opentofu
+	cfg.Headers.set("User-Agent", httpclient.OpenTofuUserAgent(version.String()))
+
 	// Create the remote backend API client.
 	b.client, err = tfe.NewClient(cfg)
+
 	if err != nil {
 		diags = diags.Append(tfdiags.Sourceless(
 			tfdiags.Error,
