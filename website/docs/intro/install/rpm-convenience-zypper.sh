2fcc1eaf38c7eedc45b69cbbf6cabf1290d26692
Isaac Parker <parrotmac@gmail.com>
2023-11-28 09:22:08 -0700

    fix: quote URLs; add security & behaviorial opts
    Breakdown of options:
    - `--proto '=https'` Permit only HTTPS
    - `--tlsv1.2` Require TLS version 1.2 or later
    - `-f` Fail fast on any non-OK status code
    - `-s` (Existing) Silent/Don't show progress
    - `-S` Show error, if any!
    - `-L` Follow redirects (e.g. usually some kind of permanent redirect)
    
    Signed-off-by: Isaac Parker <parrotmac@gmail.com>

--- website/docs/intro/install/rpm-convenience-zypper.sh
+++ website/docs/intro/install/rpm-convenience-zypper.sh
@@ -1,4 +1,4 @@
-curl -s https://packagecloud.io/install/repositories/opentofu/tofu/script.rpm.sh?any=true -o /tmp/tofu-repository-setup.sh
+curl --proto '=https' --tlsv1.2 -fsSL 'https://packagecloud.io/install/repositories/opentofu/tofu/script.rpm.sh?any=true' -o /tmp/tofu-repository-setup.sh
 # Inspect the downloaded script at /tmp/tofu-repository-setup.sh before running
 sudo bash /tmp/tofu-repository-setup.sh
 rm /tmp/tofu-repository-setup.sh
