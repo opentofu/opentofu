9a505e1034a253f206e229bead634d5e84f76ad0
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2023-12-06 12:52:19 +0100

    Updated installation instructions to packages.opentofu.org
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>

--- website/docs/intro/install/deb-step2.sh
+++ website/docs/intro/install/deb-step2.sh
@@ -1,3 +1,3 @@
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://packagecloud.io/opentofu/tofu/gpgkey | sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu.gpg
+curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu.gpg
 sudo chmod a+r /etc/apt/keyrings/opentofu.gpg
