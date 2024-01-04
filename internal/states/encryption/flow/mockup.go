d56211e928b99d2090effded7eae9a90a8a7bc9d
StephanHCB <sbs_github_u43a@packetloss.de>
2024-01-04 16:27:46 +0100

    feat(#1030): add minimal config handling to mockup
    Signed-off-by: StephanHCB <sbs_github_u43a@packetloss.de>

--- internal/states/encryption/flow/mockup.go
+++ internal/states/encryption/flow/mockup.go
@@ -8,43 +8,104 @@ import (
 
 // MockUpLoggingFlow will be removed when we replace it with the real implementation.
 type MockUpLoggingFlow struct {
-	configKey string
-	logger    hclog.Logger
+	configKey                 string
+	encryptionConfigs         map[ConfigurationSource]encryptionconfig.Config
+	decryptionFallbackConfigs map[ConfigurationSource]encryptionconfig.Config
+	logger                    hclog.Logger
 }
 
 func NewMock(configKey string) Flow {
 	return &MockUpLoggingFlow{
-		configKey: configKey,
-		logger:    logging.HCLogger(),
+		configKey:                 configKey,
+		encryptionConfigs:         make(map[ConfigurationSource]encryptionconfig.Config),
+		decryptionFallbackConfigs: make(map[ConfigurationSource]encryptionconfig.Config),
+		logger:                    logging.HCLogger(),
 	}
 }
 
 func (m *MockUpLoggingFlow) DecryptState(payload []byte) ([]byte, error) {
 	m.logger.Trace("encryption:DecryptState", "payloadSize", len(payload))
+	if err := m.mergeAndValidateConfigurations(); err != nil {
+		return []byte{}, err
+	}
 	return payload, nil
 }
 
 func (m *MockUpLoggingFlow) EncryptState(state []byte) ([]byte, error) {
 	m.logger.Trace("encryption:EncryptState", "stateSize", len(state))
+	if err := m.mergeAndValidateConfigurations(); err != nil {
+		return []byte{}, err
+	}
 	return state, nil
 }
 
 func (m *MockUpLoggingFlow) DecryptPlan(payload []byte) ([]byte, error) {
 	m.logger.Trace("encryption:DecryptPlan", "payloadSize", len(payload))
+	if err := m.mergeAndValidateConfigurations(); err != nil {
+		return []byte{}, err
+	}
 	return payload, nil
 }
 
 func (m *MockUpLoggingFlow) EncryptPlan(plan []byte) ([]byte, error) {
 	m.logger.Trace("encryption:EncryptPlan", "planSize", len(plan))
+	if err := m.mergeAndValidateConfigurations(); err != nil {
+		return []byte{}, err
+	}
 	return plan, nil
 }
 
 func (m *MockUpLoggingFlow) EncryptionConfiguration(source ConfigurationSource, config encryptionconfig.Config) error {
 	m.logger.Trace("encryption:EncryptionConfiguration", "source", source, "config", config)
+	// for this simple mock, we just store the configuration, so we can later merge and validate it
+	m.encryptionConfigs[source] = config
 	return nil
 }
 
 func (m *MockUpLoggingFlow) DecryptionFallbackConfiguration(source ConfigurationSource, config encryptionconfig.Config) error {
 	m.logger.Trace("encryption:DecryptionFallbackConfiguration", "source", source, "config", config)
+	// for this simple mock, we just store the configuration, so we can later merge and validate it
+	m.decryptionFallbackConfigs[source] = config
 	return nil
+}
+
+func (m *MockUpLoggingFlow) mergeAndValidateConfigurations() error {
+	// this logic will appear in a similar form in the actual flow implementation. For now, we just merge
+	// and validate the configuration.
+
+	mergedEncryptionConfig := encryptionconfig.MergeConfigs(
+		configOrNil(m.encryptionConfigs, ConfigurationSourceEnvDefault),
+		configOrNil(m.encryptionConfigs, ConfigurationSourceCode),
+		configOrNil(m.encryptionConfigs, ConfigurationSourceEnv),
+	)
+	encryptionconfig.InjectDefaultNamesIfUnset(mergedEncryptionConfig)
+
+	mergedDecryptionFallbackConfig := encryptionconfig.MergeConfigs(
+		configOrNil(m.decryptionFallbackConfigs, ConfigurationSourceEnvDefault),
+		configOrNil(m.decryptionFallbackConfigs, ConfigurationSourceCode),
+		configOrNil(m.decryptionFallbackConfigs, ConfigurationSourceEnv),
+	)
+	encryptionconfig.InjectDefaultNamesIfUnset(mergedDecryptionFallbackConfig)
+
+	if mergedEncryptionConfig != nil {
+		if err := mergedEncryptionConfig.Validate(); err != nil {
+			return err
+		}
+	}
+	if mergedDecryptionFallbackConfig != nil {
+		if err := mergedDecryptionFallbackConfig.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func configOrNil(configs map[ConfigurationSource]encryptionconfig.Config, source ConfigurationSource) *encryptionconfig.Config {
+	conf, ok := configs[source]
+	if ok {
+		return &conf
+	} else {
+		return nil
+	}
 }
