6d1480f0b5d38a13aa22bdccc42a11f7d87900ce
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2024-01-10 14:03:38 +0100

    Fixed Alpine and RPM instructions
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>

--- website/docs/intro/install/alpine-convenience.sh
+++ website/docs/intro/install/alpine-convenience.sh
@@ -8,7 +8,7 @@ chmod +x install-opentofu.sh
 # Please inspect the downloaded script
 
 # Run the installer:
-./install-opentofu.sh --install-method alpine
+./install-opentofu.sh --install-method apk
 
 # Remove the installer:
 rm install-opentofu.sh
