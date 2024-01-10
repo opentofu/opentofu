370ca0578ca7562c2e2333af4b2b9738e6f54405
Christian Mesh <christianmesh1@gmail.com>
2024-01-10 10:56:34 -0500

    docs: fix wget https://get.opentofu.org command (#1095) (#1096)
    Signed-off-by: Patrick Decat <pdecat@gmail.com>
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>
    Co-authored-by: Patrick Decat <pdecat@gmail.com>

--- website/docs/intro/install/alpine-convenience.sh
+++ website/docs/intro/install/alpine-convenience.sh
@@ -1,6 +1,6 @@
 # Download the installer script:
 curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
-# Alternatively: wget --secure-protocol=TLSv1_2 --https-only https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
+# Alternatively: wget --secure-protocol=TLSv1_2 --https-only https://get.opentofu.org/install-opentofu.sh -O install-opentofu.sh
 
 # Give it execution permissions:
 chmod +x install-opentofu.sh
