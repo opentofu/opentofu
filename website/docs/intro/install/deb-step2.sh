ac7608c136bfe83db5f577b4fbf3bea174d8cdab
Nils Ballmann <45712604+nsballmann@users.noreply.github.com>
2024-01-16 11:38:38 +0100

    docs(install): add missing steps in Debian family when facing hardened umask
    Signed-off-by: Nils Ballmann <45712604+nsballmann@users.noreply.github.com>

--- website/docs/intro/install/deb-step2.sh
+++ website/docs/intro/install/deb-step2.sh
@@ -1,4 +1,4 @@
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://get.opentofu.org/opentofu.gpg | sudo tee /etc/apt/keyrings/opentofu.gpg >/dev/null
 curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
-sudo chmod a+r /etc/apt/keyrings/opentofu.gpg
+sudo chmod a+r /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
