0aeae578fe2eee4cf1e224b1bfaba6bcb5a72ac8
Christian Mesh <christianmesh1@gmail.com>
2024-01-17 06:50:53 -0500

    Remove unused resources in variable reference test
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/command/testdata/test/variables_reference/setup/main.tf
+++ internal/command/testdata/test/variables_reference/setup/main.tf
@@ -1,16 +1,6 @@
 variable "content" {
   type = string
 }
-/*
-resource "random_string" "file_name" {
-  length           = 16
-  special          = false
-}
-
-resource "local_file" "foo" {
-  filename = random_string.file_name.result
-  content  = var.content
-}*/
 
 output "file_name" {
   value = "output_value"
