49c112494f8b8601e841c829e5dc0fd9f4cc60a2
Janos <86970079+janosdebugs@users.noreply.github.com>
2024-01-25 08:36:56 +0100

    Backport: Fix typo in standalone-install.sh (#1186) (#1187)
    Signed-off-by: Johannes Røsvik <j.rosvik@gmail.com>
    Signed-off-by: Janos <86970079+janosdebugs@users.noreply.github.com>
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>

--- website/docs/intro/install/standalone-install.sh
+++ website/docs/intro/install/standalone-install.sh
@@ -2,7 +2,7 @@
 curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
 # Alternatively: wget --secure-protocol=TLSv1_2 --https-only https://get.opentofu.org/install-opentofu.sh -O install-opentofu.sh
 
-# Grand execution permissions:
+# Grant execution permissions:
 chmod +x install-opentofu.sh
 
 # Please inspect the downloaded script at this point.
