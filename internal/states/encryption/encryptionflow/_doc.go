c5a56806dbf885ebfc7aa5cd1e00612928de00af
Christian Mesh <christianmesh1@gmail.com>
2024-01-31 12:30:30 -0500

    Remove initial states/encryption package in favor of encryption package
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/states/encryption/encryptionflow/_doc.go
+++
@@ -1,3 +0,0 @@
-// Package encryptionflow contains the top-level flow for client-side state encryption and the interfaces
-// an encryption method or key provider need to implement.
-package encryptionflow
