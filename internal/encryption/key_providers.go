f3c62c91848633ff29c1302fda0da174709734c5
Christian Mesh <christianmesh1@gmail.com>
2024-02-02 11:05:50 -0500

    Remove unused file
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/encryption/key_providers.go
+++
@@ -1,9 +0,0 @@
-package encryption
-
-type PassphraseKeyProvider struct {
-	Passphrase string `hcl:"passphrase"`
-}
-
-func (p PassphraseKeyProvider) KeyData() ([]byte, error) {
-	return []byte(p.Passphrase), nil
-}
