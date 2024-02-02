32142949d2a6b8bcb8ccb14874e92a7bb5bbb2e0
James Humphries <james@james-humphries.co.uk>
2024-02-02 16:34:22 +0000

    Updated encryption example test to be functional and commented
    Signed-off-by: James Humphries <james@james-humphries.co.uk>
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/encryption/example_test.go
+++ internal/encryption/example_test.go
@@ -2,13 +2,43 @@ package encryption_test
 
 import (
 	"fmt"
+
+	"github.com/hashicorp/hcl/v2"
 	"github.com/opentofu/opentofu/internal/encryption"
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/static"
 	"github.com/opentofu/opentofu/internal/encryption/method/aesgcm"
 	"github.com/opentofu/opentofu/internal/encryption/registry/lockingencryptionregistry"
 )
 
+var (
+	ConfigA = `
+backend {
+	enforced = true
+}
+`
+	ConfigB = `
+key_provider "static" "basic" {
+	key = "6f6f706830656f67686f6834616872756f3751756165686565796f6f72653169"
+}
+method "aes_gcm" "example" {
+	cipher = key_provider.static.basic
+}
+planfile {
+	method = method.aes_gcm.example
+}
+
+backend {
+	method = method.aes_gcm.example
+}
+`
+)
+
+// ExampleEncryption demonstrates how to use the encryption package to encrypt and decrypt data.
+// This example uses the static key provider and the AES-GCM method.
+// It also shows how to merge multiple configurations.
 func ExampleEncryption() {
+	// Construct a new registry
+	// the registry is where we store the key providers and methods
 	reg := lockingencryptionregistry.New()
 	if err := reg.RegisterKeyProvider(static.New()); err != nil {
 		panic(err)
@@ -17,42 +47,46 @@ func ExampleEncryption() {
 		panic(err)
 	}
 
-	cfgA, diags := encryption.LoadConfigFromString("Test Source A", `
-backend {
-	enforced = true
-}
-`)
-	if diags.HasErrors() {
-		panic(diags.Error())
-	}
+	// Load the 2 different configurations
+	cfgA, diags := encryption.LoadConfigFromString("Test Source A", ConfigA)
+	handleDiags(diags)
 
-	cfgB, diags := encryption.LoadConfigFromString("Test Source B", `
-key_provider "static" "basic" {
-	key = "6f6f706830656f67686f6834616872756f3751756165686565796f6f72653169"
-}
-method "aes_gcm" "example" {
-	cipher = key_provider.static.basic
-}
-backend {
-	method = method.aes_cipher.example
-}
-`)
-	if diags.HasErrors() {
-		panic(diags.Error())
-	}
+	cfgB, diags := encryption.LoadConfigFromString("Test Source B", ConfigB)
+	handleDiags(diags)
 
+	// Merge the configurations
 	cfg := encryption.MergeConfigs(cfgA, cfgB)
 
+	// Construct the encryption object
 	enc, diags := encryption.New(reg, cfg)
+	handleDiags(diags)
 
+	// Encrypt the data, for this example we will be using the string "test",
+	// but in a real world scenario this would be the plan file.
+	sourceData := []byte("test")
+	encrypted, err := enc.PlanFile().EncryptPlan(sourceData)
+	if err != nil {
+		panic(err)
+	}
+	if string(encrypted) == "test" {
+		panic("The data has not been encrypted!")
+	}
+
+	// Decrypt
+	decryptedPlan, err := enc.PlanFile().DecryptPlan(encrypted)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", decryptedPlan)
+	// Output: test
+}
+
+func handleDiags(diags hcl.Diagnostics) {
 	for _, d := range diags {
 		println(d.Error())
 	}
-
 	if diags.HasErrors() {
 		panic(diags.Error())
 	}
-
-	fmt.Printf("%#v\n", enc)
-	// Output: test
 }
