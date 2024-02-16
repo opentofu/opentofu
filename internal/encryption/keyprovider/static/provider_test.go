be4d634d302cbdac3c2e43821cfdcbab4f8d5af1
James Humphries <james@james-humphries.co.uk>
2024-02-16 11:55:58 +0000

    Updated provider_test.go to use table based tests and fixed them to handle new metadata
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/encryption/keyprovider/static/provider_test.go
+++ internal/encryption/keyprovider/static/provider_test.go
@@ -6,46 +6,67 @@ import (
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/static"
 )
 
-func TestEmpty(t *testing.T) {
-	descriptor := static.New()
-	config := descriptor.ConfigStruct()
-	keyProvider, err := config.Build()
-	if err != nil {
-		panic(err)
+func TestKeyProvider(t *testing.T) {
+	type testCase struct {
+		name          string
+		key           string
+		expectSuccess bool
+		expectedData  string
+		expectedMeta  string
 	}
-	data, err := keyProvider.Provide()
-	if err != nil {
-		t.Fatalf("unespected error: %v", err)
-	}
-	if len(data) != 0 {
-		t.Fatalf("unexpected key output: %v", data)
-	}
-}
 
-// TestInvalidInput tests if an error is throw with an invalid input.
-func TestInvalidInput(t *testing.T) {
-	descriptor := static.New()
-	config := descriptor.ConfigStruct().(*static.Config)
-	config.Key = "G"
-	_, err := config.Build()
-	if err == nil {
-		t.Fatalf("unexpected success")
+	testCases := []testCase{
+		{
+			name:          "Empty",
+			expectSuccess: true,
+			expectedData:  "",
+			expectedMeta:  "magic",
+		},
+		{
+			name:          "InvalidInput",
+			key:           "G",
+			expectSuccess: false,
+		},
+		{
+			name:          "Success",
+			key:           "48656c6c6f20776f726c6421",
+			expectSuccess: true,
+			expectedData:  "Hello world!",
+			expectedMeta:  "magic",
+		},
 	}
-}
 
-func TestSuccess(t *testing.T) {
-	descriptor := static.New()
-	config := descriptor.ConfigStruct().(*static.Config)
-	config.Key = "48656c6c6f20776f726c6421"
-	keyProvider, err := config.Build()
-	if err != nil {
-		panic(err)
-	}
-	data, err := keyProvider.Provide()
-	if err != nil {
-		t.Fatalf("unespected error: %v", err)
-	}
-	if string(data) != "Hello world!" {
-		t.Fatalf("unexpected key output: %v", data)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			descriptor := static.New()
+			c := descriptor.ConfigStruct().(*static.Config)
+
+			// Set key if provided
+			if tc.key != "" {
+				c.Key = tc.key
+			}
+
+			keyProvider, buildErr := c.Build()
+			if tc.expectSuccess {
+				if buildErr != nil {
+					t.Fatalf("unexpected error: %v", buildErr)
+				}
+
+				data, newMetadata, err := keyProvider.Provide(nil)
+				if err != nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+				if string(data) != tc.expectedData {
+					t.Fatalf("unexpected key output: got %v, want %v", data, tc.expectedData)
+				}
+				if string(newMetadata) != tc.expectedMeta {
+					t.Fatalf("unexpected metadata: got %v, want %v", newMetadata, tc.expectedMeta)
+				}
+			} else {
+				if buildErr == nil {
+					t.Fatalf("expected an error but got none")
+				}
+			}
+		})
 	}
 }
