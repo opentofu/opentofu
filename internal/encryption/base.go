35e99cd93c7b6c063a215ae71cc9fe7debe0a4d5
James Humphries <james@james-humphries.co.uk>
2024-02-22 12:13:26 +0000

    Cleaned up base.go
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/encryption/base.go
+++ internal/encryption/base.go
@@ -17,6 +17,10 @@ import (
 )
 
 const (
+	// encryptionVersion is the version of the encryption format
+	// used in the json that is stored in the backend
+	// This is used to ensure that we can handle future changes to the encryption format
+	// and to ensure that we can handle migration between multiple versions of the encryption format over time
 	encryptionVersion = "v0"
 )
 
@@ -73,10 +77,12 @@ func (b *baseEncryption) encrypt(data []byte) ([]byte, hcl.Diagnostics) {
 			})
 			return nil, diags
 		}
+		// if no method is defined and Enforced is false, we can just return the data as is, no encryption
+		// should be performed
 		return data, nil
 	}
 
-	encd, err := encryptor.Encrypt(data)
+	encrypted, err := encryptor.Encrypt(data)
 	if err != nil {
 		return nil, append(diags, &hcl.Diagnostic{
 			Severity: hcl.DiagError,
@@ -85,8 +91,10 @@ func (b *baseEncryption) encrypt(data []byte) ([]byte, hcl.Diagnostics) {
 		})
 	}
 
-	es.Data = encd
-	jsond, err := json.Marshal(es)
+	es.Data = encrypted
+
+	// Marshal the baseData struct to json ready to be stored
+	marshalledData, err := json.Marshal(es)
 	if err != nil {
 		return nil, append(diags, &hcl.Diagnostic{
 			Severity: hcl.DiagError,
@@ -95,10 +103,9 @@ func (b *baseEncryption) encrypt(data []byte) ([]byte, hcl.Diagnostics) {
 		})
 	}
 
-	return jsond, diags
+	return marshalledData, diags
 }
 
-// TODO Find a way to make these errors actionable / clear
 func (b *baseEncryption) decrypt(data []byte, validator func([]byte) error) ([]byte, hcl.Diagnostics) {
 	if b.target == nil {
 		return data, nil
@@ -109,32 +116,14 @@ func (b *baseEncryption) decrypt(data []byte, validator func([]byte) error) ([]b
 	if err != nil {
 		return nil, hcl.Diagnostics{&hcl.Diagnostic{
 			Severity: hcl.DiagError,
-			Summary:  "Invalid data format for decryption",
+			Summary:  "Failed to decode encrypted payload as json",
 			Detail:   err.Error(),
 		}}
 	}
 
-	if len(es.Version) == 0 {
-		// Not a valid payload, might be already decrypted
-		err = validator(data)
-		if err == nil {
-			// Yep, it'b already decrypted
-			return data, nil
-		} else {
-			// Nope, just bad input
-			return nil, hcl.Diagnostics{&hcl.Diagnostic{
-				Severity: hcl.DiagError,
-				Summary:  "Unable to determine data structure during decryption",
-			}}
-		}
-	}
-
-	if es.Version != encryptionVersion {
-		return nil, hcl.Diagnostics{&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Invalid encrypted payload version",
-			Detail:   fmt.Sprintf("%b != %b", es.Version, encryptionVersion),
-		}}
+	data, diags := validateVersion(data, validator, es)
+	if diags.HasErrors() {
+		return nil, diags
 	}
 
 	methods, diags := b.buildTargetMethods(es.Meta)
@@ -142,29 +131,48 @@ func (b *baseEncryption) decrypt(data []byte, validator func([]byte) error) ([]b
 		return nil, diags
 	}
 
+	// Ensure that we have some methods that we can use
 	if len(methods) == 0 {
+		// If we do not have any methods defined, we can just use the validator to check if the data is valid
+		// and return that
 		err = validator(data)
-		if err == nil {
-			// No methods/fallbacks specified and data is valid payload
-			return data, diags
-		} else {
-			// TODO improve this error message
+		if err != nil {
 			return nil, append(diags, &hcl.Diagnostic{
 				Severity: hcl.DiagError,
+				Detail:   "Attempted decryption failed, no methods or fallbacks specified and the data is invalid",
 				Summary:  err.Error(),
 			})
 		}
+		// No methods/fallbacks specified and data is valid payload
+		return data, diags
 	}
 
+	// Initialize diagnostics to record any issues encountered during decryption.
 	var methodDiags hcl.Diagnostics
+
+	// Attempt to decrypt the data using each method in turn
 	for _, method := range methods {
 		if method == nil {
-			// No method specified for this target
+			// If no method has been specified for this target, attempt to validate and return the
+			// data if it is valid
 			err = validator(data)
-			if err == nil {
-				return data, diags
+			if err != nil {
+				// We record the validation failure but continue to the next method
+				// this allows us to record all failures and not just the first one
+				methodDiags = append(methodDiags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Attempted decryption failed for " + b.name,
+					Detail:   err.Error(),
+				})
+				continue
 			}
-			// toDO improve this error message
+			// Validation was successful, return the data
+			return data, diags
+		}
+		// Attempt to decrypt the data using the method
+		decrypted, err := method.Decrypt(es.Data)
+		if err != nil {
+			// Record the failure and move on
 			methodDiags = append(methodDiags, &hcl.Diagnostic{
 				Severity: hcl.DiagError,
 				Summary:  "Attempted decryption failed for " + b.name,
@@ -172,19 +180,11 @@ func (b *baseEncryption) decrypt(data []byte, validator func([]byte) error) ([]b
 			})
 			continue
 		}
-		uncd, err := method.Decrypt(es.Data)
-		if err == nil {
-			// Success
-			return uncd, diags
-		}
-		// Record the failure
-		methodDiags = append(methodDiags, &hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Attempted decryption failed for " + b.name,
-			Detail:   err.Error(),
-		})
+		// Successfully decrypted, return the data
+		return decrypted, diags
 	}
 
+	// If we have reached here, we have attempted to decrypt with all the methods and failed
 	// Record the overall failure
 	diags = append(diags, &hcl.Diagnostic{
 		Severity: hcl.DiagError,
@@ -193,4 +193,38 @@ func (b *baseEncryption) decrypt(data []byte, validator func([]byte) error) ([]b
 	})
 
 	return nil, append(diags, methodDiags...)
+}
+
+// validateVersion takes in the possibly encrypted data and validates that it is in the expected format
+// It returns the data if it is already decrypted, or diagnostics if it is not in the expected format
+func validateVersion(data []byte, validator func([]byte) error, es baseData) ([]byte, hcl.Diagnostics) {
+	// Valid payloads should have their `Version` field set correctly
+	// We can use this as flag to detect if the json marshalling unmarshalled a valid payload
+	// or if it was just bad input
+	if len(es.Version) == 0 {
+		// Check if the data is already decrypted by passing it through the validator
+		// If it's already decrypted then we can return the data directly
+		err := validator(data)
+		if err != nil {
+			// we can return an error because someone could've passed in some other form of json data
+			return nil, hcl.Diagnostics{&hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "Unable to determine data type during decryption",
+			}}
+		}
+		// Data may already be decrypted, return it
+		return data, nil
+	}
+
+	if es.Version != encryptionVersion {
+		// In the future, we should handle migration between versions, however for now we should just error
+		// as we only have one version
+		return nil, hcl.Diagnostics{&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Invalid encrypted payload version",
+			Detail:   fmt.Sprintf("expected %q, got %q", encryptionVersion, es.Version),
+		}}
+	}
+
+	return data, nil
 }
