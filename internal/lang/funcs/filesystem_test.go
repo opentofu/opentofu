e56642520e6fc90f2110385f0561600762bdda63
sanskruti-shahu <sanskruti.shahu@harness.io>
2024-02-23 01:26:37 +0530

    did some very minor fix
    Signed-off-by: sanskruti-shahu <sanskruti.shahu@harness.io>

--- internal/lang/funcs/filesystem_test.go
+++ internal/lang/funcs/filesystem_test.go
@@ -151,7 +151,7 @@ func TestTemplateFile(t *testing.T) {
 			cty.StringVal("testdata/recursive.tmpl"),
 			cty.MapValEmpty(cty.String),
 			cty.NilVal,
-			`testdata/recursive.tmpl:1,3-16: Error in function call; Call to function "templatefile" failed: cannot recursively call templatefile from inside templatefile call.`,
+			`testdata/recursive.tmpl:1,3-16: Error in function call; Call to function "templatefile" failed: cannot recursively call templatefile from inside templatefile or templatestring.`,
 		},
 		{
 			cty.StringVal("testdata/list.tmpl"),
