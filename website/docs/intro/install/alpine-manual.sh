a356889a6c6d27d769856da2ddedc20819b355c0
Janos <86970079+janosdebugs@users.noreply.github.com>
2024-02-27 14:29:43 +0100

    Backport: #1314 removing erroneously added docs stuff (#1315)
    Signed-off-by: Janos <86970079+janosdebugs@users.noreply.github.com>

--- website/docs/intro/install/alpine-manual.sh
+++ website/docs/intro/install/alpine-manual.sh
@@ -1,7 +1,2 @@
-# Copyright (c) The OpenTofu Authors
-# SPDX-License-Identifier: MPL-2.0
-# Copyright (c) 2023 HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 echo '@community https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
 apk add opentofu@community
