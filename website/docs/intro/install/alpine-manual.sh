806c58582462c4564ddabf4a65f8a8c455c4a5fa
Russell Poovey <09.our_seekers@icloud.com>
2024-02-27 05:23:47 -0500

    Update alpine-manual.sh (#1311)
    Signed-off-by: Russell Poovey <git@0896c69e.anonaddy.com>

--- website/docs/intro/install/alpine-manual.sh
+++ website/docs/intro/install/alpine-manual.sh
@@ -3,5 +3,5 @@
 # Copyright (c) 2023 HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-echo '@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories
-apk add opentofu@testing
+echo '@community https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
+apk add opentofu@community
