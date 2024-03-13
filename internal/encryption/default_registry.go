fd18bdd5619bb20177a70e907a6246752e70bbc5
Christian Mesh <christianmesh1@gmail.com>
2024-03-13 17:31:59 -0400

    Initial prototype of azure_kms key provider (untested)
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/encryption/default_registry.go
+++ internal/encryption/default_registry.go
@@ -9,6 +9,7 @@ import (
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/aws_kms"
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/azure_kms"
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/gcp_kms"
+	"github.com/opentofu/opentofu/internal/encryption/keyprovider/azure_kms"
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/pbkdf2"
 	"github.com/opentofu/opentofu/internal/encryption/method/aesgcm"
 	"github.com/opentofu/opentofu/internal/encryption/registry/lockingencryptionregistry"
@@ -30,6 +31,9 @@ func init() {
 		panic(err)
 	}
 	if err := DefaultRegistry.RegisterKeyProvider(gcp_kms.New()); err != nil {
+		panic(err)
+	}
+	if err := DefaultRegistry.RegisterKeyProvider(azure_kms.New()); err != nil {
 		panic(err)
 	}
 }
