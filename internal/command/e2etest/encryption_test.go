3ff5a9b2cac68019b48e6cc7dfb1f46ac77c7ab9
Christian Mesh <christianmesh1@gmail.com>
2024-03-13 11:18:23 -0400

    Encryption state e2e test
    Full migration to and from flow
    
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/command/e2etest/encryption_test.go
+++ internal/command/e2etest/encryption_test.go
@@ -1,25 +1,28 @@
 package e2etest
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/opentofu/opentofu/internal/e2e"
 )
 
-func TestEncryptionFlow(t *testing.T) {
+// This test covers the scenario where a user migrates an existing project
+// to having encryption enabled, uses it, then migrates back to encryption
+// disabled
+func TestEncryptionStateFlow(t *testing.T) {
 
 	// This test reaches out to registry.opentofu.org to download the
-	// template provider, so it can only run if network access is allowed.
-	// We intentionally don't try to stub this here, because there's already
-	// a stubbed version of this in the "command" package and so the goal here
-	// is to test the interaction with the real repository.
+	// mock provider, so it can only run if network access is allowed
 	skipIfCannotAccessNetwork(t)
 
-	fixturePath := filepath.Join("testdata", "encryption-enabled")
+	fixturePath := filepath.Join("testdata", "encryption-flow")
 	tf := e2e.NewBinary(t, tofuBin, fixturePath)
 
-	// Setup the local encryption
+	// tofu init
 	_, stderr, err := tf.Run("init")
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
@@ -28,30 +31,116 @@ func TestEncryptionFlow(t *testing.T) {
 		t.Errorf("unexpected stderr output:\n%s", stderr)
 	}
 
-	_, stderr, err = tf.Run("apply", `-var=iter=first`, `-auto-approve`)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
+	iter := 0
+
+	apply := func() (stdout, stderr string, err error) {
+		iter += 1
+		return tf.Run("apply", fmt.Sprintf("-var=iter=%v", iter), "-auto-approve")
 	}
-	if stderr != "" {
-		t.Errorf("unexpected stderr output:\n%s", stderr)
+	applySuccess := func() {
+		_, stderr, err := apply()
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		if stderr != "" {
+			t.Fatalf("unexpected stderr output:\n%s", stderr)
+		}
+	}
+	applyFailure := func(msg string) {
+		_, stderr, err := apply()
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		if !strings.Contains(stderr, msg) {
+			t.Fatalf("expected stderr output %q:\n%s", msg, stderr)
+		}
 	}
 
-	_, err = tf.LocalState()
-	if err == nil || err.Error() != "Error reading statefile: Unsupported state file format: This state file is encrypted and can not be read without an encryption configuration" {
-		t.Fatalf("failed to read state file: %q", err.Error())
+	requireUnencryptedState := func() {
+		_, err = tf.LocalState()
+		if err != nil {
+			t.Fatalf("expected unencrypted state file: %q", err)
+		}
+	}
+	requireEncryptedState := func() {
+		_, err = tf.LocalState()
+		if err == nil || err.Error() != "Error reading statefile: Unsupported state file format: This state file is encrypted and can not be read without an encryption configuration" {
+			t.Fatalf("expected encrypted state file: %q", err)
+		}
 	}
 
-	_, stderr, err = tf.Run("apply", `-var=iter=second`, `-auto-approve`)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
+	enable := func(path string) {
+		src := tf.Path(path + ".disabled")
+		dst := tf.Path(path)
+		err := os.Rename(src, dst)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
 	}
-	if stderr != "" {
-		t.Errorf("unexpected stderr output:\n%s", stderr)
+	disable := func(path string) {
+		src := tf.Path(path)
+		dst := tf.Path(path + ".disabled")
+		err := os.Rename(src, dst)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
 	}
 
-	_, err = tf.LocalState()
-	if err == nil || err.Error() != "Error reading statefile: Unsupported state file format: This state file is encrypted and can not be read without an encryption configuration" {
-		t.Fatalf("failed to read state file: %q", err.Error())
+	with := func(path string, fn func()) {
+		enable(path)
+		fn()
+		disable(path)
 	}
 
+	// Actual test begins HERE
+
+	{
+		// Everything works before adding encryption configuration
+		applySuccess()
+		requireUnencryptedState()
+		// Check read/write of state file
+		applySuccess()
+		requireUnencryptedState()
+	}
+
+	with("required.tf", func() {
+		// Can't switch directly to encryption, need to migrate
+		applyFailure("decrypted payload provided without fallback specified")
+		requireUnencryptedState()
+	})
+
+	with("migrateto.tf", func() {
+		// Migrate to using encryption
+		applySuccess()
+		requireEncryptedState()
+		// Make changes and confirm it's still encrypted (even with migration enabled)
+		applySuccess()
+		requireEncryptedState()
+	})
+
+	{
+		// Unconfigured encryption clearly fails on encrypted state
+		applyFailure("can not be read without an encryption configuration")
+	}
+
+	with("required.tf", func() {
+		// Encryption works with fallback removed
+		applySuccess()
+		requireEncryptedState()
+	})
+
+	with("migratefrom.tf", func() {
+		// Apply migration from encrypted state
+		applySuccess()
+		requireUnencryptedState()
+		// Make changes and confirm it's still encrypted (even with migration enabled)
+		applySuccess()
+		requireUnencryptedState()
+	})
+
+	{
+		// Back to no encryption configuration with unencrypted state
+		applySuccess()
+		requireUnencryptedState()
+	}
 }
