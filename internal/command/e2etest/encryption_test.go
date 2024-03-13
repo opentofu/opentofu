7c4b26b527ccdb94be38c929d32067a51e732bc4
Christian Mesh <christianmesh1@gmail.com>
2024-03-13 12:01:15 -0400

    Integrate plans into encryption e2e tests
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/command/e2etest/encryption_test.go
+++ internal/command/e2etest/encryption_test.go
@@ -4,20 +4,60 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 	"strings"
 	"testing"
 
 	"github.com/opentofu/opentofu/internal/e2e"
 )
 
+type tofuResult struct {
+	t *testing.T
+
+	stdout string
+	stderr string
+	err    error
+}
+
+func (r tofuResult) Success() tofuResult {
+	if r.stderr != "" {
+		debug.PrintStack()
+		r.t.Fatalf("unexpected stderr output:\n%s", r.stderr)
+	}
+	if r.err != nil {
+		debug.PrintStack()
+		r.t.Fatalf("unexpected error: %s", r.err)
+	}
+
+	return r
+}
+
+func (r tofuResult) Failure() tofuResult {
+	if r.err == nil {
+		debug.PrintStack()
+		r.t.Fatal("expected error")
+	}
+	return r
+}
+
+func (r tofuResult) StderrContains(msg string) tofuResult {
+	if !strings.Contains(r.stderr, msg) {
+		debug.PrintStack()
+		r.t.Fatalf("expected stderr output %q:\n%s", msg, r.stderr)
+	}
+	return r
+}
+
 // This test covers the scenario where a user migrates an existing project
 // to having encryption enabled, uses it, then migrates back to encryption
 // disabled
-func TestEncryptionStateFlow(t *testing.T) {
+func TestEncryptionFlow(t *testing.T) {
 
 	// This test reaches out to registry.opentofu.org to download the
 	// mock provider, so it can only run if network access is allowed
 	skipIfCannotAccessNetwork(t)
+
+	// There is a lot of setup / helpers defined.  Actual test logic is below.
 
 	fixturePath := filepath.Join("testdata", "encryption-flow")
 	tf := e2e.NewBinary(t, tofuBin, fixturePath)
@@ -33,27 +73,21 @@ func TestEncryptionStateFlow(t *testing.T) {
 
 	iter := 0
 
-	apply := func() (stdout, stderr string, err error) {
+	run := func(args ...string) tofuResult {
+		stdout, stderr, err := tf.Run(args...)
+		return tofuResult{t, stdout, stderr, err}
+	}
+	apply := func() tofuResult {
 		iter += 1
-		return tf.Run("apply", fmt.Sprintf("-var=iter=%v", iter), "-auto-approve")
+		return run("apply", fmt.Sprintf("-var=iter=%v", iter), "-auto-approve")
 	}
-	applySuccess := func() {
-		_, stderr, err := apply()
-		if err != nil {
-			t.Fatalf("unexpected error: %s", err)
-		}
-		if stderr != "" {
-			t.Fatalf("unexpected stderr output:\n%s", stderr)
-		}
+
+	createPlan := func(planfile string) tofuResult {
+		iter += 1
+		return run("plan", fmt.Sprintf("-var=iter=%v", iter), "-out="+planfile)
 	}
-	applyFailure := func(msg string) {
-		_, stderr, err := apply()
-		if err == nil {
-			t.Fatal("expected error")
-		}
-		if !strings.Contains(stderr, msg) {
-			t.Fatalf("expected stderr output %q:\n%s", msg, stderr)
-		}
+	applyPlan := func(planfile string) tofuResult {
+		return run("apply", "-auto-approve", planfile)
 	}
 
 	requireUnencryptedState := func() {
@@ -69,78 +103,115 @@ func TestEncryptionStateFlow(t *testing.T) {
 		}
 	}
 
-	enable := func(path string) {
+	with := func(path string, fn func()) {
 		src := tf.Path(path + ".disabled")
 		dst := tf.Path(path)
-		err := os.Rename(src, dst)
-		if err != nil {
-			t.Fatalf(err.Error())
-		}
-	}
-	disable := func(path string) {
-		src := tf.Path(path)
-		dst := tf.Path(path + ".disabled")
-		err := os.Rename(src, dst)
-		if err != nil {
-			t.Fatalf(err.Error())
-		}
-	}
 
-	with := func(path string, fn func()) {
-		enable(path)
+		err := os.Rename(src, dst)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+
 		fn()
-		disable(path)
+
+		err = os.Rename(dst, src)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
 	}
 
 	// Actual test begins HERE
+	// NOTE: state plans are still readable and tests the encryption state
+
+	unencryptedPlan := "unencrypted.tfplan"
+	encryptedPlan := "encrypted.tfplan"
 
 	{
 		// Everything works before adding encryption configuration
-		applySuccess()
+		apply().Success()
 		requireUnencryptedState()
 		// Check read/write of state file
-		applySuccess()
+		apply().Success()
+		requireUnencryptedState()
+
+		// Save an unencrypted plan
+		createPlan(unencryptedPlan).Success()
+		// Validate unencrypted plan
+		applyPlan(unencryptedPlan).Success()
 		requireUnencryptedState()
 	}
 
 	with("required.tf", func() {
 		// Can't switch directly to encryption, need to migrate
-		applyFailure("decrypted payload provided without fallback specified")
+		apply().Failure().StderrContains("decrypted payload provided without fallback specified")
 		requireUnencryptedState()
 	})
 
 	with("migrateto.tf", func() {
 		// Migrate to using encryption
-		applySuccess()
+		apply().Success()
 		requireEncryptedState()
 		// Make changes and confirm it's still encrypted (even with migration enabled)
-		applySuccess()
+		apply().Success()
+		requireEncryptedState()
+
+		// Save an encrypted plan
+		createPlan(encryptedPlan).Success()
+
+		// Apply encrypted plan (with migration active)
+		applyPlan(encryptedPlan).Success()
+		requireEncryptedState()
+		// Apply unencrypted plan (with migration active)
+		applyPlan(unencryptedPlan).StderrContains("Saved plan is stale")
 		requireEncryptedState()
 	})
 
 	{
 		// Unconfigured encryption clearly fails on encrypted state
-		applyFailure("can not be read without an encryption configuration")
+		apply().Failure().StderrContains("can not be read without an encryption configuration")
 	}
 
 	with("required.tf", func() {
 		// Encryption works with fallback removed
-		applySuccess()
+		apply().Success()
+		requireEncryptedState()
+
+		// Can't apply unencrypted plan
+		applyPlan(unencryptedPlan).Failure().StderrContains("decrypted payload provided without fallback specified")
+		requireEncryptedState()
+
+		// Apply encrypted plan
+		applyPlan(encryptedPlan).StderrContains("Saved plan is stale")
 		requireEncryptedState()
 	})
 
 	with("migratefrom.tf", func() {
 		// Apply migration from encrypted state
-		applySuccess()
+		apply().Success()
 		requireUnencryptedState()
 		// Make changes and confirm it's still encrypted (even with migration enabled)
-		applySuccess()
+		apply().Success()
+		requireUnencryptedState()
+
+		// Apply unencrypted plan (with migration active)
+		applyPlan(unencryptedPlan).StderrContains("Saved plan is stale")
+		requireUnencryptedState()
+
+		// Apply encrypted plan (with migration active)
+		applyPlan(encryptedPlan).StderrContains("Saved plan is stale")
 		requireUnencryptedState()
 	})
 
 	{
 		// Back to no encryption configuration with unencrypted state
-		applySuccess()
+		apply().Success()
+		requireUnencryptedState()
+
+		// Apply unencrypted plan
+		applyPlan(unencryptedPlan).StderrContains("Saved plan is stale")
+		requireUnencryptedState()
+		// Can't apply encrypted plan
+		applyPlan(encryptedPlan).Failure().StderrContains("the given plan file is encrypted and requires a valid encryption")
 		requireUnencryptedState()
 	}
 }
