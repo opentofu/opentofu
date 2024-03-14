19a994ee7f997349975a6d2518a1137bf3dd4ff3
Janos <86970079+janosdebugs@users.noreply.github.com>
2024-03-14 15:05:05 +0100

    Documentation updates for 1.7.0-alpha1 (state encryption) (#1396)
    Signed-off-by: Janos <86970079+janosdebugs@users.noreply.github.com>

--- website/docs/language/state/examples/encryption/terraform_remote_state.tf
+++ website/docs/language/state/examples/encryption/terraform_remote_state.tf
@@ -2,11 +2,11 @@ terraform {
   encryption {
     # Key provider and method configuration here
 
-    remote {
+    remote_state_data_sources {
       default {
         method = method.my_method.my_name
       }
-      terraform_remote_state "my_state" {
+      remote_state_data_source "my_state" {
         method = method.my_method.my_other_name
       }
     }
