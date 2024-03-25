d0684ce1f9711be2ea2c96f5d94c29643a927fed
ollevche <ollevche@gmail.com>
2024-03-25 11:10:48 +0200

    fix panic when encryption enabled for remote and cloud backends
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/backend/remote/backend.go
+++ internal/backend/remote/backend.go
@@ -378,7 +378,7 @@ func (b *Remote) Configure(obj cty.Value) tfdiags.Diagnostics {
 	}
 
 	// Configure a local backend for when we need to run operations locally.
-	b.local = backendLocal.NewWithBackend(b, nil)
+	b.local = backendLocal.NewWithBackend(b, b.encryption)
 	b.forceLocal = b.forceLocal || !entitlements.Operations
 
 	// Enable retries for server errors as the backend is now fully configured.
