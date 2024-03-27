50334eb9e74a0193acf037369b4d1d5d88ee4cec
ollevche <ollevche@gmail.com>
2024-03-27 17:03:47 +0200

    add minor improvements
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/encryption/keyprovider/openbao/config.go
+++ internal/encryption/keyprovider/openbao/config.go
@@ -9,10 +9,6 @@ import (
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider"
 )
 
-// TODO: compliance tests
-
-// TODO: script to run bao for tests
-
 type Config struct {
 	Address string `hcl:"address,optional"`
 	Token   string `hcl:"token,optional"`
@@ -21,11 +17,21 @@ type Config struct {
 	DataKeyBitSize int    `hcl:"data_key_bit_size,optional"`
 }
 
-const defaultDataKeyBitSize = 256
-
 func (c Config) Build() (keyprovider.KeyProvider, keyprovider.KeyMeta, error) {
+	if c.KeyName == "" {
+		return nil, nil, &keyprovider.ErrInvalidConfiguration{
+			Message: "no key name found",
+		}
+	}
+
 	if c.DataKeyBitSize == 0 {
 		c.DataKeyBitSize = defaultDataKeyBitSize
+	}
+
+	if err := validateDataKeyBitSize(c.DataKeyBitSize); err != nil {
+		return nil, nil, &keyprovider.ErrInvalidConfiguration{
+			Cause: err,
+		}
 	}
 
 	// DefaultConfig reads BAO_ADDR and some other optional env variables.
@@ -61,6 +67,8 @@ type clientConstructor func(config *openbao.Config, token string) (client, error
 var errNoOpenBaoTokenFound = errors.New("no openbao token found")
 
 // newClient variable allows to inject different client implementations.
+// In order to keep client interface simple, token setting is in this function as well.
+// It's not possible to pass token in config.
 var newClient clientConstructor = func(config *openbao.Config, token string) (client, error) {
 	// NewClient reads BAO_TOKEN and some other optional env variables.
 	c, err := openbao.NewClient(config)
@@ -76,4 +84,14 @@ var newClient clientConstructor = func(config *openbao.Config, token string) (cl
 	}
 
 	return c.Logical(), nil
+}
+
+const defaultDataKeyBitSize = 256
+
+func validateDataKeyBitSize(dataKeyBitSize int) error {
+	if dataKeyBitSize != 128 && dataKeyBitSize != 256 && dataKeyBitSize != 512 {
+		return fmt.Errorf("invalid data key bit size: %d, supported options are 128, 256 or 512", dataKeyBitSize)
+	}
+
+	return nil
 }
