9fe41c47b2a71ca4d8fb927591b687b251a2131d
Christian Mesh <christianmesh1@gmail.com>
2024-04-03 10:39:24 -0400

    Add unencrypted Method for migrations
    Also *requires* methods to be provided for all targets
    
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

---
+++ internal/encryption/method/unencrypted/method.go
@@ -0,0 +1,38 @@
+package unencrypted
+
+import (
+	"github.com/opentofu/opentofu/internal/encryption/method"
+)
+
+func New() method.Descriptor {
+	return &descriptor{}
+}
+
+type descriptor struct{}
+
+func (f *descriptor) ID() method.ID {
+	return "unencrypted"
+}
+func (f *descriptor) ConfigStruct() method.Config {
+	return new(config)
+}
+
+type config struct{}
+
+func (c *config) Build() (method.Method, error) {
+	return new(unenc), nil
+}
+
+type unenc struct{}
+
+func (a *unenc) Encrypt(data []byte) ([]byte, error) {
+	panic("Placeholder for type check!  Should never be called!")
+}
+func (a *unenc) Decrypt(data []byte) ([]byte, error) {
+	panic("Placeholder for type check!  Should never be called!")
+}
+
+func Is(m method.Method) bool {
+	_, ok := m.(*unenc)
+	return ok
+}
