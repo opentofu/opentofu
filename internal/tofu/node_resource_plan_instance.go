553970875758e5ba5f390dcb017ca1f3f18b409d
ollevche <ollevche@gmail.com>
2024-04-22 20:00:47 +0300

    change to a better fix with no actual values
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/tofu/node_resource_plan_instance.go
+++ internal/tofu/node_resource_plan_instance.go
@@ -560,11 +560,21 @@ func (n *NodePlannableResourceInstance) importState(ctx EvalContext, addr addrs.
 
 	// Insert marks from configuration
 	if n.Config != nil {
+		keyData := EvalDataForNoInstanceKey
 
-		forEach, _ := evaluateForEachExpression(n.Config.ForEach, ctx)
-		evalDataInstanceKey := EvalDataForInstanceKey(addr.Resource.Key, forEach)
+		switch {
+		case n.Config.Count != nil:
+			keyData = InstanceKeyEvalData{
+				CountIndex: cty.UnknownVal(cty.Number),
+			}
+		case n.Config.ForEach != nil:
+			keyData = InstanceKeyEvalData{
+				EachKey:   cty.UnknownVal(cty.String),
+				EachValue: cty.UnknownVal(cty.DynamicPseudoType),
+			}
+		}
 
-		valueWithConfigurationSchemaMarks, _, configDiags := ctx.EvaluateBlock(n.Config.Config, n.Schema, nil, evalDataInstanceKey)
+		valueWithConfigurationSchemaMarks, _, configDiags := ctx.EvaluateBlock(n.Config.Config, n.Schema, nil, keyData)
 		diags = diags.Append(configDiags)
 		if configDiags.HasErrors() {
 			return instanceRefreshState, diags
