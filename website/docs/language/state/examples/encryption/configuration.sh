a31aec5950307ed9f6e2a34af573c6e738db33e0
Wasabi ワサビ /°\  <3478932+111a5ab1@users.noreply.github.com>
2024-05-12 14:07:02 +1000

    doc: fix syntax and remove unsupported blocks
    Signed-off-by: Wasabi ワサビ /°\  <3478932+111a5ab1@users.noreply.github.com>

--- website/docs/language/state/examples/encryption/configuration.sh
+++ website/docs/language/state/examples/encryption/configuration.sh
@@ -1,28 +1,25 @@
 TF_ENCRYPTION=$(cat <<EOF
-terraform {
-  encryption {
-    key_provider "some_key_provider" "some_name" {
-      # Key provider options here
-    }
-
-    method "some_method" "some_method_name" {
-      # Method options here
-      keys = key_provider.some_key_provider.some_name
-    }
-
-    state {
-      # Encryption/decryption for state data
-      method = method.some_method.some_method_name
-    }
-
-    plan {
-      # Encryption/decryption for plan data
-      method = method.some_method.some_method_name
-    }
-
-    remote_state_data_sources {
-      # See below
-    }
-  }
+key_provider "some_key_provider" "some_name" {
+  # Key provider options here
 }
-EOF)
+
+method "some_method" "some_method_name" {
+  # Method options here
+  keys = key_provider.some_key_provider.some_name
+}
+
+state {
+  # Encryption/decryption for state data
+  method = method.some_method.some_method_name
+}
+
+plan {
+  # Encryption/decryption for plan data
+  method = method.some_method.some_method_name
+}
+
+remote_state_data_sources {
+  # See below
+}
+EOF
+)
