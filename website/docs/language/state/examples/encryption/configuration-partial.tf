2d723daccff2c49cb34e7f5bfb30ca11e0b6ea78
Wasabi ワサビ /°\  <3478932+111a5ab1@users.noreply.github.com>
2024-05-12 14:09:33 +1000

    doc: partial encryption config example
    Signed-off-by: Wasabi ワサビ /°\  <3478932+111a5ab1@users.noreply.github.com>

---
+++ website/docs/language/state/examples/encryption/configuration-partial.tf
@@ -0,0 +1,22 @@
+terraform {
+  encryption {
+    method "some_method" "some_method_name" {
+      # Method options here
+      keys = key_provider.some_key_provider.some_name
+    }
+
+    state {
+      # Encryption/decryption for state data
+      method = method.some_method.some_method_name
+    }
+
+    plan {
+      # Encryption/decryption for plan data
+      method = method.some_method.some_method_name
+    }
+
+    remote_state_data_sources {
+      # See below
+    }
+  }
+}
