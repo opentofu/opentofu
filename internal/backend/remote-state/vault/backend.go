2301c7bce881cc80ed58597875477fc37e6c43a9
Matthew John <matthew@dockstudios.co.uk>
2024-05-25 08:09:22 +0100

    Update vault backend for configurations specific to vault

--- internal/backend/remote-state/vault/backend.go
+++ internal/backend/remote-state/vault/backend.go
@@ -8,10 +8,9 @@ package consul
 import (
 	"context"
 	"net"
-	"strings"
 	"time"
 
-	consulapi "github.com/hashicorp/consul/api"
+	vaultapi "github.com/hashicorp/vault/api"
 	"github.com/opentofu/opentofu/internal/backend"
 	"github.com/opentofu/opentofu/internal/encryption"
 	"github.com/opentofu/opentofu/internal/legacy/helper/schema"
@@ -21,46 +20,38 @@ import (
 func New(enc encryption.StateEncryption) backend.Backend {
 	s := &schema.Backend{
 		Schema: map[string]*schema.Schema{
-			"path": &schema.Schema{
+			"mount": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "Path to store state in Consul",
+				Description: "KVv2 mount name in Vault",
 			},
 
-			"access_token": &schema.Schema{
+			"name": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Secret base path to store state in Vault",
+			},
+
+			"vault_token": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
-				Description: "Access token for a Consul ACL",
+				Description: "Token for Vault",
 				Default:     "", // To prevent input
 			},
 
 			"address": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
-				Description: "Address to the Consul Cluster",
+				Description: "HTTP URL to the Consul Cluster",
 				Default:     "", // To prevent input
 			},
 
-			"scheme": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Scheme to communicate to Consul with",
-				Default:     "", // To prevent input
-			},
-
-			"datacenter": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Datacenter to communicate with",
-				Default:     "", // To prevent input
-			},
-
-			"http_auth": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "HTTP Auth in the format of 'username:password'",
-				Default:     "", // To prevent input
-			},
+			// "scheme": &schema.Schema{
+			// 	Type:        schema.TypeString,
+			// 	Optional:    true,
+			// 	Description: "Scheme to communicate to Consul with",
+			// 	Default:     "", // To prevent input
+			// },
 
 			"gzip": &schema.Schema{
 				Type:        schema.TypeBool,
@@ -83,19 +74,19 @@ func New(enc encryption.StateEncryption) backend.Backend {
 				DefaultFunc: schema.EnvDefaultFunc("CONSUL_CACERT", ""),
 			},
 
-			"cert_file": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "A path to a PEM-encoded certificate provided to the remote agent; requires use of key_file.",
-				DefaultFunc: schema.EnvDefaultFunc("CONSUL_CLIENT_CERT", ""),
-			},
+			// "cert_file": &schema.Schema{
+			// 	Type:        schema.TypeString,
+			// 	Optional:    true,
+			// 	Description: "A path to a PEM-encoded certificate provided to the remote agent; requires use of key_file.",
+			// 	DefaultFunc: schema.EnvDefaultFunc("CONSUL_CLIENT_CERT", ""),
+			// },
 
-			"key_file": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "A path to a PEM-encoded private key, required if cert_file is specified.",
-				DefaultFunc: schema.EnvDefaultFunc("CONSUL_CLIENT_KEY", ""),
-			},
+			// "key_file": &schema.Schema{
+			// 	Type:        schema.TypeString,
+			// 	Optional:    true,
+			// 	Description: "A path to a PEM-encoded private key, required if cert_file is specified.",
+			// 	DefaultFunc: schema.EnvDefaultFunc("CONSUL_CLIENT_KEY", ""),
+			// },
 		},
 	}
 
@@ -109,9 +100,11 @@ type Backend struct {
 	encryption encryption.StateEncryption
 
 	// The fields below are set from configure
-	client     *consulapi.Client
+	client     *vaultapi.Client
 	configData *schema.ResourceData
 	lock       bool
+	token      string
+	mount      string
 }
 
 func (b *Backend) configure(ctx context.Context) error {
@@ -121,56 +114,38 @@ func (b *Backend) configure(ctx context.Context) error {
 	// Store the lock information
 	b.lock = b.configData.Get("lock").(bool)
 
+	b.mount = b.configData.Get("mount").(string)
+
 	data := b.configData
 
 	// Configure the client
-	config := consulapi.DefaultConfig()
+	config := vaultapi.DefaultConfig()
 
 	// replace the default Transport Dialer to reduce the KeepAlive
-	config.Transport.DialContext = dialContext
+	// config.Transport.DialContext = dialContext
 
-	if v, ok := data.GetOk("access_token"); ok && v.(string) != "" {
-		config.Token = v.(string)
+	if v, ok := data.GetOk("vault_token"); ok && v.(string) != "" {
+		b.token = v.(string)
 	}
 	if v, ok := data.GetOk("address"); ok && v.(string) != "" {
 		config.Address = v.(string)
 	}
-	if v, ok := data.GetOk("scheme"); ok && v.(string) != "" {
-		config.Scheme = v.(string)
-	}
-	if v, ok := data.GetOk("datacenter"); ok && v.(string) != "" {
-		config.Datacenter = v.(string)
-	}
 
-	if v, ok := data.GetOk("ca_file"); ok && v.(string) != "" {
-		config.TLSConfig.CAFile = v.(string)
-	}
-	if v, ok := data.GetOk("cert_file"); ok && v.(string) != "" {
-		config.TLSConfig.CertFile = v.(string)
-	}
-	if v, ok := data.GetOk("key_file"); ok && v.(string) != "" {
-		config.TLSConfig.KeyFile = v.(string)
-	}
+	// if v, ok := data.GetOk("datacenter"); ok && v.(string) != "" {
+	// 	config.Datacenter = v.(string)
+	// }
 
-	if v, ok := data.GetOk("http_auth"); ok && v.(string) != "" {
-		auth := v.(string)
+	// if v, ok := data.GetOk("ca_file"); ok && v.(string) != "" {
+	// 	config.TLSConfig.CAFile = v.(string)
+	// }
+	// if v, ok := data.GetOk("cert_file"); ok && v.(string) != "" {
+	// 	config.TLSConfig.CertFile = v.(string)
+	// }
+	// if v, ok := data.GetOk("key_file"); ok && v.(string) != "" {
+	// 	config.TLSConfig.KeyFile = v.(string)
+	// }
 
-		var username, password string
-		if strings.Contains(auth, ":") {
-			split := strings.SplitN(auth, ":", 2)
-			username = split[0]
-			password = split[1]
-		} else {
-			username = auth
-		}
-
-		config.HttpAuth = &consulapi.HttpBasicAuth{
-			Username: username,
-			Password: password,
-		}
-	}
-
-	client, err := consulapi.NewClient(config)
+	client, err := vaultapi.NewClient(config)
 	if err != nil {
 		return err
 	}
