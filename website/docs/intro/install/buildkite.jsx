7b0e9c19b9d1c8785abb9745bf9c547d0190e501
Wei <zxc37984@gmail.com>
2024-05-29 19:32:34 +0800

    fix: slack link and `BuildKite` component (#1681)
    Signed-off-by: Wei Hong <zxc37984@gmail.com>
    Signed-off-by: David Sims <simsdj82@gmail.com>

--- website/docs/intro/install/buildkite.jsx
+++ website/docs/intro/install/buildkite.jsx
@@ -8,8 +8,21 @@
 import React from 'react';
 import BuildKiteSVG from './buildkite.svg'
 
-export default ({children, color}) => (
-    <p style={{textAlign: "center", padding: "1.5rem"}}><a href={"https://buildkite.com"} style={{display: "block", color: "#fff", textDecoration: "none"}}>
-        Thank you to <BuildKiteSVG style={{maxWidth: "50%", marginLeft: "auto", marginRight: "auto"}}/> for sponsoring the OpenTofu package hosting.
-    </a></p>
-);
+const BuildKite = () => {
+    return (
+      <p style={{ textAlign: "center", padding: "1.5rem" }}>
+        <a
+          href={"https://buildkite.com"}
+          style={{ display: "block", color: "#fff", textDecoration: "none" }}
+        >
+          Thank you to{" "}
+          <BuildKiteSVG
+            style={{ maxWidth: "50%", marginLeft: "auto", marginRight: "auto" }}
+          />{" "}
+          for sponsoring the OpenTofu package hosting.
+        </a>
+      </p>
+    );
+};
+
+export default BuildKite;
