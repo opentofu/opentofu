df8a773680a64f9f847a84e9ce884c4ec93500e2
Christian Mesh <christianmesh1@gmail.com>
2024-05-29 09:23:28 -0400

    [backport 1.6] fix: slack link and `BuildKite` component (#1681) (#1693)
    Signed-off-by: Wei Hong <zxc37984@gmail.com>
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>
    Co-authored-by: Wei <zxc37984@gmail.com>

--- website/docs/intro/install/buildkite.jsx
+++ website/docs/intro/install/buildkite.jsx
@@ -1,8 +1,28 @@
+/**
+ * Copyright (c) The OpenTofu Authors
+ * SPDX-License-Identifier: MPL-2.0
+ * Copyright (c) 2023 HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import React from 'react';
 import BuildKiteSVG from './buildkite.svg'
 
-export default ({children, color}) => (
-    <p style={{textAlign: "center", padding: "1.5rem"}}><a href={"https://buildkite.com"} style={{display: "block", color: "#fff", textDecoration: "none"}}>
-        Thank you to <BuildKiteSVG style={{maxWidth: "50%", marginLeft: "auto", marginRight: "auto"}}/> for sponsoring the OpenTofu package hosting.
-    </a></p>
-);
+const BuildKite = () => {
+    return (
+      <p style={{ textAlign: "center", padding: "1.5rem" }}>
+        <a
+          href={"https://buildkite.com"}
+          style={{ display: "block", color: "#fff", textDecoration: "none" }}
+        >
+          Thank you to{" "}
+          <BuildKiteSVG
+            style={{ maxWidth: "50%", marginLeft: "auto", marginRight: "auto" }}
+          />{" "}
+          for sponsoring the OpenTofu package hosting.
+        </a>
+      </p>
+    );
+};
+
+export default BuildKite;
