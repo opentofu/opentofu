704efe9423e04f53ab965bb34337d7c65f772849
ollevche <ollevche@gmail.com>
2024-05-31 16:21:05 +0300

    remove copyright_headers.go
    Signed-off-by: ollevche <ollevche@gmail.com>

---
+++ scripts/add-copyright-headers.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Copyright (c) The OpenTofu Authors
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (c) 2023 HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+go run github.com/hashicorp/copywrite headers
