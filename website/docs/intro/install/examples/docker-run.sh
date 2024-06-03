40afcdd467fb12d0387d2ed6329c771301f4fdfd
eduardo <eduardoslafiq@gmail.com>
2024-06-04 00:23:41 +0100

    (docs): update 'Use OpenTofu as Docker Image' documentation
    - Update 'Usage' topic including a tutorial to run your first 'init' command via Docker.
    
    Refs: https://github.com/opentofu/opentofu/issues/1556
    Signed-off-by: eduardo <eduardoslafiq@gmail.com>

--- website/docs/intro/install/examples/docker-run.sh
+++ website/docs/intro/install/examples/docker-run.sh
@@ -1,5 +1,20 @@
+# Init providers plugins
 docker run \
     --workdir=/srv/workspace \
     --mount type=bind,source=.,target=/srv/workspace \
     ghcr.io/opentofu/opentofu:latest \
     init
+
+# Creating plan file
+docker run \
+    --workdir=/srv/workspace \
+    --mount type=bind,source=.,target=/srv/workspace \
+    ghcr.io/opentofu/opentofu:latest \
+    plan -out=main.plan
+
+# Applying plan file
+docker run \
+    --workdir=/srv/workspace \
+    --mount type=bind,source=.,target=/srv/workspace \
+    ghcr.io/opentofu/opentofu:latest \
+    apply "/srv/workspace/main.plan"
