9f0ab9e0bedc5e77a28d06942aa69dc0da59c275
David Sims <simsdj82@gmail.com>
2024-06-13 20:14:44 +1000

    Unit tests for updated severity code
    Signed-off-by: David Sims <simsdj82@gmail.com>

---
+++ internal/tfdiags/severity_test.go
@@ -0,0 +1,56 @@
+package tfdiags
+
+import (
+	"fmt"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewSeverity(t *testing.T) {
+	testCases := []struct {
+		name         string
+		input        SeverityLevel
+		expected     SeverityLevel
+		pedanticMode bool
+	}{
+		{"normal warning", WarningLevel, WarningLevel, false},
+		{"pedantic warning", WarningLevel, ErrorLevel, true},
+		{"normal error", ErrorLevel, ErrorLevel, false},
+		{"pedantic error", ErrorLevel, ErrorLevel, true},
+	}
+
+	for _, tc := range testCases{
+		t.Run(tc.name, func (t *testing.T){
+			PedanticMode = tc.pedanticMode
+			assert.Equal(t, tc.expected, NewSeverity(tc.input).SeverityLevel)
+		})
+	}
+
+	// Reset pedantic mode to stop chances of interfering with other tests
+	PedanticMode = true
+}
+
+func TestSeverityToHCL(t *testing.T) {
+	testCases := []struct {
+		name         string
+		input        SeverityLevel
+		expected     hcl.DiagnosticSeverity
+		pedanticMode bool
+	}{
+		{"normal hcl warning", WarningLevel, hcl.DiagWarning, false},
+		{"pedantic hcl warning", WarningLevel, hcl.DiagError, true},
+		{"normal hcl error", ErrorLevel, hcl.DiagError, false},
+		{"pedantic hcl error", ErrorLevel, hcl.DiagError, true},
+	}
+
+	for _, tc := range testCases{
+		t.Run(tc.name, func (t *testing.T){
+			PedanticMode = tc.pedanticMode
+			assert.Equal(t, tc.expected, NewSeverity(tc.input).ToHCL())
+		})
+	}
+
+	// Reset pedantic mode to stop chances of interfering with other tests
+	PedanticMode = false
+}
