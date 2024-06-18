463426f446b86b85ccba21592453855f36fc2adf
Janos <86970079+janosdebugs@users.noreply.github.com>
2024-06-18 15:54:52 +0200

    Added missing license header
    Signed-off-by: Janos <86970079+janosdebugs@users.noreply.github.com>

--- internal/testutils/must.go
+++ internal/testutils/must.go
@@ -1,3 +1,8 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package testutils
 
 // Must turns an error into a panic. Use for tests only.
