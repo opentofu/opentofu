b3fd90099abfad6112b8b5b0bfd3ffcec43a5f7a
David Sims <simsdj82@gmail.com>
2024-06-26 22:56:28 +1000

    Unit testing for parse global options
    Signed-off-by: David Sims <simsdj82@gmail.com>

--- cmd/tofu/main_test.go
+++ cmd/tofu/main_test.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -367,4 +368,40 @@ func TestMkConfigDir_noparent(t *testing.T) {
 	if err.Error() != expectedError {
 		t.Fatalf("Expected error: %s, but got: %v", expectedError, err)
 	}
+}
+
+func TestParseGlobalOptions(t *testing.T) {
+	testCases := []struct {
+		name     string
+		args     []string
+		expected map[string]string
+	}{
+		{"positive tc chdir", []string{"-chdir=target", "plan", "-state=file.tfstate"}, map[string]string{"chdir": "target"}},
+		{"positive tc 1 version", []string{"-version", "plan", "-state=file.tfstate"}, map[string]string{"version": ""}},
+		{"positive tc 2 version", []string{"-v", "plan", "-state=file.tfstate"}, map[string]string{"version": ""}},
+		{"positive tc 3 version", []string{"--version", "plan", "-state=file.tfstate"}, map[string]string{"version": ""}},
+		{"positive tc 4 version", []string{"plan", "-state=file.tfstate", "-version"}, map[string]string{"version": ""}},
+		{"positive tc 5 version", []string{"plan", "-state=file.tfstate", "-v"}, map[string]string{"version": ""}},
+		{"positive tc 6 version", []string{"plan", "-state=file.tfstate", "--version"}, map[string]string{"version": ""}},
+		{"positive tc random", []string{"-random", "plan", "-state=file.tfstate"}, map[string]string{"random": ""}},
+		{"positive tc multi", []string{"-help", "-version", "plan", "-state=file.tfstate"}, map[string]string{"help": "", "version": ""}},
+		{"positive tc omitted", []string{"plan", "-state=file.tfstate"}, map[string]string{}},
+	}
+
+	var opts map[string]string
+	var err error
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			opts, err = parseGlobalOptions(tc.args)
+			assert.EqualValues(t, tc.expected, opts)
+			assert.Nil(t, err)
+		})
+	}
+
+	t.Run("negative tc chdir", func(t *testing.T) {
+		opts, err = parseGlobalOptions([]string{"-chdir", "plan", "-state=file.tfstate"})
+		assert.Nil(t, opts)
+		assert.Error(t, err)
+	})
 }
