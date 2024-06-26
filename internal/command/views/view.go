4156e15befe932dedc4955f7efb560857c6abc64
David Sims <simsdj82@gmail.com>
2024-06-26 23:43:11 +1000

    Additional work on pedantic mode
    Signed-off-by: David Sims <simsdj82@gmail.com>

--- internal/command/views/view.go
+++ internal/command/views/view.go
@@ -38,6 +38,9 @@ type View struct {
 	// will be dereferenced as late as possible when rendering diagnostics in
 	// order to access the config loader cache.
 	configSources func() map[string][]byte
+
+	// Pedantic mode is used to treat warnings as errors
+	pedanticMode bool
 }
 
 // Initialize a View with the given streams, a disabled colorize object, and a
@@ -75,6 +78,7 @@ func (v *View) Configure(view *arguments.View) {
 	v.colorize.Disable = view.NoColor
 	v.compactWarnings = view.CompactWarnings
 	v.concise = view.Concise
+	v.pedanticMode = view.PedanticMode
 }
 
 // SetConfigSources overrides the default no-op callback with a new function
