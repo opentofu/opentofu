afc8d2f739b4d6cef5691678e745cd88f072a968
James Humphries <james@james-humphries.co.uk>
2024-07-03 12:59:18 +0100

    moved combinePathValueMarks to marks.go
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/tofu/marks.go
+++ internal/tofu/marks.go
@@ -42,3 +42,48 @@ func marksEqual(a, b []cty.PathValueMarks) bool {
 
 	return true
 }
+
+// combinePathValueMarks will combine the marks from two sets of marks with paths, ensuring that we don't duplicate marks
+// for the same path, but instead combine the marks for the same path
+// This ensures that we don't lose user marks when combining 2 different sets of marks for the same path
+func combinePathValueMarks(marks []cty.PathValueMarks, other []cty.PathValueMarks) []cty.PathValueMarks {
+	// TODO: Find a nice home for this method so it can be used in other places too
+
+	// skip some work if we don't have any marks in either of the lists
+	if len(marks) == 0 {
+		return other
+	}
+	if len(other) == 0 {
+		return marks
+	}
+
+	combined := make([]cty.PathValueMarks, 0, len(marks))
+	// construct the initial set of marks
+	combined = append(combined, marks...)
+
+	// check if we've already inserted this by looping over and calling .Equals().
+	// This isn't so nice but there is no nice comparison for cty.PathValueMarks
+	// so we have to do it this way
+	for _, mark := range other {
+		exists := false
+		for i, existing := range combined {
+			if mark.Path.Equals(existing.Path) {
+				// if we found a matching path, we should combine the marks and update the existing item
+				dupe := existing
+				for k, v := range mark.Marks {
+					dupe.Marks[k] = v
+				}
+				combined[i] = dupe
+				exists = true
+				break
+			}
+		}
+		// Otherwise we haven't seen this path before, so we should add it to the list
+		// no merging required
+		if !exists {
+			combined = append(combined, mark)
+		}
+	}
+
+	return combined
+}
--- internal/tofu/marks_test.go
+++ internal/tofu/marks_test.go
@@ -9,8 +9,9 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/opentofu/opentofu/internal/lang/marks"
 	"github.com/zclconf/go-cty/cty"
+
+	"github.com/opentofu/opentofu/internal/lang/marks"
 )
 
 func TestMarksEqual(t *testing.T) {
@@ -104,6 +105,101 @@ func TestMarksEqual(t *testing.T) {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			if marksEqual(tc.a, tc.b) != tc.equal {
 				t.Fatalf("marksEqual(\n%#v,\n%#v,\n) != %t\n", tc.a, tc.b, tc.equal)
+			}
+		})
+	}
+}
+
+func TestCombinePathValueMarks(t *testing.T) {
+	paths := map[string]cty.PathValueMarks{
+		"a.b": {
+			Path:  cty.Path{cty.GetAttrStep{Name: "a"}, cty.GetAttrStep{Name: "b"}},
+			Marks: cty.NewValueMarks(marks.Sensitive),
+		},
+		"a.c": {
+			Path:  cty.Path{cty.GetAttrStep{Name: "a"}, cty.GetAttrStep{Name: "c"}},
+			Marks: cty.NewValueMarks(marks.Sensitive),
+		},
+		"[0]": {
+			Path:  cty.Path{cty.IndexStep{Key: cty.NumberIntVal(0)}},
+			Marks: cty.NewValueMarks("a"),
+		},
+		"a.b<alt>": {
+			Path:  cty.Path{cty.GetAttrStep{Name: "a"}, cty.GetAttrStep{Name: "b"}},
+			Marks: cty.NewValueMarks("a"),
+		},
+	}
+
+	tests := []struct {
+		name string
+		LHS  []cty.PathValueMarks
+		RHS  []cty.PathValueMarks
+		Want []cty.PathValueMarks
+	}{
+		{
+			name: "no marks",
+			LHS:  []cty.PathValueMarks{},
+			RHS:  []cty.PathValueMarks{},
+			Want: []cty.PathValueMarks{},
+		},
+		{
+			name: "one mark",
+			LHS:  []cty.PathValueMarks{paths["a.b"]},
+			RHS:  []cty.PathValueMarks{},
+			Want: []cty.PathValueMarks{paths["a.b"]},
+		},
+		{
+			name: "one overlapping mark",
+			LHS:  []cty.PathValueMarks{paths["a.b"]},
+			RHS:  []cty.PathValueMarks{paths["a.b"]},
+			Want: []cty.PathValueMarks{paths["a.b"]},
+		},
+		{
+			name: "one non-overlapping mark",
+			LHS:  []cty.PathValueMarks{paths["a.b"]},
+			RHS:  []cty.PathValueMarks{paths["a.c"]},
+			Want: []cty.PathValueMarks{paths["a.b"], paths["a.c"]},
+		},
+		{
+			name: "one overlapping and two non-overlapping marks",
+			LHS:  []cty.PathValueMarks{paths["a.b"], paths["a.c"], paths["[0]"]},
+			RHS:  []cty.PathValueMarks{paths["a.c"]},
+			Want: []cty.PathValueMarks{paths["a.b"], paths["a.c"], paths["[0]"]},
+		},
+		{
+			name: "one overlapping mark with different values",
+			LHS: []cty.PathValueMarks{
+				{
+					Path:  cty.Path{cty.GetAttrStep{Name: "a"}, cty.GetAttrStep{Name: "b"}},
+					Marks: cty.NewValueMarks(marks.Sensitive),
+				},
+			},
+			RHS: []cty.PathValueMarks{
+				{
+					Path:  cty.Path{cty.GetAttrStep{Name: "a"}, cty.GetAttrStep{Name: "b"}},
+					Marks: cty.NewValueMarks("OTHERMARK"),
+				},
+			},
+			Want: []cty.PathValueMarks{
+				{
+					Path:  cty.Path{cty.GetAttrStep{Name: "a"}, cty.GetAttrStep{Name: "b"}},
+					Marks: cty.NewValueMarks(marks.Sensitive, "OTHERMARK"),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := combinePathValueMarks(test.LHS, test.RHS)
+			if len(got) != len(test.Want) {
+				t.Fatalf("incorrect result length\ngot:  %#v\nwant: %#v", got, test.Want)
+			}
+
+			for i, want := range test.Want {
+				if !got[i].Equal(want) {
+					t.Errorf("incorrect result\nindex: %d\ngot:  %#v\nwant: %#v", i, got[i], want)
+				}
 			}
 		})
 	}
