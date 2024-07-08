4d64972ed2d5d64964622b1b84a10c3005c26672
David Sims <simsdj82@gmail.com>
2024-07-08 21:33:40 +1000

    Additional work on unit tests
    Signed-off-by: David Sims <simsdj82@gmail.com>

---
+++ internal/command/views/view_test.go
@@ -0,0 +1,45 @@
+package views
+
+import (
+	"fmt"
+	"github.com/opentofu/opentofu/internal/terminal"
+	"github.com/opentofu/opentofu/internal/tfdiags"
+	"io"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestDiagnosticsPedanticMode(t *testing.T) {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("error setting up reader and writer: %s", err)
+	}
+
+	streams := &terminal.Streams{
+		Stderr: &terminal.OutputStream{
+			File: writer,
+		},
+	}
+
+	view := NewView(streams)
+	view.PedanticMode = true
+
+	diags := tfdiags.Diagnostics{tfdiags.SimpleWarning("TEST")}
+	view.Diagnostics(diags)
+
+	writer.Close()
+
+	out, err := io.ReadAll(reader)
+	if err != nil {
+		t.Fatalf("error reading from reader: %s", err)
+	}
+
+	got := strings.TrimSpace(string(out))
+	want := fmt.Sprintf("%s: TEST", tfdiags.Error)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("expected: %v got: %v", want, got)
+	}
+}
