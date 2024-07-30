2246847eab4667e7bd9ba548b8e78295a7521dcc
ollevche <ollevche@gmail.com>
2024-07-30 15:32:39 +0300

    fix more compile time errors (temp hacks)
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/tofu/transform_provider.go
+++ internal/tofu/transform_provider.go
@@ -792,7 +792,7 @@ func (t *ProviderConfigTransformer) addProxyProviders(g *Graph, c *configs.Confi
 		fullParentAddr := addrs.AbsProviderConfig{
 			Provider: fqn,
 			Module:   parentPath,
-			Alias:    pair.InParent.Addr().Alias,
+			Alias:    pair.InParentTODO().Addr().Alias,
 		}
 
 		fullName := fullAddr.String()
