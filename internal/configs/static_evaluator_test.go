90bc538525077b74e4fbe1650c830356e6f32df5
ollevche <ollevche@gmail.com>
2024-09-25 15:08:17 +0300

    remove redundant nolint comment
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/configs/static_evaluator_test.go
+++ internal/configs/static_evaluator_test.go
@@ -16,8 +16,6 @@ import (
 )
 
 // This exercises most of the logic in StaticEvaluator and staticScopeData
-//
-//nolint:cyclop // it's a test
 func TestStaticEvaluator_Evaluate(t *testing.T) {
 	// Synthetic file for building test components
 	testData := `
