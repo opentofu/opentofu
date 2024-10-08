856e301169a71f68053349da0c6c148512909e0c
ollevche <ollevche@gmail.com>
2024-10-08 18:15:14 +0300

    rm TODO / update comment
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/tofu/node_resource_import.go
+++ internal/tofu/node_resource_import.go
@@ -32,9 +32,8 @@ type graphNodeImportState struct {
 }
 
 func (n *graphNodeImportState) SetPotentialProviders(potentialProviders []distinguishableProvider) {
-	// Technically we shouldn't get here as we don't support import with for_each on providers in resources / modules
-	// So we should panic here (ensure we have a code that validates it somewhere else) / implement a proper user error.
-	//TODO Ronny - don't forget to handle this scenario ^
+	// Technically we shouldn't get here as we don't support import with for_each on providers in resources / modules.
+	// We should panic here since we validated provider match between import/resource previously on configuration parsing.
 	panic("implement me")
 }
 
