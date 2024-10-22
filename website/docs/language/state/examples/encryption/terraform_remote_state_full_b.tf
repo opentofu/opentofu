c3cc2e423a9a462703bc62c17e7f57af64356d78
AbstractionFactory <179820029+abstractionfactory@users.noreply.github.com>
2024-10-22 11:10:37 +0200

    Adding comprehensive example for remote state data sources
    Signed-off-by: AbstractionFactory <179820029+abstractionfactory@users.noreply.github.com>

---
+++ website/docs/language/state/examples/encryption/terraform_remote_state_full_b.tf
@@ -0,0 +1,32 @@
+terraform {
+  encryption {
+    # Note that the name of the key here is different:
+    key_provider "pbkdf2" "mykeyrenamed" {
+      passphrase             = "OpenTofu has encryption"
+      # Note the fixed encryption_metadata_key here:
+      encrypted_metadata_key = "certificates"
+    }
+    method "aes_gcm" "mymethod" {
+      keys = key_provider.pbkdf2.mykeyrenamed
+    }
+    remote_state_data_sources {
+      default {
+        method = method.aes_gcm.mymethod
+      }
+    }
+  }
+}
+
+
+data "terraform_remote_state" "cert" {
+  backend = "local"
+  config = {
+    # Refer to the other project here:
+    path = "../certificates/terraform.tfstate"
+  }
+}
+
+output "cert" {
+  # Use data from the other project by referencing it as follows:
+  value = data.terraform_remote_state.cert.outputs.cert_pem
+}
