d5a249b03267e6f0c018877ed4fc3f804bb09efa
Eunan <ehardy2000@googlemail.com>
2024-11-14 01:00:33 +0000

    basic oci client complete
    Signed-off-by: Eunan <25510708+eunanio@users.noreply.github.com>

--- internal/ociclient/client.go
+++ internal/ociclient/client.go
@@ -1,7 +1,9 @@
 package ociclient
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -79,7 +81,7 @@ func (c *Client) PullBlob(opts PullBlobOptions) ([]byte, error) {
 
 	if resp.StatusCode != 200 {
 		if resp.StatusCode == http.StatusUnauthorized {
-			return nil, fmt.Errorf("unauthorized, please use nori login to authenticate")
+			return nil, fmt.Errorf("unauthorized, please use docker login to authenticate")
 		}
 		return nil, fmt.Errorf("failed to pull blob: %s", resp.Status)
 	}
@@ -98,14 +100,164 @@ func (c *Client) PullManifest(opts PullManifestOptions) (*spec.Manifest, error) 
 	if err != nil {
 		return nil, fmt.Errorf("error parsing ref: %w", err)
 	}
-	return nil, nil
+
+	endpoint := getManifestEndpont(ref, false)
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Accept", spec.MediaTypeImageManifest)
+	if c.Credentials != nil {
+		req.Header.Add("Authorization", c.Credentials.encoded)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		if resp.StatusCode == http.StatusUnauthorized {
+			return nil, fmt.Errorf("unauthorized, please use nori login to authenticate")
+		}
+
+		return nil, fmt.Errorf("cannot to pull manifest: %s", resp.Status)
+	}
+
+	manifestBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	manifest := &spec.Manifest{}
+	err = json.Unmarshal(manifestBytes, manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	return manifest, nil
 }
 
 func (c *Client) PushBlob(opts PushBlobOptions) error {
+	ref, err := ParseRef(opts.Ref)
+	if err != nil {
+		return err
+	}
+	protocol := getHTTPProtocol(opts.Insecure)
+	var endpoint string
+	if ref.Namespace != "" {
+		endpoint = fmt.Sprintf("%s://%s/v2/%s/%s/blobs/uploads/", protocol, ref.Host, ref.Namespace, ref.Name)
+	} else {
+		endpoint = fmt.Sprintf("%s://%s/v2/%s/blobs/uploads/", protocol, ref.Host, ref.Name)
+	}
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %s", err.Error())
+	}
+
+	if c.Credentials != nil {
+		req.Header.Add("Authorization", c.Credentials.encoded)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %s", err.Error())
+	}
+
+	if resp.StatusCode != 202 {
+		return fmt.Errorf("failed to push blob: %s", resp.Status)
+	}
+
+	location := resp.Header.Get("Location")
+	req, err = http.NewRequest("PUT", location, bytes.NewReader(opts.Blob))
+	if err != nil {
+		return fmt.Errorf("error uploading blob: %s", err.Error())
+	}
+
+	req.Header.Add("Content-Type", "application/octet-stream")
+	req.Header.Add("Content-Length", fmt.Sprintf("%d", len(opts.Blob)))
+	query := req.URL.Query()
+	query.Add("digest", "")
+	req.URL.RawQuery = query.Encode()
+
+	if c.Credentials != nil {
+		req.Header.Add("Authorization", c.Credentials.encoded)
+	}
+
+	resp, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 201 {
+		if resp.StatusCode == http.StatusUnauthorized {
+			return fmt.Errorf("unauthorized, please use docker login to authenticate")
+		}
+		return fmt.Errorf("failed to push blob: %s", resp.Status)
+	}
+
 	return nil
 }
 
 func (c *Client) PushManifest(opts PushManifestOptions) error {
+	ref, err := ParseRef(opts.Ref)
+	if err != nil {
+		return err
+	}
+	endpoint := getManifestEndpont(ref, opts.Insecure)
+	req, err := http.NewRequest("HEAD", endpoint, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %s", err.Error())
+	}
+
+	if c.Credentials != nil {
+		req.Header.Add("Authorization", c.Credentials.encoded)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %s", err.Error())
+	}
+
+	jsonBytes, err := json.Marshal(opts.Manifest)
+	if err != nil {
+		return fmt.Errorf("error marshalling manifest: %s", err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		uploadReq, err := http.NewRequest("PUT", endpoint, bytes.NewReader(jsonBytes))
+		if err != nil {
+			return fmt.Errorf("error creating request: %s", err.Error())
+		}
+
+		uploadReq.Header.Add("Content-Type", spec.MediaTypeImageManifest)
+		uploadReq.Header.Add("Content-Length", fmt.Sprintf("%d", len(jsonBytes)))
+
+		if c.Credentials != nil {
+			uploadReq.Header.Add("Authorization", c.Credentials.encoded)
+		}
+
+		resp, err = client.Do(uploadReq)
+		if err != nil {
+			return fmt.Errorf("error sending request: %s", err.Error())
+		}
+
+		if resp.StatusCode != 201 {
+			if resp.StatusCode == http.StatusUnauthorized {
+				return fmt.Errorf("unauthorized, please use nori login to authenticate")
+			}
+			return fmt.Errorf("failed to push manifest: %s", resp.Status)
+		}
+	}
+
 	return nil
 }
 
@@ -118,10 +270,19 @@ func getBlobEndpont(ref OciReference, digest string) string {
 	}
 }
 
-func getManifestEndpont(ref OciReference, digest string) string {
+func getManifestEndpont(ref OciReference, insecure bool) string {
+	protocol := getHTTPProtocol(insecure)
 	if ref.Namespace != "" {
-		return fmt.Sprintf("https://%s/%s/%s/v2/%s/%s/manifests/%s", ref.Host, ref.Namespace, ref.Name, ref.Version, ref.Name, ref.Version)
+		return fmt.Sprintf("%s://%s/v2/%s/%s/manifests/%s", protocol, ref.Host, ref.Namespace, ref.Name, ref.Version)
 	} else {
-		return fmt.Sprintf("https://%s/v2/%s/%s/manifests/%s", ref.Host, ref.Name, ref.Version, ref.Name, ref.Version)
+		return fmt.Sprintf("%s://%s/v2/%s/manifests/%s", protocol, ref.Host, ref.Name, ref.Version)
 	}
+}
+
+func getHTTPProtocol(insecure bool) string {
+	if insecure {
+		return "http"
+	}
+
+	return "https"
 }
