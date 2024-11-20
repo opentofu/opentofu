96d3e54ce4b859994099f48010d5c570aa560c30
Martin Atkins <mart@degeneration.co.uk>
2024-11-19 16:31:21 -0800

    getproviders: OCIRepository type
    Previously we were using a string consisting of a hostname and a name
    concatenated with a slash, but the OCI distribution spec doesn't define
    any such address syntax -- that's something we've essentially invented
    for OpenTofu, albeit with strong inspiration from Docker -- so we'll
    use a struct type to represent these addresses internally and then deal
    with parsing the slash-concatenated form at the same time as we're doing
    all of the other parsing, for consistency.
    
    This means that it's now the responsibility of whatever is dealing with
    the address template to also deal with the splitting of the hostname and
    the repository name.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/getproviders/oci_mirror_source.go
+++ internal/getproviders/oci_mirror_source.go
@@ -11,6 +11,7 @@ import (
 	"regexp"
 
 	"github.com/apparentlymart/go-versions/versions"
+	svchost "github.com/hashicorp/terraform-svchost"
 
 	"github.com/opentofu/opentofu/internal/addrs"
 	"github.com/opentofu/opentofu/internal/tfdiags"
@@ -21,14 +22,14 @@ import (
 // from OCI Distribution registries, selected by transforming the
 // requested provider source address to an OCI repository address.
 type OCIMirrorSource struct {
-	getRepositoryAddress func(providerAddr addrs.Provider) (string, tfdiags.Diagnostics)
+	getRepositoryAddress func(providerAddr addrs.Provider) (OCIRepository, tfdiags.Diagnostics)
 }
 
 var _ Source = (*OCIMirrorSource)(nil)
 
 // NewOCIMirrorSource constructs and returns a new OCI mirror source with
 // the given repository address generation callback.
-func NewOCIMirrorSource(repositoryAddressFunc func(providerAddr addrs.Provider) (string, tfdiags.Diagnostics)) *OCIMirrorSource {
+func NewOCIMirrorSource(repositoryAddressFunc func(providerAddr addrs.Provider) (OCIRepository, tfdiags.Diagnostics)) *OCIMirrorSource {
 	return &OCIMirrorSource{
 		getRepositoryAddress: repositoryAddressFunc,
 	}
@@ -71,7 +72,7 @@ func (o *OCIMirrorSource) ForDisplay(provider tfaddr.Provider) string {
 // repositoryAddress calls the getRepositoryAddress callback and then performs some
 // postprocessing on its result to make it more convenient to use in the other
 // methods of this type.
-func (o *OCIMirrorSource) repositoryAddress(providerAddr addrs.Provider) (string, error) {
+func (o *OCIMirrorSource) repositoryAddress(providerAddr addrs.Provider) (OCIRepository, error) {
 	ret, diags := o.getRepositoryAddress(providerAddr)
 	if diags.HasErrors() {
 		// This is an awkward situation where we have a diagnostics-based
@@ -79,11 +80,15 @@ func (o *OCIMirrorSource) repositoryAddress(providerAddr addrs.Provider) (string
 		// compromise and adapt the diagnostics into an error object.
 		// This means that if the function returns only warnings and
 		// no errors then those warnings will be lost. :(
-		return "", diags.Err()
+		return OCIRepository{}, diags.Err()
+	}
+
+	if !svchost.IsValid(ret.Hostname) {
+		return ret, fmt.Errorf("an OCI mirror's translation template transformed provider address %q into an OCI distribution registry address with invalid hostname %q", providerAddr, ret.Hostname)
 	}
 
 	// The result must be a valid name as defined in the OCI distribution specification.
-	if !ociDistributionRepoPattern.MatchString(ret) {
+	if !ociDistributionNamePattern.MatchString(ret.Name) {
 		// OpenTofu's provider address syntax permits a wider repertiore of
 		// Unicode characters than the OCI distribution name pattern allows,
 		// so a likely way to get here is to try to install a provider
@@ -94,27 +99,33 @@ func (o *OCIMirrorSource) repositoryAddress(providerAddr addrs.Provider) (string
 		// syntax is compatible enough with the OCI repository address syntax
 		// that we can use a string representation of the provider address
 		// with the same regular expression pattern.
-		if !ociDistributionRepoPattern.MatchString(providerAddr.String()) {
+		if !ociDistributionNamePattern.MatchString(providerAddr.Namespace + "/" + providerAddr.Type) {
 			// (TODO: Should the CLI configuration template for this offer some
 			// functions to help users transform a non-ASCII provider address
 			// segment into a reasonable ASCII equivalent? Non-ASCII provider
 			// namespaces and types are pretty rare, so we'll wait to see if
 			// that's needed.)
-			return ret, fmt.Errorf("requested provider address %q contains characters that are not valid in an OCI distribution repository name, so this provider cannot be installed from an OCI repository as %q", providerAddr, ret)
+			return ret, fmt.Errorf("requested provider address %q contains characters that are not valid in an OCI distribution repository name, so this provider cannot be installed from an OCI repository as %q", providerAddr, ret.Name)
 		}
 		// We'd get here if the invalidity was caused by a literal part
 		// of the template, regardless of the given provider address
 		// components.
-		return ret, fmt.Errorf("an OCI mirror's translation template transformed provider address %q into OCI distribution registry address %q, which is not valid OCI address syntax", providerAddr, ret)
+		return ret, fmt.Errorf("an OCI mirror's translation template transformed provider address %q into OCI distribution registry name %q, which is not valid OCI address syntax", providerAddr, ret.Name)
 	}
 
 	return ret, nil
 }
 
-// ociDistributionRepoPattern is a compiled regular expression pattern corresponding to the
-// "name" pattern as defined in the OCI distribution specification, following a hostname
-// with an optional port number.
-//
-// The hostname is not constrained to the rules defined in RFC 1123 because we intend to allow
-// IDNs that are not yet translated into Punycode.
-var ociDistributionRepoPattern = regexp.MustCompile(`^[^\:/]+(\:[0-9]+)?/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*(\/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*)*$`)
+// OCIRepository represents an OCI Distribution repository address.
+type OCIRepository struct {
+	Hostname string
+	Name     string
+}
+
+// ociDistributionNamePattern is a compiled regular expression pattern corresponding to the
+// "name" pattern as defined in the OCI distribution specification.
+var ociDistributionNamePattern = regexp.MustCompile(`^[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*(\/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*)*$`)
+
+func (r OCIRepository) String() string {
+	return r.Hostname + "/" + r.Name
+}
