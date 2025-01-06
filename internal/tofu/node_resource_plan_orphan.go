a763d7a07cc69de2cb732713c1e690ddbd8246d1
ollevche <ollevche@gmail.com>
2025-01-06 19:54:05 +0200

    fix: provider not initialized in some cases (mostly, deposed)
    Signed-off-by: ollevche <ollevche@gmail.com>

--- internal/tofu/node_resource_plan_orphan.go
+++ internal/tofu/node_resource_plan_orphan.go
@@ -63,6 +63,7 @@ func (n *NodePlannableResourceInstanceOrphan) Execute(ctx EvalContext, op walkOp
 		}
 		return n.managedResourceExecute(ctx)
 	case addrs.DataResourceMode:
+		// TODO/Oleksandr: should we resolve provider for data resources as well here?
 		return n.dataResourceExecute(ctx)
 	default:
 		panic(fmt.Errorf("unsupported resource mode %s", n.Config.Mode))
