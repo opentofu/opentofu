da33ac75ced9e7f5558f8943352298ec851b1695
Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>
2025-01-22 15:22:14 +0400

    Move transform arguments into a separate variable
    Signed-off-by: Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>

--- internal/tofu/node_resource_abstract.go
+++ internal/tofu/node_resource_abstract.go
@@ -602,24 +602,19 @@ func (n *NodeAbstractResourceInstance) readResourceInstanceStateDeposed(evalCtx 
 		return nil, diags.Append(fmt.Errorf("no schema available for %s while reading state; this is a bug in OpenTofu and should be reported", addr))
 	}
 	// prevAddr will match the newAddr if the resource wasn't moved (prevRunAddr checks move results)
-	if prevAddr := n.prevRunAddr(evalCtx); isResourceMovedToDifferentType(addr, prevAddr) {
-		src, diags = moveResourceState(stateTransformArgs{
-			currentAddr:          addr,
-			prevAddr:             prevAddr,
-			provider:             provider,
-			objectSrc:            src,
-			currentSchema:        schema,
-			currentSchemaVersion: currentVersion,
-		})
+	prevAddr := n.prevRunAddr(evalCtx)
+	transformArgs := stateTransformArgs{
+		currentAddr:          addr,
+		prevAddr:             prevAddr,
+		provider:             provider,
+		objectSrc:            src,
+		currentSchema:        schema,
+		currentSchemaVersion: currentVersion,
+	}
+	if isResourceMovedToDifferentType(addr, prevAddr) {
+		src, diags = moveResourceState(transformArgs)
 	} else {
-		src, diags = upgradeResourceState(stateTransformArgs{
-			currentAddr:          addr,
-			prevAddr:             prevAddr,
-			provider:             provider,
-			objectSrc:            src,
-			currentSchema:        schema,
-			currentSchemaVersion: currentVersion,
-		})
+		src, diags = upgradeResourceState(transformArgs)
 	}
 
 	if n.Config != nil {
