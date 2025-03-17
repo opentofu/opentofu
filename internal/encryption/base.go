9e44e1d382c0a611eda26b8762fa3b70bf00b74d
Klopklopi <76015884+Klopklopi@users.noreply.github.com>
2025-03-17 11:49:13 +0100

    Apply suggestions from code review
    Co-authored-by: Andrei Ciobanu <andreic9203@gmail.com>
    Signed-off-by: Klopklopi <76015884+Klopklopi@users.noreply.github.com>

--- internal/encryption/base.go
+++ internal/encryption/base.go
@@ -237,6 +237,7 @@ func (base *baseEncryption) decrypt(data []byte, validator func([]byte) error) (
 	//	sep = "\n"
 	//}
 
+       	errs = append([]error{fmt.Errorf("decryption failed for all provided methods")}, errs...)
 	errMessage := errors.Join(errs...).Error()
 
 	return nil, StatusUnknown, errors.New(errMessage)
