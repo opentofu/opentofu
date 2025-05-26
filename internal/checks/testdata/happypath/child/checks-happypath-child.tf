26b61ffc587494e9403568c2bf258f2a4ff72407
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2025-05-26 12:08:48 +0300

    Add support for the ephemeral resources pre and postcondition evaluation (#2844)
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/checks/testdata/happypath/child/checks-happypath-child.tf
+++ internal/checks/testdata/happypath/child/checks-happypath-child.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 resource "null_resource" "b" {
   lifecycle {
     precondition {
@@ -18,6 +26,27 @@ resource "null_resource" "c" {
   }
 }
 
+data "aws_s3_object" "foo" {
+  lifecycle {
+    precondition {
+      condition     = self.id == ""
+      error_message = "Impossible data."
+    }
+  }
+  bucket = "test-bucket"
+  key    = "test-key"
+}
+
+ephemeral "aws_secretsmanager_secret_version" "bar" {
+  lifecycle {
+    precondition {
+      condition     = self.id == ""
+      error_message = "Impossible ephemeral."
+    }
+  }
+  secret_id = "secret-manager-id"
+}
+
 output "b" {
   value = null_resource.b.id
 
@@ -26,4 +55,3 @@ output "b" {
     error_message = "B has no id."
   }
 }
-
