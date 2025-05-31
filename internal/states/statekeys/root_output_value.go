7d67bc1a6f876c98220db69ec0e4eb28479cc6f8
Martin Atkins <mart@degeneration.co.uk>
2025-05-30 17:38:16 -0700

    statekeys: Key generation and parsing for statestore
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

---
+++ internal/states/statekeys/root_output_value.go
@@ -0,0 +1,55 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package statekeys
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/opentofu/opentofu/internal/addrs"
+	"github.com/opentofu/opentofu/internal/states/statestore"
+)
+
+type RootModuleOutputValue struct {
+	name string
+}
+
+const rootModuleOutputValuePrefix = "output"
+
+func NewRootModuleOutputValue(addr addrs.AbsOutputValue) RootModuleOutputValue {
+	if !addr.Module.IsRoot() {
+		panic("NewRootModuleOutputValue with non-root output value")
+	}
+	return RootModuleOutputValue{name: addr.OutputValue.Name}
+}
+
+func rootModuleOutputValueFromStorage(raw string) (Key, error) {
+	nameStr, err := decodeBase32(raw)
+	if err != nil {
+		return nil, err
+	}
+	if !hclsyntax.ValidIdentifier(nameStr) {
+		return nil, fmt.Errorf("invalid output value name")
+	}
+	return RootModuleOutputValue{nameStr}, nil
+}
+
+// ForStorage implements Key.
+func (r RootModuleOutputValue) ForStorage() statestore.Key {
+	return makeStorageKey(rootModuleOutputValuePrefix, r.name)
+}
+
+func (r RootModuleOutputValue) Address() addrs.AbsOutputValue {
+	return addrs.AbsOutputValue{
+		Module: addrs.RootModuleInstance,
+		OutputValue: addrs.OutputValue{
+			Name: r.name,
+		},
+	}
+}
+
+// keySigil implements Key.
+func (r RootModuleOutputValue) keySigil() {}
