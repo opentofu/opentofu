061e3562bac0629082df808c5ea11b0b8762cfa9
Diogenes Fernandes <diofeher@gmail.com>
2025-07-14 16:28:31 -0300

    refactor: make jsonentities dependent of jsonformat/structured
    Signed-off-by: Diogenes Fernandes <diofeher@gmail.com>

--- internal/command/jsonentities/output.go
+++ internal/command/jsonentities/output.go
@@ -8,6 +8,9 @@ package jsonentities
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/opentofu/opentofu/internal/command/jsonformat/structured"
+	"github.com/opentofu/opentofu/internal/command/jsonformat/structured/attribute_path"
 )
 
 type Output struct {
@@ -22,4 +25,24 @@ type Outputs map[string]Output
 
 func (o Outputs) String() string {
 	return fmt.Sprintf("Outputs: %d", len(o))
+}
+
+// FromJsonViewsOutput unmarshals the raw values in the viewsjson.Output structs into
+// generic interface{} types that can be reasoned about.
+func FromJsonViewsOutput(output Output) structured.Change {
+	return structured.Change{
+		// We model resource formatting as NoOps.
+		Before: structured.UnmarshalGeneric(output.Value),
+		After:  structured.UnmarshalGeneric(output.Value),
+
+		// We have some sensitive values, but we don't have any unknown values.
+		Unknown:         false,
+		BeforeSensitive: output.Sensitive,
+		AfterSensitive:  output.Sensitive,
+
+		// We don't display replacement data for resources, and all attributes
+		// are relevant.
+		ReplacePaths:       attribute_path.Empty(false),
+		RelevantAttributes: attribute_path.AlwaysMatcher(),
+	}
 }
