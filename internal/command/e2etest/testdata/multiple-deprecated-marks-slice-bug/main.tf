47ea78bbc6e8390055dd5f5acae6ea202f52e3b7
James Humphries <james@james-humphries.co.uk>
2025-08-06 10:44:10 +0100

    added link to issue in e2e test
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/command/e2etest/testdata/multiple-deprecated-marks-slice-bug/main.tf
+++ internal/command/e2etest/testdata/multiple-deprecated-marks-slice-bug/main.tf
@@ -5,6 +5,7 @@ module "test" {
 # This creates a SINGLE value with MULTIPLE deprecated marks at different paths
 # Each field gets its own PathValueMark with ONLY a deprecation mark
 # This pattern triggers the slice modification bug in unmarkDeepWithPathsDeprecated
+# https://github.com/opentofu/opentofu/issues/3104
 locals {
   all_deprecated = {
     a = module.test.out1
