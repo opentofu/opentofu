3c8cd26662e9591204ac57ab995acf3931348925
Christian Mesh <christianmesh1@gmail.com>
2025-08-21 11:19:06 -0400

    Use cty.Transform to simplify ephemeralasnull impl rules
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/lang/funcs/ephemeral.go
+++ internal/lang/funcs/ephemeral.go
@@ -27,9 +27,7 @@ var EphemeralAsNullFunc = function.New(&function.Spec{
 		return args[0].Type(), nil
 	},
 	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
-		var ephemeralAsNull func(cty.Value) cty.Value
-		// Inspired heavily by https://github.com/zclconf/go-cty/blob/v1.16.3/cty/unknown_as_null.go#L10
-		ephemeralAsNull = func(val cty.Value) cty.Value {
+		return cty.Transform(args[0], func(_ cty.Path, val cty.Value) (cty.Value, error) {
 			ty := val.Type()
 
 			// Preserve non-ephemeral marks
@@ -38,7 +36,7 @@ var EphemeralAsNullFunc = function.New(&function.Spec{
 
 			switch {
 			case val.IsNull():
-				return cty.NullVal(ty).WithMarks(nonEphemeralMarks)
+				return cty.NullVal(ty).WithMarks(nonEphemeralMarks), nil
 			case !val.IsKnown():
 				// This mirrors the logic in IsSensitive()
 				//
@@ -52,60 +50,14 @@ var EphemeralAsNullFunc = function.New(&function.Spec{
 				// unknown value will be ephemeral or not. For more information,
 				// refer to https://github.com/opentofu/opentofu/issues/2415
 
-				return cty.UnknownVal(val.Type()).WithMarks(nonEphemeralMarks)
+				return cty.UnknownVal(val.Type()).WithMarks(nonEphemeralMarks), nil
 			case val.HasMark(marks.Ephemeral):
 				// This whole value is marked as ephemeral and should be null
-				return cty.NullVal(val.Type()).WithMarks(nonEphemeralMarks)
-			case ty.IsListType() || ty.IsTupleType() || ty.IsSetType():
-				unmarkedVal, marks := val.Unmark()
-				oldVals := unmarkedVal.AsValueSlice()
-				if len(oldVals) == 0 {
-					return val
-				}
-
-				newVals := make([]cty.Value, len(oldVals))
-				for i, v := range oldVals {
-					newVals[i] = ephemeralAsNull(v)
-				}
-				switch {
-				case ty.IsListType():
-					return cty.ListVal(newVals).WithMarks(marks)
-				case ty.IsTupleType():
-					return cty.TupleVal(newVals).WithMarks(marks)
-				default:
-					return cty.SetVal(newVals).WithMarks(marks)
-				}
-			case ty.IsMapType() || ty.IsObjectType():
-				unmarkedVal, marks := val.Unmark()
-				oldVals := unmarkedVal.AsValueMap()
-				if len(oldVals) == 0 {
-					return val
-				}
-
-				newVals := make(map[string]cty.Value, len(oldVals))
-				for k, v := range oldVals {
-					newVals[k] = ephemeralAsNull(v)
-				}
-
-				switch {
-				case ty.IsMapType():
-					return cty.MapVal(newVals).WithMarks(marks)
-				default:
-					return cty.ObjectVal(newVals).WithMarks(marks)
-				}
-			case val.CanIterateElements():
-				// The cases above should be exhaustive, but we should be *absolutely* sure
-				_, allMarks := val.UnmarkDeep()
-				if _, ok := allMarks[marks.Ephemeral]; ok {
-					return cty.NullVal(val.Type()).WithSameMarks(val)
-				}
-				return val
+				return cty.NullVal(val.Type()).WithMarks(nonEphemeralMarks), nil
 			default:
 				// Not marked
-				return val
+				return val, nil
 			}
-		}
-
-		return ephemeralAsNull(args[0]), nil
+		})
 	},
 })
