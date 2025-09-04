5cb04694eb0687da06dfd73fef6ffe9cee262a33
Martin Atkins <mart@degeneration.co.uk>
2025-09-03 18:47:20 -0700

    lang/eval: ConfigInstance.PrepareToPlan analyzes ephemeral resources
    There's now enough here to produce a data structure where each ephemeral
    resource instance is associated with all of the resource instances that
    must complete planning before it can be closed.
    
    This also doubles as the first demonstration of dynamic analysis of
    resource instance dependencies using cty marks, with
    TestPrepare_ephemeralResourceUsers illustrating the more precise results.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

---
+++ internal/lang/eval/config_prepare_test.go
@@ -0,0 +1,186 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package eval_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/zclconf/go-cty/cty"
+
+	"github.com/opentofu/opentofu/internal/addrs"
+	"github.com/opentofu/opentofu/internal/configs"
+	"github.com/opentofu/opentofu/internal/configs/configschema"
+	"github.com/opentofu/opentofu/internal/lang/eval"
+	"github.com/opentofu/opentofu/internal/providers"
+)
+
+func TestPrepare_ephemeralResourceUsers(t *testing.T) {
+	configInst, diags := eval.NewConfigInstance(t.Context(), &eval.ConfigCall{
+		EvalContext: &eval.EvalContext{
+			Modules: eval.ModulesForTesting(map[addrs.ModuleSourceLocal]*configs.Module{
+				addrs.ModuleSourceLocal("."): configs.ModuleFromStringForTesting(t, `
+					terraform {
+						required_providers {
+							foo = {
+								source = "test/foo"
+							}
+						}
+					}
+					ephemeral "foo" "a" {
+						count = 2
+
+						name = "a ${count.index}"
+					}
+					ephemeral "foo" "b" {
+						count = 2
+
+						name = ephemeral.foo.a[count.index].id
+					}
+					locals {
+					    # This is intentionally a more complex expression
+						# to analyze, to prove that we can still chase the
+						# instance-specific references through it.
+						# This produces a tuple of two-element tuples with the
+						# corresponding ids of ephemeral.foo.a and
+						# ephemeral.foo.b respectively.
+						together = [
+							for i, a in ephemeral.foo.a :
+							[a.id, ephemeral.foo.b[i].id]
+						]
+					}
+					resource "foo" "c" {
+						count = 2
+
+						# Even indirectly through this projection of values
+						# from the two ephemeral resources we should correctly
+						# detect that foo.c instances are correlated with
+						# ephemeral.foo.a and ephemeral.foo.b instances of
+						# the same index.
+						something = local.together[count.index]
+
+						# The above is really just an overly-complicated way of
+						# writing this:
+						#
+						# something = [
+						#   ephemeral.foo.a[count.index],
+						#   ephemeral.foo.b[count.index],
+						# ]
+					}
+				`),
+			}),
+			Providers: eval.ProvidersForTesting(map[addrs.Provider]*providers.GetProviderSchemaResponse{
+				addrs.MustParseProviderSourceString("test/foo"): {
+					Provider: providers.Schema{
+						Block: &configschema.Block{},
+					},
+					EphemeralResources: map[string]providers.Schema{
+						"foo": {
+							Block: &configschema.Block{
+								Attributes: map[string]*configschema.Attribute{
+									"name": {
+										Type:     cty.String,
+										Required: true,
+									},
+									"id": {
+										Type:     cty.String,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+					ResourceTypes: map[string]providers.Schema{
+						"foo": {
+							Block: &configschema.Block{
+								Attributes: map[string]*configschema.Attribute{
+									"something": {
+										Type:      cty.List(cty.String),
+										Optional:  true,
+										WriteOnly: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+		},
+		RootModuleSource: addrs.ModuleSourceLocal("."),
+		InputValues:      eval.InputValuesForTesting(map[string]cty.Value{}),
+	})
+	if diags.HasErrors() {
+		t.Fatalf("unexpected errors: %s", diags.Err())
+	}
+
+	got, diags := configInst.PrepareToPlan(t.Context())
+	if diags.HasErrors() {
+		t.Fatalf("unexpected errors: %s", diags.Err())
+	}
+
+	fooA := addrs.Resource{
+		Mode: addrs.EphemeralResourceMode,
+		Type: "foo",
+		Name: "a",
+	}.Absolute(addrs.RootModuleInstance)
+	fooB := addrs.Resource{
+		Mode: addrs.EphemeralResourceMode,
+		Type: "foo",
+		Name: "b",
+	}.Absolute(addrs.RootModuleInstance)
+	fooC := addrs.Resource{
+		Mode: addrs.ManagedResourceMode,
+		Type: "foo",
+		Name: "c",
+	}.Absolute(addrs.RootModuleInstance)
+	inst0 := addrs.IntKey(0)
+	inst1 := addrs.IntKey(1)
+
+	// The analysis should detect that:
+	// - ephemeral.foo.a[0] is used by ephemeral.foo.b[0] and foo.c[0]
+	// - ephemeral.foo.a[1] is used by ephemeral.foo.b[1] and foo.c[1]
+	// - ephemeral.foo.b[0] is used by only foo.c[0]
+	// - ephemeral.foo.b[1] is used by only foo.c[1]
+	// In particular, the evaluator should be able to notice that
+	// only the correlated instance keys have any relationship between
+	// them, and so e.g. ephemeral.foo.a[0] is NOT used by ephemeral.foo.b[1].
+	//
+	// This level of precision was not possible in the traditional
+	// "package tofu" language runtime, because it calculated dependencies
+	// based only on static analysis, but this new evaluator uses dynamic
+	// analysis. Refer to [configgraph.ContributingResourceInstances]
+	// to learn more about how that's meant to work, if you're trying to
+	// debug a regression here that made the analysis less precise.
+	want := &eval.ResourceRelationships{
+		// Note that this field captures _inverse_ dependencies: the values
+		// are instances that depend on the keys.
+		//
+		// The best way to understand this is that the ephemeral resource
+		// instance identified in an element's key must remain "open" until all
+		// of the instances identified in the element's value have finished
+		// planning.
+		EphemeralResourceUsers: addrs.MakeMap(
+			addrs.MakeMapElem(fooA.Instance(inst0), addrs.MakeSet(
+				fooB.Instance(inst0),
+				fooC.Instance(inst0),
+			)),
+			addrs.MakeMapElem(fooA.Instance(inst1), addrs.MakeSet(
+				fooB.Instance(inst1),
+				fooC.Instance(inst1),
+			)),
+			addrs.MakeMapElem(fooB.Instance(inst0), addrs.MakeSet(
+				fooC.Instance(inst0),
+			)),
+			addrs.MakeMapElem(fooB.Instance(inst1), addrs.MakeSet(
+				fooC.Instance(inst1),
+			)),
+		),
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Error("wrong result\n" + diff)
+	}
+}
