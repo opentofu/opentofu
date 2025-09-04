d4425ef9d14c92d6486063f525c191554349e79f
Martin Atkins <mart@degeneration.co.uk>
2025-09-03 19:06:58 -0700

    lang/eval+lang/exprs: More exprs.EvalError markings
    It doesn't hurt to be liberal in handling this everywhere that we know
    there's an error because applying the mark where it was already present
    is effectively a no-op, so we'll introduce more of these both for
    robustness and to help folks who are reading this code in future to
    learn the EvalError patterns by observation.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/lang/eval/internal/configgraph/provider_instance.go
+++ internal/lang/eval/internal/configgraph/provider_instance.go
@@ -13,7 +13,7 @@ import (
 //
 // Note that this type's name is slightly misleading because it does not
 // represent an already-running provider that requests can be sent to, but
-// rather the configuration that sould be sent to a running instance of
+// rather the configuration that should be sent to a running instance of
 // this provider in order to prepare it for use. This package does not deal
 // with "configured" providers directly at all, instead expecting its caller
 // (e.g. an implementation or the plan or apply phase) to handle the provider
