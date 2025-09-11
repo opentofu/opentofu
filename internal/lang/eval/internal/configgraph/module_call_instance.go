97e57374bc92422f36131e79ac8381b7d9b5ab7e
Martin Atkins <mart@degeneration.co.uk>
2025-09-11 16:45:45 -0700

    lang/eval: Rework how module calls work
    The previous approach had the problem that we only had the
    CompiledModuleInstance object for a child module briefly in a local
    variable while evaluating the child call, which is sufficient for just
    evaluation but is not enough to support full config tree traversal for
    questions like "what resource instances are declared throughout the
    configuration?" and "what configgraph node corresponds to this provider
    configuration instance address?"
    
    This moves the separation of concerns around a little to be perhaps more
    like how resource instances work, where the ModuleCallInstance.Value
    method just wraps a function provided by the "compile" layer which then
    takes the config value and compiles the child module instance.
    
    This means that the "compile" layer can then hold on to the
    CompiledModuleInstance object for the child module instance as part of
    the "glue" in the ModuleCallInstance object and then use it to deal with
    the config-tree-traversing methods in its own CompiledModuleInstance
    implementation.
    
    The new test of ConfigInstance.PrepareToPlan in lang/eval illustrates that
    the system is now finally able to walk the whole configuration tree to find
    resource instances and the provider instances they depend on.
    
    (The handling of inheritance of providers between parent and child module
    instances is still not working like the current system does, because the
    "providers sidechannel" mechanism remains incomplete.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/lang/eval/internal/configgraph/module_call_instance.go
+++ internal/lang/eval/internal/configgraph/module_call_instance.go
@@ -30,6 +30,23 @@ type ModuleCallInstance struct {
 	// a separate address type for an "absolute module call instance".
 	ModuleInstanceAddr addrs.ModuleInstance
 
+	// The separation of concerns here is a little tangled and complicated,
+	// unfortunately: this node type is responsible for deciding the
+	// final value to use for the child module's "inputs", from the
+	// [ModuleCallInstance.InputsValue] method, but the code that compiled
+	// the object is the one that ultimately needs to compile the child
+	// module instance and so [ModuleCallInstance.Value] accesses the outputs
+	// value of the compiled instance indirectly through
+	// [ModuleCallInstanceGlue.OutputsValue], which presumably indirectly
+	// depends on the [ModuleCallInstance.InputsValue] result but that's
+	// not this package's responsibility.
+	//
+	// Maybe we can find a less upsetting way to arrange this later, but for
+	// now this mostly respects the idea that this package is responsible
+	// for preparing values while the caller (via Glue) is responsible for
+	// compilation and interaction with external dependencies like the module
+	// installer.
+
 	// Glue is provided by whatever compiled this object to allow us to learn
 	// more about the module that is being called.
 	Glue ModuleCallInstanceGlue
@@ -41,10 +58,18 @@ type ModuleCallInstance struct {
 	// language could potentially use different syntax for input variables
 	// that allows constructing the entire map dynamically using expression
 	// syntax.
+	//
+	// TODO: This should actually really be map[addrs.InputVariable]*OnceValuer
+	// so that each input variable can be resolved independently, since the
+	// "package tofu" implementation allows dependencies from a module
+	// instance's output values to its own input variables as long as there
+	// are no cycles in the dependency chain within the module.
 	InputsValuer *OnceValuer
 
 	// TODO: Also something for the "providers side-channel", as represented
 	// by the "providers" meta-argument in the current language.
+
+	validatedInputs grapheval.Once[cty.Value]
 }
 
 var _ exprs.Valuer = (*ModuleCallInstance)(nil)
@@ -57,30 +82,61 @@ func (m *ModuleCallInstance) StaticCheckTraversal(traversal hcl.Traversal) tfdia
 	return nil
 }
 
+// InputsValue returns the validated inputs value that should be passed when
+// compiling the child module instance.
+//
+// Whatever diagnostics this returns should eventually be returned through
+// the [ModuleCallInstanceGlue.OutputsValue] method on the object in the
+// Glue field, after indirection through whatever the compilation layer does
+// to compile and evaluate the child module instance.
+func (m *ModuleCallInstance) InputsValue(ctx context.Context) (cty.Value, tfdiags.Diagnostics) {
+	// We perform the config validation in a [grapheval.Once] so that we
+	// can coalesce around a single call to m.Glue.ValidateInputs, which is
+	// likely to cause a provider RPC request.
+	return m.validatedInputs.Do(ctx, func(ctx context.Context) (cty.Value, tfdiags.Diagnostics) {
+		inputsVal, diags := m.InputsValuer.Value(ctx)
+		if diags.HasErrors() {
+			return cty.DynamicVal, diags
+		}
+		moreDiags := m.Glue.ValidateInputs(ctx, inputsVal)
+		// FIXME: Our "contextual diagnostics" mechanism, where the callee provides
+		// an attribute path and then the caller discovers a suitable source range
+		// for each diagnostic based on information in the body, can only work
+		// when we have direct access to a [hcl.Body], but we intentionally
+		// abstracted that away here. We'll need to find a different design for
+		// contextual diagnostics that can work through the [exprs.Valuer]
+		// abstraction to make a best effort to interpret attribute paths against
+		// whatever the valuer was evaluating.
+		diags = diags.Append(moreDiags)
+		if moreDiags.HasErrors() {
+			return cty.DynamicVal, diags
+		}
+		return inputsVal, diags
+	})
+}
+
 // Value implements exprs.Valuer.
 func (m *ModuleCallInstance) Value(ctx context.Context) (cty.Value, tfdiags.Diagnostics) {
-	inputsVal, diags := m.InputsValuer.Value(ctx)
-	if diags.HasErrors() {
-		return cty.DynamicVal, diags
-	}
-
-	moreDiags := m.Glue.ValidateInputs(ctx, inputsVal)
-	// FIXME: Our "contextual diagnostics" mechanism, where the callee provides
-	// an attribute path and then the caller discovers a suitable source range
-	// for each diagnostic based on information in the body, can only work
-	// when we have direct access to a [hcl.Body], but we intentionally
-	// abstracted that away here. We'll need to find a different design for
-	// contextual diagnostics that can work through the [exprs.Valuer]
-	// abstraction to make a best effort to interpret attribute paths against
-	// whatever the valuer was evaluating.
-	diags = diags.Append(moreDiags)
-	if moreDiags.HasErrors() {
-		return cty.DynamicVal, diags
-	}
-
 	// The actual result value is decided by our caller, which is expected
 	// to know how to actually find, compile, and evaluate the target module.
-	return exprs.EvalResult(m.Glue.OutputsValue(ctx, inputsVal))
+	//
+	// It should also arrange for any diagnostics returned by
+	// [ModuleCallInstance.InputsValue] on this same object to get passed
+	// through into the m.Glue.OutputsValue diagnostics.
+	//
+	// FIXME: This should actually be modelled as a separate valuer for
+	// each output value because the "package tofu" implementation allows
+	// dependencies from a module instance's output values to its own input
+	// variables as long as there are no cycles in the dependency chain within
+	// the module. However, we can only do that after making the exprs package's
+	// model of expression evaluation a little smarter so it can support
+	// partially-evaluated values -- in this case, an object value with only
+	// the specifically-requested output values actually populated in it.
+	// The good news is that making that work would also be a step towards
+	// allowing resource instances to depend on other instances of the same
+	// resource, so the complexity of making that work would pay off in at least
+	// two different ways.
+	return exprs.EvalResult(m.Glue.OutputsValue(ctx))
 }
 
 // ValueSourceRange implements exprs.Valuer.
