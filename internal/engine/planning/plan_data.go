4636fcffe20989da32cb5d8ffb593f35f19f0b2d
Martin Atkins <mart@degeneration.co.uk>
2025-09-12 11:52:35 -0700

    engine/planning: Separate function for each resource mode
    All of this still leads to a TODO panic but we now have a separate method
    to implement for each of the resource modes, since the handling of each
    is pretty independent of the others.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

---
+++ internal/engine/planning/plan_data.go
@@ -0,0 +1,19 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package planning
+
+import (
+	"context"
+
+	"github.com/opentofu/opentofu/internal/lang/eval"
+	"github.com/opentofu/opentofu/internal/tfdiags"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func (p *planContext) planDesiredDataResourceInstance(ctx context.Context, inst *eval.DesiredResourceInstance, oracle *eval.PlanningOracle) (cty.Value, tfdiags.Diagnostics) {
+	// TODO: Implement
+	panic("unimplemented")
+}
