647089773461bc2faaddf31ab27ff72368c96396
Martin Atkins <mart@degeneration.co.uk>
2025-09-16 15:19:46 -0700

    tofu: Warn if object literal includes unused attribute for input variable
    We intentionally allow assigning object types with a superset of the
    attributes included in an input variable's object type constraints because
    it makes it possible to assign a whole object for which only some of the
    attributes are relevant for one input variable but a different subset might
    be relevant when the object value is used in a different part of the
    configuration.
    
    However, when the variable is defined using an object literal expression
    there is no possible way an unexpected attribute could be useful in a
    different part of the configuration, and so that's very very likely to be
    a mistake rather than intentional. Therefore we'll generate a "linter-like"
    warning in that case to help the author notice their mistake without
    introducing any new "strict-mode" language features, or other complexity
    that would be harder to maintain and evolve over time.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/tofu/node_module_variable_test.go
+++ internal/tofu/node_module_variable_test.go
@@ -12,8 +12,10 @@ import (
 	"testing"
 
 	"github.com/go-test/deep"
+	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty-debug/ctydebug"
 	"github.com/zclconf/go-cty/cty"
 
 	"github.com/opentofu/opentofu/internal/addrs"
@@ -304,6 +306,54 @@ func TestNodeModuleVariableConstraints(t *testing.T) {
 
 		if !found {
 			t.Fatalf("missing expected error\nwant summary: %s\ngot: %s", wantSummary, diags.Err().Error())
+		}
+	})
+}
+
+func TestNodeModuleVariable_warningDiags(t *testing.T) {
+	t.Run("unused object attribute", func(t *testing.T) {
+		n := &nodeModuleVariable{
+			Addr: addrs.InputVariable{Name: "foo"}.Absolute(addrs.RootModuleInstance),
+			Config: &configs.Variable{
+				Name: "foo",
+				ConstraintType: cty.Object(map[string]cty.Type{
+					"foo": cty.String,
+					"bar": cty.String,
+				}),
+			},
+			Expr: &hclsyntax.ObjectConsExpr{
+				Items: []hclsyntax.ObjectConsItem{
+					{
+						KeyExpr: &hclsyntax.LiteralValueExpr{
+							Val: cty.StringVal("baz"),
+							SrcRange: hcl.Range{
+								Filename: "test.tofu",
+							},
+						},
+						ValueExpr: &hclsyntax.LiteralValueExpr{
+							Val: cty.StringVal("..."),
+						},
+					},
+				},
+			},
+			ModuleInstance: addrs.RootModuleInstance,
+		}
+		// We use the "ForRPC" representation of the diagnostics just because
+		// it's more friendly for comparison and we care only about the
+		// user-facing information in the diagnostics, not their concrete types.
+		gotDiags := n.warningDiags(t.Context()).ForRPC()
+		var wantDiags tfdiags.Diagnostics
+		wantDiags = wantDiags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagWarning,
+			Summary:  "Object attribute is ignored",
+			Detail:   `The object type for input variable "foo" does not include an attribute named "baz", so this definition is unused. Did you mean to set attribute "bar" instead?`,
+			Subject: &hcl.Range{
+				Filename: "test.tofu",
+			},
+		})
+		wantDiags = wantDiags.ForRPC()
+		if diff := cmp.Diff(wantDiags, gotDiags, ctydebug.CmpOptions); diff != "" {
+			t.Error("wrong diagnostics\n" + diff)
 		}
 	})
 }
