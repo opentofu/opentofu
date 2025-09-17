1f66cad613771b6ebbcbe7e58a554c54625919cf
Diogenes Fernandes <diofeher@gmail.com>
2025-09-17 13:25:36 -0300

    new implementation
    Signed-off-by: Diogenes Fernandes <diofeher@gmail.com>

--- internal/backend/remote/signal_windows.go
+++ internal/backend/remote/signal_windows.go
@@ -11,21 +11,15 @@ package remote
 import (
 	"fmt"
 	"syscall"
+
+	"golang.org/x/sys/windows"
 )
 
 // sendInterruptSignal sends an Ctrl+Break event to the given process ID.
 func sendInterruptSignal(pid int) error {
-	d, e := syscall.LoadDLL("kernel32.dll")
-	if e != nil {
-		return fmt.Errorf("LoadDLL: %v\n", e)
-	}
-	p, e := d.FindProc("GenerateConsoleCtrlEvent")
-	if e != nil {
-		return fmt.Errorf("FindProc: %v\n", e)
-	}
-	r, _, e := p.Call(syscall.CTRL_BREAK_EVENT, uintptr(pid))
-	if r == 0 {
-		return fmt.Errorf("GenerateConsoleCtrlEvent: %v\n", e)
+	err := windows.GenerateConsoleCtrlEvent(syscall.CTRL_BREAK_EVENT, uint32(pid))
+	if err != nil {
+		return fmt.Errorf("GenerateConsoleCtrlEvent: %v\n", err)
 	}
 	return nil
 }
