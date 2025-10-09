97bb8555819a6dbf7e301c6a4044271fe2d8cd53
Larry Bordowitz <laurence.bordowitz@gmail.com>
2025-10-09 10:10:35 -0500

    chunk getFile logic
    Signed-off-by: Larry Bordowitz <laurence.bordowitz@gmail.com>

--- internal/command/cliconfig/config_locations_helper_test.go
+++ internal/command/cliconfig/config_locations_helper_test.go
@@ -40,18 +40,13 @@ type testTemplateInfo struct {
 	Index      int
 }
 
-// getFile generates an example configuration file for use in the locations tests.
-// The generated configuration needs to satisfy two criteria. When looking at the
-// final configuration, we should be able to determine:
-//   - which files contributed to it
-//   - the order those files were merged
-//
-// These are satisfied with a index host and a pairwise "comparison host", whose "module.vX"
-// value receives X from the index of the configuration file with highest precedence.
-// The resulting configuration will only have hosts information, which is sufficient
-// for the purposes of the location tests.
-func getFile(i, n int) ([]byte, error) {
-	subDs := make([]string, n-1)
+// buildSubdomains creates a list of pairs of indices, encoded as subdomain names.
+// These subdomains are of the form "{i}and{j}", where i is always strictly less than j.
+// When we merge two configurations, we want to know which one has "precedence", so we
+// can look at the "module version" value. We use the index for that module version,
+// since configuration files are identified by their index in the test's list of files.
+func buildSubDomains(i, n int) []string {
+	subDomains := make([]string, n-1)
 	adjust := 0
 	for x := range n {
 		if x == i {
@@ -62,16 +57,31 @@ func getFile(i, n int) ([]byte, error) {
 		if x < i {
 			j, k = x, i
 		}
-		subDs[x-adjust] = fmt.Sprintf("%dand%d", j, k)
+		subDomains[x-adjust] = fmt.Sprintf("%dand%d", j, k)
 	}
+	return subDomains
+}
 
+// getFile generates an example configuration file for use in the locations tests.
+// Config files are identified by their name's index in a given test's file list.
+// The generated configuration needs to satisfy two criteria. When looking at the
+// final configuration, we should be able to determine:
+//   - which files contributed to it
+//   - the order those files were merged
+//
+// These are satisfied with a index host and a pairwise "comparison host", whose "module.vX"
+// value receives X from the index of the configuration file with highest precedence.
+// The resulting configuration will only have hosts information, which is sufficient
+// for the purposes of the location tests.
+func getFile(i, n int) ([]byte, error) {
+	subDomains := buildSubDomains(i, n)
 	templateFileName := "config-location.tpl"
 	templateFileLocation := filepath.Join(fixtureDir, templateFileName)
 
 	tpl := template.Must(template.ParseFiles(templateFileLocation))
 
 	byteW := new(bytes.Buffer)
-	err := tpl.ExecuteTemplate(byteW, templateFileName, testTemplateInfo{Subdomains: subDs, Index: i})
+	err := tpl.ExecuteTemplate(byteW, templateFileName, testTemplateInfo{Subdomains: subDomains, Index: i})
 	if err != nil {
 		return nil, err
 	}
