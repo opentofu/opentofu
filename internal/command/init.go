a965431795d97d5f104a99e68d3a41cb74232396
Christian Mesh <christianmesh1@gmail.com>
2025-10-09 10:13:00 -0400

    Fix race conditions with parallelized provider install
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

--- internal/command/init.go
+++ internal/command/init.go
@@ -960,6 +960,8 @@ func (c *InitCommand) getProviders(ctx context.Context, config *configs.Config, 
 			}
 		},
 	}
+	// Ensure that events emitted on multiple routines do not trigger race conditions
+	evts = evts.Serialized()
 	ctx = evts.OnContext(ctx)
 
 	mode := providercache.InstallNewProvidersOnly
