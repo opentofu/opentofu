2e90af870a3d61e091f80b90a679d1ab8cd78b39
Anurag Ojha <aojharaj2004@gmail.com>
2025-10-29 09:19:30 +0000

    typo
    Signed-off-by: Anurag Ojha <aojharaj2004@gmail.com>

--- internal/backend/remote-state/azure/backend.go
+++ internal/backend/remote-state/azure/backend.go
@@ -321,7 +321,7 @@ func (b *Backend) configure(ctx context.Context) error {
 			OIDCTokenFilePath: data.Get("oidc_token_file_path").(string),
 			OIDCRequestURL:    data.Get("oidc_request_url").(string),
 			OIDCRequestToken:  data.Get("oidc_request_token").(string),
-			ADOPipelineServiceConnectionID:  data.Get("ado_pipeline_service_conncetion_id").(string),
+			ADOPipelineServiceConnectionID:  data.Get("ado_pipeline_service_connection_id").(string),
 		},
 		MSIAuthConfig: auth.MSIAuthConfig{
 			UseMsi:   data.Get("use_msi").(bool),
