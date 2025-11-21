9e1153eb6841de731812315054379fc8c5376458
Martin Atkins <mart@degeneration.co.uk>
2025-11-20 16:40:54 -0800

    backend/local: Scaffolding for new runtime can use providers
    This is some minimal glue to help the new runtime use the providers that
    were gathered up by the existing logic in the "command" package.
    
    This is cheating a little because this is relying on "tofu init" still
    using the old approach just enough to find out which providers are needed
    and get them installed, but our current focus is on the main plan and
    apply phases and so it's convenient to be able to leave that part untouched
    for now and return to improve it later, once we have more of the
    fundamentals in place.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/backend/local/backend_temp_new_runtime.go
+++ internal/backend/local/backend_temp_new_runtime.go
@@ -7,10 +7,12 @@ package local
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 	"sync/atomic"
 
 	"github.com/apparentlymart/go-versions/versions"
@@ -26,6 +28,7 @@ import (
 	"github.com/opentofu/opentofu/internal/lang/eval"
 	"github.com/opentofu/opentofu/internal/plans"
 	"github.com/opentofu/opentofu/internal/providers"
+	"github.com/opentofu/opentofu/internal/provisioners"
 	"github.com/opentofu/opentofu/internal/states"
 	"github.com/opentofu/opentofu/internal/states/statemgr"
 	"github.com/opentofu/opentofu/internal/tfdiags"
@@ -149,7 +152,8 @@ func (b *Local) opPlanWithExperimentalRuntime(stopCtx context.Context, cancelCtx
 	}
 
 	plugins := &newRuntimePlugins{
-		// TODO: ...
+		providers:    b.ContextOpts.Providers,
+		provisioners: b.ContextOpts.Provisioners,
 	}
 	evalCtx := &eval.EvalContext{
 		RootModuleDir:      op.ConfigDir,
@@ -160,6 +164,16 @@ func (b *Local) opPlanWithExperimentalRuntime(stopCtx context.Context, cancelCtx
 		Providers:    plugins,
 		Provisioners: plugins,
 	}
+	defer func() {
+		// We'll call close with a cancel-free context because we do still
+		// want to shut the providers down even if we're dealing with
+		// graceful shutdown after cancellation.
+		err := plugins.Close(context.WithoutCancel(ctx))
+		// If a provider fails to close there isn't really much we can do
+		// about that... this shouldn't really be possible unless the
+		// plugin process already exited for some other reason anyway.
+		log.Printf("[ERROR] plugin shutdown failed: %s", err)
+	}()
 
 	// The new config-loading system wants to work in terms of module source
 	// addresses rather than raw local filenames, so we'll ask the
@@ -309,6 +323,17 @@ func (n *newRuntimeModules) ModuleConfig(ctx context.Context, source addrs.Modul
 }
 
 type newRuntimePlugins struct {
+	providers    map[addrs.Provider]providers.Factory
+	provisioners map[string]provisioners.Factory
+
+	// unconfiguredInsts is all of the provider instances we've created for
+	// unconfigured uses such as schema fetching and validation, which we
+	// currently just leave running for the remainder of the life of this
+	// object though perhaps we'll do something more clever eventually.
+	//
+	// Must hold a lock on mu throughout any access to this map.
+	unconfiguredInsts map[addrs.Provider]providers.Unconfigured
+	mu                sync.Mutex
 }
 
 var _ eval.Providers = (*newRuntimePlugins)(nil)
@@ -316,61 +341,212 @@ var _ eval.Provisioners = (*newRuntimePlugins)(nil)
 
 // NewConfiguredProvider implements evalglue.Providers.
 func (n *newRuntimePlugins) NewConfiguredProvider(ctx context.Context, provider addrs.Provider, configVal cty.Value) (providers.Configured, tfdiags.Diagnostics) {
-	var diags tfdiags.Diagnostics
-	diags = diags.Append(tfdiags.Sourceless(
-		tfdiags.Error,
-		"Cannot use providers in new runtime codepath",
-		fmt.Sprintf("Can't use provider %q: new runtime codepath doesn't know how to instantiate providers yet", provider),
-	))
-	return nil, diags
+	inst, diags := n.newProviderInst(ctx, provider)
+	if diags.HasErrors() {
+		return nil, diags
+	}
+
+	resp := inst.ConfigureProvider(ctx, providers.ConfigureProviderRequest{
+		Config: configVal,
+
+		// We aren't actually Terraform, so we'll just pretend to be a
+		// Terraform version that has roughly the same functionality that
+		// OpenTofu currently has, since providers are permitted to use this to
+		// adapt their behavior for older versions of Terraform.
+		TerraformVersion: "1.13.0",
+	})
+	diags = diags.Append(resp.Diagnostics)
+	if resp.Diagnostics.HasErrors() {
+		return nil, diags
+	}
+
+	return inst, diags
 }
 
 // ProviderConfigSchema implements evalglue.Providers.
 func (n *newRuntimePlugins) ProviderConfigSchema(ctx context.Context, provider addrs.Provider) (*providers.Schema, tfdiags.Diagnostics) {
 	var diags tfdiags.Diagnostics
-	diags = diags.Append(tfdiags.Sourceless(
-		tfdiags.Error,
-		"Cannot use providers in new runtime codepath",
-		fmt.Sprintf("Can't use provider %q: new runtime codepath doesn't know how to instantiate providers yet", provider),
-	))
-	return nil, diags
+
+	inst, moreDiags := n.unconfiguredProviderInst(ctx, provider)
+	diags = diags.Append(moreDiags)
+	if moreDiags.HasErrors() {
+		return nil, diags
+	}
+
+	resp := inst.GetProviderSchema(ctx)
+	diags = diags.Append(resp.Diagnostics)
+	if resp.Diagnostics.HasErrors() {
+		return nil, diags
+	}
+
+	return &resp.Provider, diags
 }
 
 // ResourceTypeSchema implements evalglue.Providers.
 func (n *newRuntimePlugins) ResourceTypeSchema(ctx context.Context, provider addrs.Provider, mode addrs.ResourceMode, typeName string) (*providers.Schema, tfdiags.Diagnostics) {
 	var diags tfdiags.Diagnostics
-	diags = diags.Append(tfdiags.Sourceless(
-		tfdiags.Error,
-		"Cannot use providers in new runtime codepath",
-		fmt.Sprintf("Can't use provider %q: new runtime codepath doesn't know how to instantiate providers yet", provider),
-	))
-	return nil, diags
+
+	inst, moreDiags := n.unconfiguredProviderInst(ctx, provider)
+	diags = diags.Append(moreDiags)
+	if moreDiags.HasErrors() {
+		return nil, diags
+	}
+
+	resp := inst.GetProviderSchema(ctx)
+	diags = diags.Append(resp.Diagnostics)
+	if resp.Diagnostics.HasErrors() {
+		return nil, diags
+	}
+
+	// NOTE: Callers expect us to return nil if we successfully fetch the
+	// provider schema and then find there is no matching resource type, because
+	// the caller is typically in a better position to return a useful error
+	// message than we are.
+
+	var types map[string]providers.Schema
+	switch mode {
+	case addrs.ManagedResourceMode:
+		types = resp.ResourceTypes
+	case addrs.DataResourceMode:
+		types = resp.DataSources
+	case addrs.EphemeralResourceMode:
+		types = resp.EphemeralResources
+	default:
+		// We don't support any other modes, so we'll just treat these as
+		// a request for a resource type that doesn't exist at all.
+		return nil, nil
+	}
+	ret, ok := types[typeName]
+	if !ok {
+		return nil, diags
+	}
+	return &ret, diags
 }
 
 // ValidateProviderConfig implements evalglue.Providers.
 func (n *newRuntimePlugins) ValidateProviderConfig(ctx context.Context, provider addrs.Provider, configVal cty.Value) tfdiags.Diagnostics {
 	var diags tfdiags.Diagnostics
-	diags = diags.Append(tfdiags.Sourceless(
-		tfdiags.Error,
-		"Cannot use providers in new runtime codepath",
-		fmt.Sprintf("Can't use provider %q: new runtime codepath doesn't know how to instantiate providers yet", provider),
-	))
+
+	inst, moreDiags := n.unconfiguredProviderInst(ctx, provider)
+	diags = diags.Append(moreDiags)
+	if moreDiags.HasErrors() {
+		return diags
+	}
+
+	resp := inst.ValidateProviderConfig(ctx, providers.ValidateProviderConfigRequest{
+		Config: configVal,
+	})
+	diags = diags.Append(resp.Diagnostics)
 	return diags
 }
 
 // ValidateResourceConfig implements evalglue.Providers.
 func (n *newRuntimePlugins) ValidateResourceConfig(ctx context.Context, provider addrs.Provider, mode addrs.ResourceMode, typeName string, configVal cty.Value) tfdiags.Diagnostics {
 	var diags tfdiags.Diagnostics
-	diags = diags.Append(tfdiags.Sourceless(
-		tfdiags.Error,
-		"Cannot use providers in new runtime codepath",
-		fmt.Sprintf("Can't use provider %q: new runtime codepath doesn't know how to instantiate providers yet", provider),
-	))
+
+	inst, moreDiags := n.unconfiguredProviderInst(ctx, provider)
+	diags = diags.Append(moreDiags)
+	if moreDiags.HasErrors() {
+		return diags
+	}
+
+	switch mode {
+	case addrs.ManagedResourceMode:
+		resp := inst.ValidateResourceConfig(ctx, providers.ValidateResourceConfigRequest{
+			TypeName: typeName,
+			Config:   configVal,
+		})
+		diags = diags.Append(resp.Diagnostics)
+	case addrs.DataResourceMode:
+		resp := inst.ValidateDataResourceConfig(ctx, providers.ValidateDataResourceConfigRequest{
+			TypeName: typeName,
+			Config:   configVal,
+		})
+		diags = diags.Append(resp.Diagnostics)
+	case addrs.EphemeralResourceMode:
+		resp := inst.ValidateEphemeralConfig(ctx, providers.ValidateEphemeralConfigRequest{
+			TypeName: typeName,
+			Config:   configVal,
+		})
+		diags = diags.Append(resp.Diagnostics)
+	default:
+		// If we get here then it's a bug because the cases above should
+		// cover all valid values of [addrs.ResourceMode].
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Unsupported resource mode",
+			fmt.Sprintf("Attempted to validate resource of unsupported mode %s; this is a bug in OpenTofu.", mode),
+		))
+	}
 	return diags
+}
+
+func (m *newRuntimePlugins) unconfiguredProviderInst(ctx context.Context, provider addrs.Provider) (providers.Unconfigured, tfdiags.Diagnostics) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if running, ok := m.unconfiguredInsts[provider]; ok {
+		return running, nil
+	}
+
+	inst, diags := m.newProviderInst(ctx, provider)
+	if diags.HasErrors() {
+		return nil, diags
+	}
+
+	if m.unconfiguredInsts == nil {
+		m.unconfiguredInsts = make(map[addrs.Provider]providers.Unconfigured)
+	}
+	m.unconfiguredInsts[provider] = inst
+	return inst, diags
+}
+
+// newProviderInst creates a new instance of the given provider.
+//
+// The result is not retained anywhere inside the receiver. Each call to this
+// function returns a new object. A successful result is always an unconfigured
+// provider, but we return [providers.Interface] in case the caller would like
+// to subsequently configure the result before returning it as
+// [providers.Configured].
+//
+// If you intend to use the resulting instance only for "unconfigured"
+// operations like fetching schema, use
+// [newRuntimePlugins.unconfiguredProviderInst] instead to potentially reuse
+// an already-active instance of the same provider.
+func (m *newRuntimePlugins) newProviderInst(_ context.Context, provider addrs.Provider) (providers.Interface, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+	factory, ok := m.providers[provider]
+	if !ok {
+		// FIXME: If this error remains reachable in the final version of this
+		// code (i.e. if some caller isn't already guaranteeing that all
+		// providers from the configuration and state are included here) then
+		// we should make this error message more actionable.
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Provider unavailable",
+			fmt.Sprintf("This configuration requires provider %q, but it isn't installed.", provider),
+		))
+		return nil, diags
+	}
+
+	inst, err := factory()
+	if err != nil {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Provider failed to start",
+			fmt.Sprintf("Failed to launch provider %q: %s.", provider, tfdiags.FormatError(err)),
+		))
+		return nil, diags
+	}
+
+	return inst, diags
 }
 
 // ProvisionerConfigSchema implements evalglue.Provisioners.
 func (n *newRuntimePlugins) ProvisionerConfigSchema(ctx context.Context, typeName string) (*configschema.Block, tfdiags.Diagnostics) {
+	// TODO: Implement this in terms of [newRuntimePlugins.provisioners].
+	// But provisioners aren't in scope for our "walking skeleton" phase of
+	// development, so we'll skip this for now.
 	var diags tfdiags.Diagnostics
 	diags = diags.Append(tfdiags.Sourceless(
 		tfdiags.Error,
@@ -378,4 +554,21 @@ func (n *newRuntimePlugins) ProvisionerConfigSchema(ctx context.Context, typeNam
 		fmt.Sprintf("Can't use provisioner %q: new runtime codepath doesn't know how to instantiate provisioners yet", typeName),
 	))
 	return nil, diags
+}
+
+// Close terminates any plugins that are managed by this object and are still
+// running.
+func (n *newRuntimePlugins) Close(ctx context.Context) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	var errs error
+	for addr, p := range n.unconfiguredInsts {
+		err := p.Close(ctx)
+		if err != nil {
+			errs = errors.Join(errs, fmt.Errorf("closing provider %q: %w", addr, err))
+		}
+	}
+	n.unconfiguredInsts = nil // discard all of the memoized instances
+	return errs
 }
