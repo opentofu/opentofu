db8c872defd8666618649ef7e29fa2b809adfd5e
Martin Atkins <mart@degeneration.co.uk>
2026-02-06 10:19:47 -0800

    planning: Initial unit tests for managed resource instance execgraph
    We're currently being intentionally cautious about adding too many tests
    while these new parts of the system are still evolving and changing a lot,
    but the execGraphBuilder behavior is hopefully self-contained enough for
    a small set of basic tests to be more helpful than hurtful.
    
    We should extend this test, and add other test cases that involve more
    complicated interactions between different resource instances of different
    modes, once we feel that these new codepaths have reached a more mature
    state where we're more focused on localized maintenance than on broad
    system design exploration.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

---
+++ internal/engine/planning/execgraph_managed_test.go
@@ -0,0 +1,170 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package planning
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/zclconf/go-cty/cty"
+
+	"github.com/opentofu/opentofu/internal/addrs"
+	"github.com/opentofu/opentofu/internal/engine/internal/exec"
+	"github.com/opentofu/opentofu/internal/engine/internal/execgraph"
+	"github.com/opentofu/opentofu/internal/plans"
+)
+
+// TestExecGraphBuilder_ManagedResourceInstanceSubgraph is a unit test for
+// the ManagedResourceInstanceSubgraph method in particular, focused only on
+// the items and relationships that function produces.
+//
+// Interactions between this method and others should be tested elsewhere.
+func TestExecGraphBuilder_ManagedResourceInstanceSubgraph(t *testing.T) {
+	// instAddr is the resource instance address that each test should use
+	// for the resource instance object whose result is returned from the
+	// "Build" function. We set the return value as the result for this
+	// resource instance so that it'll appear in the graph DebugRepr for
+	// comparison.
+	instAddr := addrs.Resource{
+		Mode: addrs.ManagedResourceMode,
+		Type: "test",
+		Name: "placeholder",
+	}.Instance(addrs.NoKey).Absolute(addrs.RootModuleInstance)
+
+	tests := map[string]struct {
+		Build    func(b *execGraphBuilder, providerClientRef execgraph.ResultRef[*exec.ProviderClient]) execgraph.ResourceInstanceResultRef
+		WantRepr string
+	}{
+		"create": {
+			func(b *execGraphBuilder, providerClientRef execgraph.ResultRef[*exec.ProviderClient]) execgraph.ResourceInstanceResultRef {
+				return b.ManagedResourceInstanceSubgraph(
+					&plans.ResourceInstanceChange{
+						Addr:        instAddr,
+						PrevRunAddr: instAddr,
+						Change: plans.Change{
+							Action: plans.Create,
+							Before: cty.NullVal(cty.EmptyObject),
+							After:  cty.EmptyObjectVal,
+						},
+					},
+					providerClientRef,
+					addrs.MakeSet[addrs.AbsResourceInstance](),
+				)
+			},
+			`
+				v[0] = cty.EmptyObjectVal;
+				
+				r[0] = ResourceInstanceDesired(test.placeholder, await());
+				r[1] = ManagedFinalPlan(r[0], nil, v[0], nil);
+				r[2] = ManagedApply(r[1], nil, nil, await());
+
+				test.placeholder = r[2];
+			`,
+		},
+		"update": {
+			func(b *execGraphBuilder, providerClientRef execgraph.ResultRef[*exec.ProviderClient]) execgraph.ResourceInstanceResultRef {
+				return b.ManagedResourceInstanceSubgraph(
+					&plans.ResourceInstanceChange{
+						Addr:        instAddr,
+						PrevRunAddr: instAddr,
+						Change: plans.Change{
+							Action: plans.Update,
+							Before: cty.StringVal("before"),
+							After:  cty.StringVal("after"),
+						},
+					},
+					providerClientRef,
+					addrs.MakeSet[addrs.AbsResourceInstance](),
+				)
+			},
+			`
+				v[0] = cty.StringVal("after");
+				
+				r[0] = ResourceInstancePrior(test.placeholder);
+				r[1] = ResourceInstanceDesired(test.placeholder, await());
+				r[2] = ManagedFinalPlan(r[1], r[0], v[0], nil);
+				r[3] = ManagedApply(r[2], nil, nil, await());
+
+				test.placeholder = r[3];
+			`,
+		},
+		"delete": {
+			func(b *execGraphBuilder, providerClientRef execgraph.ResultRef[*exec.ProviderClient]) execgraph.ResourceInstanceResultRef {
+				return b.ManagedResourceInstanceSubgraph(
+					&plans.ResourceInstanceChange{
+						Addr:        instAddr,
+						PrevRunAddr: instAddr,
+						Change: plans.Change{
+							Action: plans.Delete,
+							Before: cty.EmptyObjectVal,
+							After:  cty.NullVal(cty.EmptyObject),
+						},
+					},
+					providerClientRef,
+					addrs.MakeSet[addrs.AbsResourceInstance](),
+				)
+			},
+			`
+				v[0] = cty.NullVal(cty.EmptyObject);
+				
+				r[0] = ResourceInstancePrior(test.placeholder);
+				r[1] = ManagedFinalPlan(nil, r[0], v[0], nil);
+				r[2] = ManagedApply(r[1], nil, nil, await());
+
+				test.placeholder = r[2];
+			`,
+		},
+		"delete then create": {
+			func(b *execGraphBuilder, providerClientRef execgraph.ResultRef[*exec.ProviderClient]) execgraph.ResourceInstanceResultRef {
+				return b.ManagedResourceInstanceSubgraph(
+					&plans.ResourceInstanceChange{
+						Addr:        instAddr,
+						PrevRunAddr: instAddr,
+						Change: plans.Change{
+							Action: plans.DeleteThenCreate,
+							Before: cty.StringVal("before"),
+							After:  cty.StringVal("after"),
+						},
+					},
+					providerClientRef,
+					addrs.MakeSet[addrs.AbsResourceInstance](),
+				)
+			},
+			`
+				v[0] = cty.StringVal("after");
+				v[1] = cty.NullVal(cty.String);
+				
+				r[0] = ResourceInstancePrior(test.placeholder);
+				r[1] = ResourceInstanceDesired(test.placeholder, await());
+				r[2] = ManagedFinalPlan(r[1], nil, v[0], nil);
+				r[3] = ManagedFinalPlan(nil, r[0], v[1], nil);
+				r[4] = ManagedApply(r[3], nil, nil, await(r[2]));
+				r[5] = ManagedApply(r[2], nil, nil, await(r[4]));
+
+				test.placeholder = r[5];
+			`,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			builder := newExecGraphBuilder()
+			// This test is focused only on the resource instance subgraphs,
+			// so we just use a placeholder nil result for the provider client.
+			providerClientRef := execgraph.NilResultRef[*exec.ProviderClient]()
+			resultRef := test.Build(builder, providerClientRef)
+			builder.lower.SetResourceInstanceFinalStateResult(instAddr, resultRef)
+
+			graph := builder.Finish()
+			gotGraphRepr := strings.TrimSpace(graph.DebugRepr())
+			wantGraphRepr := strings.TrimSpace(stripCommonLeadingTabs(test.WantRepr))
+			if diff := cmp.Diff(wantGraphRepr, gotGraphRepr); diff != "" {
+				t.Error("wrong result\n" + diff)
+			}
+		})
+	}
+}
