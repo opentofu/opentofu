54e424ee749ac0d027571dc6d54a7082854688ea
Martin Atkins <mart@degeneration.co.uk>
2026-02-05 18:32:09 -0800

    planning: Different execgraph subgraph shapes per change action
    Previously the generated execgraph was naive and only really supported
    "create" changes. This commit has some initial work on generalizing
    that, though it's not yet complete and will continue in later commits.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/engine/planning/execgraph_managed.go
+++ internal/engine/planning/execgraph_managed.go
@@ -6,10 +6,16 @@
 package planning
 
 import (
+	"fmt"
+
+	"github.com/zclconf/go-cty/cty"
+
 	"github.com/opentofu/opentofu/internal/addrs"
 	"github.com/opentofu/opentofu/internal/engine/internal/exec"
 	"github.com/opentofu/opentofu/internal/engine/internal/execgraph"
+	"github.com/opentofu/opentofu/internal/lang/eval"
 	"github.com/opentofu/opentofu/internal/plans"
+	"github.com/opentofu/opentofu/internal/states"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -43,25 +49,193 @@ func (b *execGraphBuilder) ManagedResourceInstanceSubgraph(
 	// desiredInstRef result we'll build below.
 	dependencyWaiter, closeDependencyAfter := b.waiterForResourceInstances(requiredResourceInstances.All())
 
-	// FIXME: If this is one of the "replace" actions then we need to generate
-	// a more complex graph that has two pairs of "final plan" and "apply".
-	instAddrRef := b.lower.ConstantResourceInstAddr(plannedChange.Addr)
-	priorStateRef := b.lower.ResourceInstancePrior(instAddrRef)
+	// Before we go any further we'll just make sure what we've been given
+	// is sensible, so that the remaining code can assume the following
+	// about the given change. Any panics in the following suggest that there's
+	// a bug in the caller, unless we're intentionally changing the rules
+	// for what the different action types represent.
+	if plannedChange.DeposedKey != states.NotDeposed && plannedChange.Action != plans.Delete {
+		// The only sensible thing to do with a deposed object is to delete it.
+		panic(fmt.Sprintf("invalid action %s for %s deposed object %s", plannedChange.Action, plannedChange.PrevRunAddr, plannedChange.DeposedKey))
+	}
+	if plannedChange.Action == plans.Create && !plannedChange.Before.IsNull() {
+		panic(fmt.Sprintf("for %s has action %s but non-null prior value", plannedChange.Addr, plannedChange.Action))
+	}
+	if (plannedChange.Action == plans.Delete || plannedChange.Action == plans.Forget) && !plannedChange.After.IsNull() {
+		panic(fmt.Sprintf("change for %s has action %s but non-null planned new value", plannedChange.PrevRunAddr, plannedChange.Action))
+	}
+	if plannedChange.Action != plans.Create && plannedChange.Action != plans.Delete && plannedChange.Action != plans.Forget && (plannedChange.Before.IsNull() || plannedChange.After.IsNull()) {
+		panic(fmt.Sprintf("change for %s has action %s but does not have both a before and after value", plannedChange.PrevRunAddr, plannedChange.Action))
+	}
+
+	// The shape of execution subgraph we generate here varies depending on
+	// which change action was planned.
+	var finalResultRef execgraph.ResourceInstanceResultRef
+	switch plannedChange.Action {
+	case plans.Create:
+		finalResultRef = b.managedResourceInstanceSubgraphCreate(plannedChange, providerClientRef, dependencyWaiter)
+	case plans.Delete:
+		finalResultRef = b.managedResourceInstanceSubgraphDelete(plannedChange, providerClientRef)
+	case plans.Update:
+		finalResultRef = b.managedResourceInstanceSubgraphUpdate(plannedChange, providerClientRef, dependencyWaiter)
+	case plans.Forget:
+		finalResultRef = b.managedResourceInstanceSubgraphForget(plannedChange, providerClientRef)
+	case plans.DeleteThenCreate, plans.ForgetThenCreate:
+		finalResultRef = b.managedResourceInstanceSubgraphDeleteOrForgetThenCreate(plannedChange, providerClientRef, dependencyWaiter)
+	case plans.CreateThenDelete:
+		finalResultRef = b.managedResourceInstanceSubgraphCreateThenDelete(plannedChange, providerClientRef, dependencyWaiter)
+	default:
+		// We should not get here: the cases above should cover every action
+		// that [planGlue.planDesiredManagedResourceInstance] can possibly
+		// produce.
+		panic(fmt.Sprintf("unsupported change action %s for %s", plannedChange.Action, plannedChange.Addr))
+	}
+
+	closeDependencyAfter(finalResultRef)
+	return finalResultRef
+}
+
+func (b *execGraphBuilder) managedResourceInstanceSubgraphCreate(
+	plannedChange *plans.ResourceInstanceChange,
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+	waitFor execgraph.AnyResultRef,
+) execgraph.ResourceInstanceResultRef {
+	instAddrRef, _ := b.managedResourceInstanceChangeAddrAndPriorStateRefs(plannedChange)
 	plannedValRef := b.lower.ConstantValue(plannedChange.After)
-	desiredInstRef := b.lower.ResourceInstanceDesired(instAddrRef, dependencyWaiter)
+	desiredInstRef := b.lower.ResourceInstanceDesired(instAddrRef, waitFor)
+	return b.managedResourceInstanceSubgraphPlanAndApply(
+		desiredInstRef,
+		execgraph.NilResultRef[*exec.ResourceInstanceObject](),
+		plannedValRef,
+		providerClientRef,
+	)
+}
+
+func (b *execGraphBuilder) managedResourceInstanceSubgraphDelete(
+	plannedChange *plans.ResourceInstanceChange,
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+) execgraph.ResourceInstanceResultRef {
+	_, priorStateRef := b.managedResourceInstanceChangeAddrAndPriorStateRefs(plannedChange)
+	plannedValRef := b.lower.ConstantValue(plannedChange.After)
+	return b.managedResourceInstanceSubgraphPlanAndApply(
+		execgraph.NilResultRef[*eval.DesiredResourceInstance](),
+		priorStateRef,
+		plannedValRef,
+		providerClientRef,
+	)
+}
+
+func (b *execGraphBuilder) managedResourceInstanceSubgraphUpdate(
+	plannedChange *plans.ResourceInstanceChange,
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+	waitFor execgraph.AnyResultRef,
+) execgraph.ResourceInstanceResultRef {
+	instAddrRef, priorStateRef := b.managedResourceInstanceChangeAddrAndPriorStateRefs(plannedChange)
+	plannedValRef := b.lower.ConstantValue(plannedChange.After)
+	desiredInstRef := b.lower.ResourceInstanceDesired(instAddrRef, waitFor)
+	return b.managedResourceInstanceSubgraphPlanAndApply(
+		desiredInstRef,
+		priorStateRef,
+		plannedValRef,
+		providerClientRef,
+	)
+}
+
+// managedResourceInstanceSubgraphPlanAndApply deals with the simple case
+// of "create a final plan and then apply it" that is shared across all of the
+// "straightforward" change actions create, update, and delete, but not for
+// the more complicated ones involving multiple primitive actions that need
+// to be carefully coordinated with each other.
+func (b *execGraphBuilder) managedResourceInstanceSubgraphPlanAndApply(
+	desiredInstRef execgraph.ResultRef[*eval.DesiredResourceInstance],
+	priorStateRef execgraph.ResourceInstanceResultRef,
+	plannedValRef execgraph.ResultRef[cty.Value],
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+) execgraph.ResourceInstanceResultRef {
 	finalPlanRef := b.lower.ManagedFinalPlan(
 		desiredInstRef,
 		priorStateRef,
 		plannedValRef,
 		providerClientRef,
 	)
-	finalResultRef := b.lower.ManagedApply(
+	return b.lower.ManagedApply(
 		finalPlanRef,
 		execgraph.NilResultRef[*exec.ResourceInstanceObject](),
 		providerClientRef,
 	)
+}
 
-	closeDependencyAfter(finalResultRef)
+func (b *execGraphBuilder) managedResourceInstanceSubgraphForget(
+	plannedChange *plans.ResourceInstanceChange,
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+) execgraph.ResourceInstanceResultRef {
+	// TODO: Add a new execgraph opcode ManagedForget and use that here.
+	panic("execgraph for Forget not yet implemented")
+}
 
-	return finalResultRef
+func (b *execGraphBuilder) managedResourceInstanceSubgraphDeleteOrForgetThenCreate(
+	plannedChange *plans.ResourceInstanceChange,
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+	waitFor execgraph.AnyResultRef,
+) execgraph.ResourceInstanceResultRef {
+	if plannedChange.Action == plans.ForgetThenCreate {
+		// TODO: Implement this action too, which is similar but with the
+		// "delete" let replaced with something like what
+		// managedResourceInstanceSubgraphForget would generate.
+		panic("execgraph for ForgetThenCreate not yet implemented")
+	}
+
+	// This has much the same _effect_ as the separate delete and create
+	// actions chained together, but we arrange the operations in such a
+	// way that the delete leg can't start unless the desired state is
+	// successfully evaluated.
+	instAddrRef, priorStateRef := b.managedResourceInstanceChangeAddrAndPriorStateRefs(plannedChange)
+	plannedValRef := b.lower.ConstantValue(plannedChange.After)
+	desiredInstRef := b.lower.ResourceInstanceDesired(instAddrRef, waitFor)
+
+	// TODO: Actually implement this
+	_, _, _ = priorStateRef, plannedValRef, desiredInstRef
+	panic("execgraph for simple replace actions not yet implemented")
+}
+
+func (b *execGraphBuilder) managedResourceInstanceSubgraphCreateThenDelete(
+	plannedChange *plans.ResourceInstanceChange,
+	providerClientRef execgraph.ResultRef[*exec.ProviderClient],
+	waitFor execgraph.AnyResultRef,
+) execgraph.ResourceInstanceResultRef {
+	// TODO: Add a new execgraph opcode ManagedForget and use that here.
+	panic("execgraph for cbd replace actions not yet implemented")
+}
+
+func (b *execGraphBuilder) managedResourceInstanceChangeAddrAndPriorStateRefs(
+	plannedChange *plans.ResourceInstanceChange,
+) (
+	newAddr execgraph.ResultRef[addrs.AbsResourceInstance],
+	priorState execgraph.ResourceInstanceResultRef,
+) {
+	if plannedChange.Action == plans.Create {
+		// For a create change there is no prior state at all, but we still
+		// need the new instance address.
+		newAddrRef := b.lower.ConstantResourceInstAddr(plannedChange.Addr)
+		return newAddrRef, execgraph.NilResultRef[*exec.ResourceInstanceObject]()
+	}
+	prevAddrRef := b.lower.ConstantResourceInstAddr(plannedChange.PrevRunAddr)
+	if plannedChange.DeposedKey != states.NotDeposed {
+		// We need to use a different operation to access deposed objects.
+		prevAddrRef := b.lower.ConstantResourceInstAddr(plannedChange.PrevRunAddr)
+		dkRef := b.lower.ConstantDeposedKey(plannedChange.DeposedKey)
+		stateRef := b.lower.ManagedAlreadyDeposed(prevAddrRef, dkRef)
+		return execgraph.NilResultRef[addrs.AbsResourceInstance](), stateRef
+	}
+	newAddrRef := b.lower.ConstantResourceInstAddr(plannedChange.Addr)
+	if !plannedChange.PrevRunAddr.Equal(plannedChange.Addr) {
+		// If the address is changing then we need to use the "change address"
+		// operation instead of just reading te prior state.
+		stateRef := b.lower.ManagedChangeAddr(prevAddrRef, newAddrRef)
+		return newAddrRef, stateRef
+	}
+	// In all other cases we just take the prior state directly out of the
+	// prior state, without any special behavior.
+	stateRef := b.lower.ResourceInstancePrior(prevAddrRef)
+	return newAddrRef, stateRef
 }
