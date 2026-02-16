835e80d1e142b783e51b07cccf6d061d6e25947e
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-02-16 09:44:33 +0200

    Add arguments and migrate workspace_command.go
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

---
+++ internal/command/arguments/workspace.go
@@ -0,0 +1,41 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package arguments
+
+import (
+	"github.com/opentofu/opentofu/internal/tfdiags"
+)
+
+type Workspace struct {
+	ViewOptions ViewOptions
+}
+
+func ParseWorkspace(args []string) (*Workspace, func(), tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+
+	ret := &Workspace{}
+	cmdFlags := defaultFlagSet("workspace")
+
+	if err := cmdFlags.Parse(args); err != nil {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Failed to parse command-line flags",
+			err.Error(),
+		))
+	}
+	if len(cmdFlags.Args()) > 0 {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Unexpected argument",
+			"Too many command line arguments. Did you mean to use -chdir?",
+		))
+	}
+
+	// we only parse but do not register the views flags since this command does not need it
+	closer, moreDiags := ret.ViewOptions.Parse()
+	diags = diags.Append(moreDiags)
+	return ret, closer, diags
+}
