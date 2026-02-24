6374a292df3c4b5bddf0930f44bd88cd46d69420
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-02-24 14:23:32 +0200

    Add unit tests
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/encryption/dual_custody_test.go
+++ internal/encryption/dual_custody_test.go
@@ -20,8 +20,9 @@ import (
 func TestDualCustody(t *testing.T) {
 	// Note: the XOR provider is not available in final OpenTofu builds because its security constraints have not
 	// been properly evaluated. The code below doesn't work in OpenTofu and is for tests only.
-	t.Run("hcl", func(t *testing.T) {
-		sourceConfig := `key_provider "pbkdf2" "base1" {
+	cases := map[string]string{
+		// simple hcl
+		"hcl": `key_provider "pbkdf2" "base1" {
 			passphrase = "Hello world! 123"
 		}
 		key_provider "pbkdf2" "base2" {
@@ -36,52 +37,9 @@ func TestDualCustody(t *testing.T) {
 		}
 		state {
 			method = method.aes_gcm.example
-		}`
-		reg := lockingencryptionregistry.New()
-		if err := reg.RegisterKeyProvider(xor.New()); err != nil {
-			panic(err)
-		}
-		if err := reg.RegisterKeyProvider(pbkdf2.New()); err != nil {
-			panic(err)
-		}
-		if err := reg.RegisterMethod(aesgcm.New()); err != nil {
-			panic(err)
-		}
-		if err := reg.RegisterMethod(unencrypted.New()); err != nil {
-			panic(err)
-		}
-
-		parsedSourceConfig, diags := config.LoadConfigFromString("source", sourceConfig)
-		if diags.HasErrors() {
-			t.Fatalf("%v", diags.Error())
-		}
-
-		staticEval := configs.NewStaticEvaluator(nil, configs.RootModuleCallForTesting())
-
-		enc, diags := New(t.Context(), reg, parsedSourceConfig, staticEval)
-		if diags.HasErrors() {
-			t.Fatalf("%v", diags.Error())
-		}
-
-		sfe := enc.State()
-		testData := []byte(`{"serial": 42, "lineage": "magic"}`)
-		encryptedState, err := sfe.EncryptState(testData)
-		if err != nil {
-			t.Fatalf("%v", err)
-		}
-		if string(encryptedState) == string(testData) {
-			t.Fatalf("The state has not been encrypted.")
-		}
-		decryptedState, _, err := sfe.DecryptState(encryptedState)
-		if err != nil {
-			t.Fatalf("%v", err)
-		}
-		if string(decryptedState) != string(testData) {
-			t.Fatalf("Incorrect decrypted state: %s", decryptedState)
-		}
-	})
-	t.Run("json", func(t *testing.T) {
-		sourceConfig := `{
+		}`,
+		// Json with raw references
+		"json with raw reference": `{
       "key_provider": {
         "pbkdf2": {
           "base1": {
@@ -109,48 +67,82 @@ func TestDualCustody(t *testing.T) {
         "method": "method.aes_gcm.example"
       }
     }
-`
-		reg := lockingencryptionregistry.New()
-		if err := reg.RegisterKeyProvider(xor.New()); err != nil {
-			panic(err)
-		}
-		if err := reg.RegisterKeyProvider(pbkdf2.New()); err != nil {
-			panic(err)
-		}
-		if err := reg.RegisterMethod(aesgcm.New()); err != nil {
-			panic(err)
-		}
-		if err := reg.RegisterMethod(unencrypted.New()); err != nil {
-			panic(err)
-		}
+`,
+		// Json format with interpolated references
+		"json with interpolation": `{
+      "key_provider": {
+        "pbkdf2": {
+          "base1": {
+			"passphrase": "Hello world! 123"
+          },
+          "base2": {
+			"passphrase": "OpenTofu has Encryption"
+          }
+        },
+        "xor": {
+          "dualcustody": {
+            "a": "${key_provider.pbkdf2.base1}",
+            "b": "${key_provider.pbkdf2.base2}"
+          }
+        }
+      },
+      "method": {
+        "aes_gcm": {
+          "example": {
+            "keys": "key_provider.xor.dualcustody"
+          }
+        }
+      },
+      "state": {
+        "method": "method.aes_gcm.example"
+      }
+    }
+`,
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			reg := lockingencryptionregistry.New()
+			if err := reg.RegisterKeyProvider(xor.New()); err != nil {
+				panic(err)
+			}
+			if err := reg.RegisterKeyProvider(pbkdf2.New()); err != nil {
+				panic(err)
+			}
+			if err := reg.RegisterMethod(aesgcm.New()); err != nil {
+				panic(err)
+			}
+			if err := reg.RegisterMethod(unencrypted.New()); err != nil {
+				panic(err)
+			}
 
-		parsedSourceConfig, diags := config.LoadConfigFromString("source", sourceConfig)
-		if diags.HasErrors() {
-			t.Fatalf("%v", diags.Error())
-		}
+			parsedSourceConfig, diags := config.LoadConfigFromString("source", tc)
+			if diags.HasErrors() {
+				t.Fatalf("%v", diags.Error())
+			}
 
-		staticEval := configs.NewStaticEvaluator(nil, configs.RootModuleCallForTesting())
+			staticEval := configs.NewStaticEvaluator(nil, configs.RootModuleCallForTesting())
 
-		enc, diags := New(t.Context(), reg, parsedSourceConfig, staticEval)
-		if diags.HasErrors() {
-			t.Fatalf("%v", diags.Error())
-		}
+			enc, diags := New(t.Context(), reg, parsedSourceConfig, staticEval)
+			if diags.HasErrors() {
+				t.Fatalf("%v", diags.Error())
+			}
 
-		sfe := enc.State()
-		testData := []byte(`{"serial": 42, "lineage": "magic"}`)
-		encryptedState, err := sfe.EncryptState(testData)
-		if err != nil {
-			t.Fatalf("%v", err)
-		}
-		if string(encryptedState) == string(testData) {
-			t.Fatalf("The state has not been encrypted.")
-		}
-		decryptedState, _, err := sfe.DecryptState(encryptedState)
-		if err != nil {
-			t.Fatalf("%v", err)
-		}
-		if string(decryptedState) != string(testData) {
-			t.Fatalf("Incorrect decrypted state: %s", decryptedState)
-		}
-	})
+			sfe := enc.State()
+			testData := []byte(`{"serial": 42, "lineage": "magic"}`)
+			encryptedState, err := sfe.EncryptState(testData)
+			if err != nil {
+				t.Fatalf("%v", err)
+			}
+			if string(encryptedState) == string(testData) {
+				t.Fatalf("The state has not been encrypted.")
+			}
+			decryptedState, _, err := sfe.DecryptState(encryptedState)
+			if err != nil {
+				t.Fatalf("%v", err)
+			}
+			if string(decryptedState) != string(testData) {
+				t.Fatalf("Incorrect decrypted state: %s", decryptedState)
+			}
+		})
+	}
 }
--- internal/encryption/keyprovider/pbkdf2/config_test.go
+++ internal/encryption/keyprovider/pbkdf2/config_test.go
@@ -8,7 +8,13 @@ package pbkdf2_test
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/opentofu/opentofu/internal/encryption/config"
+	"github.com/opentofu/opentofu/internal/encryption/keyprovider"
 	"github.com/opentofu/opentofu/internal/encryption/keyprovider/pbkdf2"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func TestHashFunctionName_Validate(t *testing.T) {
@@ -106,4 +112,252 @@ func TestConfig_Build(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestConfig_DecodeBody(t *testing.T) {
+	tc := map[string]struct {
+		setup          func(t *testing.T) (hcl.Body, *hcl.EvalContext)
+		expectedConfig *pbkdf2.Config
+	}{
+		"nil body": {
+			setup: func(*testing.T) (hcl.Body, *hcl.EvalContext) {
+				return nil, nil
+			},
+			expectedConfig: &pbkdf2.Config{},
+		},
+		"body with passphrase": {
+			setup: func(t *testing.T) (hcl.Body, *hcl.EvalContext) {
+				parsedSourceConfig, diags := config.LoadConfigFromString("source", `
+		key_provider "pbkdf2" "base1" {
+			passphrase = "Hello world! 123"
+		}`)
+				if diags.HasErrors() {
+					t.Fatalf("failed to create config: %s", diags.Error())
+				}
+				return parsedSourceConfig.KeyProviderConfigs[0].Body, &hcl.EvalContext{}
+			},
+			expectedConfig: &pbkdf2.Config{Passphrase: "Hello world! 123"},
+		},
+		"hcl body with chain reference": {
+			setup: func(t *testing.T) (hcl.Body, *hcl.EvalContext) {
+				parsedSourceConfig, diags := config.LoadConfigFromString("source", `
+		key_provider "pbkdf2" "base1" {
+			passphrase = "Hello world! 123"
+		}
+		key_provider "pbkdf2" "base2" {
+			chain = key_provider.pbkdf2.base1
+		}`)
+				if diags.HasErrors() {
+					t.Fatalf("failed to create config: %s", diags.Error())
+				}
+				evalCtx := &hcl.EvalContext{
+					Variables: map[string]cty.Value{
+						"key_provider": cty.ObjectVal(map[string]cty.Value{
+							"pbkdf2": cty.ObjectVal(map[string]cty.Value{
+								"base1": cty.ObjectVal(map[string]cty.Value{
+									"encryption_key": byteToCty([]byte("Hello world! 123")),
+									"decryption_key": byteToCty([]byte("Hello world! 123")),
+								}),
+							}),
+						}),
+					},
+				}
+				return parsedSourceConfig.KeyProviderConfigs[1].Body, evalCtx
+			},
+			expectedConfig: &pbkdf2.Config{
+				Chain: &keyprovider.Output{
+					EncryptionKey: []byte("Hello world! 123"),
+					DecryptionKey: []byte("Hello world! 123"),
+				},
+			},
+		},
+		"json body with chain reference as string": {
+			setup: func(t *testing.T) (hcl.Body, *hcl.EvalContext) {
+				parsedSourceConfig, diags := config.LoadConfigFromString("source", `{
+		      "key_provider": {
+		        "pbkdf2": {
+		          "base1": {
+					"passphrase": "Hello world! 123"
+		          },
+		          "base2": {
+					"chain": "key_provider.pbkdf2.base1"
+		          }
+		        }
+		      }
+		    }`)
+				if diags.HasErrors() {
+					t.Fatalf("failed to create config: %s", diags.Error())
+				}
+				evalCtx := &hcl.EvalContext{
+					Variables: map[string]cty.Value{
+						"key_provider": cty.ObjectVal(map[string]cty.Value{
+							"pbkdf2": cty.ObjectVal(map[string]cty.Value{
+								"base1": cty.ObjectVal(map[string]cty.Value{
+									"encryption_key": byteToCty([]byte("Hello world! 123")),
+									"decryption_key": byteToCty([]byte("Hello world! 123")),
+								}),
+							}),
+						}),
+					},
+				}
+				return parsedSourceConfig.KeyProviderConfigs[1].Body, evalCtx
+			},
+			expectedConfig: &pbkdf2.Config{
+				Chain: &keyprovider.Output{
+					EncryptionKey: []byte("Hello world! 123"),
+					DecryptionKey: []byte("Hello world! 123"),
+				},
+			},
+		},
+		"json body with interpolated chain reference": {
+			setup: func(t *testing.T) (hcl.Body, *hcl.EvalContext) {
+				parsedSourceConfig, diags := config.LoadConfigFromString("source", `{
+		      "key_provider": {
+		        "pbkdf2": {
+		          "base1": {
+					"passphrase": "Hello world! 123"
+		          },
+		          "base2": {
+					"chain": "${key_provider.pbkdf2.base1}"
+		          }
+		        }
+		      }
+		    }`)
+				if diags.HasErrors() {
+					t.Fatalf("failed to create config: %s", diags.Error())
+				}
+				evalCtx := &hcl.EvalContext{
+					Variables: map[string]cty.Value{
+						"key_provider": cty.ObjectVal(map[string]cty.Value{
+							"pbkdf2": cty.ObjectVal(map[string]cty.Value{
+								"base1": cty.ObjectVal(map[string]cty.Value{
+									"encryption_key": byteToCty([]byte("Hello world! 123")),
+									"decryption_key": byteToCty([]byte("Hello world! 123")),
+								}),
+							}),
+						}),
+					},
+				}
+				return parsedSourceConfig.KeyProviderConfigs[1].Body, evalCtx
+			},
+			expectedConfig: &pbkdf2.Config{
+				Chain: &keyprovider.Output{
+					EncryptionKey: []byte("Hello world! 123"),
+					DecryptionKey: []byte("Hello world! 123"),
+				},
+			},
+		},
+		"json with all properties defined": {
+			setup: func(t *testing.T) (hcl.Body, *hcl.EvalContext) {
+				parsedSourceConfig, diags := config.LoadConfigFromString("source", `{
+		      "key_provider": {
+		        "pbkdf2": {
+		          "base1": {
+					"passphrase": "Hello world! 123"
+		          },
+		          "base2": {
+					"chain": "${key_provider.pbkdf2.base1}",
+					"key_length": 256,
+					"iterations": 3,
+					"hash_function": "sha256",
+					"salt_length": 3
+		          }
+		        }
+		      }
+		    }`)
+				if diags.HasErrors() {
+					t.Fatalf("failed to create config: %s", diags.Error())
+				}
+				evalCtx := &hcl.EvalContext{
+					Variables: map[string]cty.Value{
+						"key_provider": cty.ObjectVal(map[string]cty.Value{
+							"pbkdf2": cty.ObjectVal(map[string]cty.Value{
+								"base1": cty.ObjectVal(map[string]cty.Value{
+									"encryption_key": byteToCty([]byte("Hello world! 123")),
+									"decryption_key": byteToCty([]byte("Hello world! 123")),
+								}),
+							}),
+						}),
+					},
+				}
+				return parsedSourceConfig.KeyProviderConfigs[1].Body, evalCtx
+			},
+			expectedConfig: &pbkdf2.Config{
+				Chain: &keyprovider.Output{
+					EncryptionKey: []byte("Hello world! 123"),
+					DecryptionKey: []byte("Hello world! 123"),
+				},
+				KeyLength:    256,
+				Iterations:   3,
+				HashFunction: "sha256",
+				SaltLength:   3,
+			},
+		},
+		"hcl with all properties defined": {
+			setup: func(t *testing.T) (hcl.Body, *hcl.EvalContext) {
+				parsedSourceConfig, diags := config.LoadConfigFromString("source", `
+key_provider "pbkdf2" "base1" {
+	passphrase = "Hello world! 123"
+}
+
+key_provider "pbkdf2" "base2" {
+	chain = key_provider.pbkdf2.base1
+	key_length = 256
+	iterations = 3
+	hash_function = "sha256"
+	salt_length = 3
+}`)
+				if diags.HasErrors() {
+					t.Fatalf("failed to create config: %s", diags.Error())
+				}
+				evalCtx := &hcl.EvalContext{
+					Variables: map[string]cty.Value{
+						"key_provider": cty.ObjectVal(map[string]cty.Value{
+							"pbkdf2": cty.ObjectVal(map[string]cty.Value{
+								"base1": cty.ObjectVal(map[string]cty.Value{
+									"encryption_key": byteToCty([]byte("Hello world! 123")),
+									"decryption_key": byteToCty([]byte("Hello world! 123")),
+								}),
+							}),
+						}),
+					},
+				}
+				return parsedSourceConfig.KeyProviderConfigs[1].Body, evalCtx
+			},
+			expectedConfig: &pbkdf2.Config{
+				Chain: &keyprovider.Output{
+					EncryptionKey: []byte("Hello world! 123"),
+					DecryptionKey: []byte("Hello world! 123"),
+				},
+				KeyLength:    256,
+				Iterations:   3,
+				HashFunction: "sha256",
+				SaltLength:   3,
+			},
+		},
+	}
+	for name, testCase := range tc {
+		t.Run(name, func(t *testing.T) {
+			body, evalCtx := testCase.setup(t)
+			cfg := &pbkdf2.Config{}
+			diags := cfg.DecodeConfig(body, evalCtx)
+			if diff := cmp.Diff(testCase.expectedConfig, cfg, cmpopts.IgnoreUnexported(pbkdf2.Config{})); diff != "" {
+				t.Errorf("wrong config. diff (-want, +got):\n%s", diff)
+			}
+			if len(diags) != 0 {
+				t.Errorf("unexpected diagnostics: %s", diags.Error())
+			}
+		})
+	}
+}
+
+func byteToCty(data []byte) cty.Value {
+	if len(data) == 0 {
+		return cty.NullVal(cty.List(cty.Number))
+	}
+	ctyData := make([]cty.Value, len(data))
+	for i, d := range data {
+		ctyData[i] = cty.NumberIntVal(int64(d))
+	}
+	return cty.ListVal(ctyData)
 }
