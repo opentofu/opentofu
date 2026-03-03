0ffecc7a14d6c507e548841e62cf76bb9d2ceffa
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-03-03 12:44:56 +0200

    Refactor all `workspace` commands to use View instead of Ui (#3770)
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/command/workspace_new.go
+++ internal/command/workspace_new.go
@@ -10,9 +10,10 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/mitchellh/cli"
+	"github.com/opentofu/opentofu/internal/command/flags"
+	"github.com/opentofu/opentofu/internal/tfdiags"
 	"github.com/posener/complete"
 
 	"github.com/opentofu/opentofu/internal/command/arguments"
@@ -20,7 +21,6 @@ import (
 	"github.com/opentofu/opentofu/internal/command/views"
 	"github.com/opentofu/opentofu/internal/encryption"
 	"github.com/opentofu/opentofu/internal/states/statefile"
-	"github.com/opentofu/opentofu/internal/tfdiags"
 )
 
 type WorkspaceNewCommand struct {
@@ -28,57 +28,69 @@ type WorkspaceNewCommand struct {
 	LegacyName bool
 }
 
-func (c *WorkspaceNewCommand) Run(args []string) int {
+func (c *WorkspaceNewCommand) Run(rawArgs []string) int {
 	ctx := c.CommandContext()
-	args = c.Meta.process(args)
-	envCommandShowWarning(c.Ui, c.LegacyName)
 
-	var stateLock bool
-	var stateLockTimeout time.Duration
-	var statePath string
-	cmdFlags := c.Meta.defaultFlagSet("workspace new")
-	c.Meta.varFlagSet(cmdFlags)
-	cmdFlags.BoolVar(&stateLock, "lock", true, "lock state")
-	cmdFlags.DurationVar(&stateLockTimeout, "lock-timeout", 0, "lock timeout")
-	cmdFlags.StringVar(&statePath, "state", "", "tofu state file")
-	cmdFlags.Usage = func() { c.Ui.Error(c.Help()) }
-	if err := cmdFlags.Parse(args); err != nil {
-		c.Ui.Error(fmt.Sprintf("Error parsing command-line flags: %s\n", err.Error()))
-		return 1
-	}
+	common, rawArgs := arguments.ParseView(rawArgs)
+	c.View.Configure(common)
+	// Because the legacy UI was using println to show diagnostics and the new view is using, by default, print,
+	// in order to keep functional parity, we setup the view to add a new line after each diagnostic.
+	c.View.DiagsWithNewline()
 
-	args = cmdFlags.Args()
-	if len(args) != 1 {
-		c.Ui.Error("Expected a single argument: NAME.\n")
+	// Propagate -no-color for legacy use of Ui. The remote backend and
+	// cloud package use this; it should be removed when/if they are
+	// migrated to views.
+	c.Meta.color = !common.NoColor
+	c.Meta.Color = c.Meta.color
+
+	// Parse and validate flags
+	args, closer, diags := arguments.ParseWorkspaceNew(rawArgs)
+	defer closer()
+
+	// Instantiate the view, even if there are flag errors, so that we render
+	// diagnostics according to the desired view
+	view := views.NewWorkspace(args.ViewOptions, c.View)
+	// ... and initialise the Meta.Ui to wrap Meta.View into a new implementation
+	// that is able to print by using View abstraction and use the Meta.Ui
+	// to ask for the user input.
+	c.Meta.configureUiFromView(args.ViewOptions)
+	if diags.HasErrors() {
+		view.Diagnostics(diags)
+		if args.ViewOptions.ViewType == arguments.ViewJSON {
+			return 1 // in case it's json, do not print the help of the command
+		}
 		return cli.RunResultHelp
 	}
+	c.GatherVariables(args.Vars)
 
-	workspace := args[0]
+	view.WarnWhenUsedAsEnvCmd(c.LegacyName)
+
+	// TODO meta-refactor: remove these when meta state locking related fields are removed and pass the
+	//  arguments to the backend component instead
+	c.stateLock = args.StateLock
+	c.stateLockTimeout = args.StateLockTimeout
+	c.statePath = args.StatePath
+
+	configPath := c.WorkingDir.NormalizePath(c.WorkingDir.RootModuleDir())
+
+	workspace := args.WorkspaceName
 
 	if !validWorkspaceName(workspace) {
-		c.Ui.Error(fmt.Sprintf(envInvalidName, workspace))
+		view.WorkspaceInvalidName(workspace)
 		return 1
 	}
 
 	// You can't ask to create a workspace when you're overriding the
 	// workspace name to be something different.
 	if current, isOverridden := c.WorkspaceOverridden(ctx); current != workspace && isOverridden {
-		c.Ui.Error(envIsOverriddenNewError)
+		view.WorkspaceIsOverriddenNewError()
 		return 1
 	}
-
-	configPath, err := modulePath(args[1:])
-	if err != nil {
-		c.Ui.Error(err.Error())
-		return 1
-	}
-
-	var diags tfdiags.Diagnostics
 
 	backendConfig, backendDiags := c.loadBackendConfig(ctx, configPath)
 	diags = diags.Append(backendDiags)
 	if diags.HasErrors() {
-		c.showDiagnostics(diags)
+		view.Diagnostics(diags)
 		return 1
 	}
 
@@ -86,7 +98,7 @@ func (c *WorkspaceNewCommand) Run(args []string) int {
 	enc, encDiags := c.EncryptionFromPath(ctx, configPath)
 	diags = diags.Append(encDiags)
 	if encDiags.HasErrors() {
-		c.showDiagnostics(diags)
+		view.Diagnostics(diags)
 		return 1
 	}
 
@@ -96,7 +108,7 @@ func (c *WorkspaceNewCommand) Run(args []string) int {
 	}, enc.State())
 	diags = diags.Append(backendDiags)
 	if backendDiags.HasErrors() {
-		c.showDiagnostics(diags)
+		view.Diagnostics(diags)
 		return 1
 	}
 
@@ -105,31 +117,43 @@ func (c *WorkspaceNewCommand) Run(args []string) int {
 
 	workspaces, err := b.Workspaces(ctx)
 	if err != nil {
-		c.Ui.Error(fmt.Sprintf("Failed to get configured named states: %s", err))
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error getting existing workspaces",
+			fmt.Sprintf("Failed listing workspaces: %s", err),
+		)})
 		return 1
 	}
 	for _, ws := range workspaces {
 		if workspace == ws {
-			c.Ui.Error(fmt.Sprintf(envExists, workspace))
+			view.WorkspaceAlreadyExists(workspace)
 			return 1
 		}
 	}
 
 	_, err = b.StateMgr(ctx, workspace)
 	if err != nil {
-		c.Ui.Error(err.Error())
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error getting the state manager",
+			fmt.Sprintf("Failed getting state manager for workspace %s: %s", workspace, err),
+		)})
 		return 1
 	}
 
 	// now set the current workspace locally
 	if err := c.SetWorkspace(workspace); err != nil {
-		c.Ui.Error(fmt.Sprintf("Error selecting new workspace: %s", err))
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error selecting the new workspace",
+			fmt.Sprintf("Failed selecting the new workspace %s: %s", workspace, err),
+		)})
 		return 1
 	}
 
-	c.Ui.Output(c.Colorize().Color(fmt.Sprintf(
-		strings.TrimSpace(envCreated), workspace)))
+	view.WorkspaceCreated(workspace)
 
+	statePath := args.StatePath
 	if statePath == "" {
 		// if we're not loading a state, then we're done
 		return 0
@@ -138,19 +162,23 @@ func (c *WorkspaceNewCommand) Run(args []string) int {
 	// load the new Backend state
 	stateMgr, err := b.StateMgr(ctx, workspace)
 	if err != nil {
-		c.Ui.Error(err.Error())
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error getting the state manager for the new workspace",
+			fmt.Sprintf("Failed getting state manager for workspace %s: %s", workspace, err),
+		)})
 		return 1
 	}
 
-	if stateLock {
-		stateLocker := clistate.NewLocker(c.stateLockTimeout, views.NewStateLocker(arguments.ViewOptions{ViewType: arguments.ViewHuman}, c.View))
+	if args.StateLock {
+		stateLocker := clistate.NewLocker(args.StateLockTimeout, views.NewStateLocker(args.ViewOptions, c.View))
 		if diags := stateLocker.Lock(stateMgr, "workspace-new"); diags.HasErrors() {
-			c.showDiagnostics(diags)
+			view.Diagnostics(diags)
 			return 1
 		}
 		defer func() {
 			if diags := stateLocker.Unlock(); diags.HasErrors() {
-				c.showDiagnostics(diags)
+				view.Diagnostics(diags)
 			}
 		}()
 	}
@@ -158,25 +186,41 @@ func (c *WorkspaceNewCommand) Run(args []string) int {
 	// read the existing state file
 	f, err := os.Open(statePath)
 	if err != nil {
-		c.Ui.Error(err.Error())
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error opening the given state",
+			fmt.Sprintf("Failed opening the given state %q: %s", statePath, err),
+		)})
 		return 1
 	}
 
 	stateFile, err := statefile.Read(f, encryption.StateEncryptionDisabled()) // Assume given statefile is not encrypted
 	if err != nil {
-		c.Ui.Error(err.Error())
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error reading the given state",
+			fmt.Sprintf("Failed reading the given state %q: %s", statePath, err),
+		)})
 		return 1
 	}
 
 	// save the existing state in the new Backend.
 	err = stateMgr.WriteState(stateFile.State)
 	if err != nil {
-		c.Ui.Error(err.Error())
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error writing the new state to the given state file",
+			fmt.Sprintf("Failed writing the new state to the given state file %q: %s", statePath, err),
+		)})
 		return 1
 	}
 	err = stateMgr.PersistState(context.TODO(), nil)
 	if err != nil {
-		c.Ui.Error(err.Error())
+		view.Diagnostics(tfdiags.Diagnostics{tfdiags.Sourceless(
+			tfdiags.Error,
+			"Error persisting the new state",
+			fmt.Sprintf("Failed persisting the new state: %s", err),
+		)})
 		return 1
 	}
 
@@ -221,10 +265,35 @@ Options:
                         to the default files terraform.tfvars and *.auto.tfvars.
                         Use this option more than once to include more than one
                         variables file.
+    
+    -json               The output of the command is printed in json format.
+
+    -json-into=out.json Produce the same output as -json, but sent directly
+                        to the given file. This allows automation to preserve
+                        the original human-readable output streams, while
+                        capturing more detailed logs for machine analysis.
 `
 	return strings.TrimSpace(helpText)
 }
 
 func (c *WorkspaceNewCommand) Synopsis() string {
 	return "Create a new workspace"
+}
+
+// TODO meta-refactor: move this to arguments once all commands are using the same shim logic
+func (c *WorkspaceNewCommand) GatherVariables(args *arguments.Vars) {
+	// FIXME the arguments package currently trivially gathers variable related
+	// arguments in a heterogeneous slice, in order to minimize the number of
+	// code paths gathering variables during the transition to this structure.
+	// Once all commands that gather variables have been converted to this
+	// structure, we could move the variable gathering code to the arguments
+	// package directly, removing this shim layer.
+
+	varArgs := args.All()
+	items := make([]flags.RawFlag, len(varArgs))
+	for i := range varArgs {
+		items[i].Name = varArgs[i].Name
+		items[i].Value = varArgs[i].Value
+	}
+	c.Meta.variableArgs = flags.RawFlags{Items: &items}
 }
