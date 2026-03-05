3a2085d5b1d00489d2e58b275e8a1c42435f279d
Martin Atkins <mart@degeneration.co.uk>
2026-03-05 13:39:50 -0800

    lang/exprs: FlatScopeForTesting
    For unit tests of components that don't depend on any particular symbol
    table structure, where we just want to test if they are using the provided
    scope _at all_, it's helpful to be able to just quickly create a static
    symbol table.
    
    This is a reusable helper function for that.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/lang/exprs/scope.go
+++ internal/lang/exprs/scope.go
@@ -7,9 +7,11 @@ package exprs
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/opentofu/opentofu/internal/tfdiags"
+	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
 )
 
@@ -45,4 +47,48 @@ func (b ChildScopeBuilder) Build(ctx context.Context, parent Scope) Scope {
 		return parent
 	}
 	return b(ctx, parent)
+}
+
+// FlatTestingScope returns a [Scope] implementation that is intended for
+// isolated testing of codepaths that do arbitrary expression evaluation without
+// any direct expectation about what's in scope, by providing just a flat
+// table of top-level symbols.
+//
+// This is not intended for use in non-test code, and it's probably also not
+// an ergonomic way to test codepaths that expect a specific shape of symbol
+// table, such as the global scope of a module. The best use of this is when
+// you just want to test whether a codepath is correctly using a scope provided
+// by its caller, where you can tailor the expressions to suit the constraints
+// of this function.
+//
+// The returned scope does not include any functions.
+func FlatScopeForTesting(symbols map[string]cty.Value) Scope {
+	return flatValueScope(symbols)
+}
+
+type flatValueScope map[string]cty.Value
+
+// HandleInvalidStep implements [Scope].
+func (s flatValueScope) HandleInvalidStep(rng tfdiags.SourceRange) tfdiags.Diagnostics {
+	var diags tfdiags.Diagnostics
+	diags = diags.Append(fmt.Errorf("invalid reference at %s", rng.StartString()))
+	return diags
+}
+
+// ResolveAttr implements [Scope].
+func (s flatValueScope) ResolveAttr(ref hcl.TraverseAttr) (Attribute, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+	v, ok := s[ref.Name]
+	if !ok {
+		diags = diags.Append(fmt.Errorf("no symbol named %q", ref.Name))
+		return nil, diags
+	}
+	return ValueOf(ConstantValuer(v)), diags
+}
+
+// ResolveFunc implements [Scope].
+func (s flatValueScope) ResolveFunc(call *hcl.StaticCall) (function.Function, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+	diags = diags.Append(fmt.Errorf("no function named %s", call.Name))
+	return function.Function{}, diags
 }
