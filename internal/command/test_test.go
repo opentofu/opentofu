95534492a074f986fb0210902455edd6cc27342a
Larry Bordowitz <laurence.bordowitz@gmail.com>
2026-03-10 14:27:16 -0500

    adding wildcard precedence test
    Signed-off-by: Larry Bordowitz <laurence.bordowitz@gmail.com>

--- internal/command/test_test.go
+++ internal/command/test_test.go
@@ -1700,6 +1700,10 @@ func TestTest_InstanceOverride(t *testing.T) {
 			expected: "3 passed, 0 failed.",
 			code:     0,
 		},
+		"module_2": {
+			expected: "3 passed, 0 failed.",
+			code:     0,
+		},
 	}
 
 	providerSource, close := newMockProviderSource(t, map[string][]string{
