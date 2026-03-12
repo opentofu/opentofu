05c267162f81a80631372a73da58173f013234a5
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-03-12 10:53:17 +0200

    Add test to confirm the issue with missing preconditions on ephemeral
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/tofu/node_resource_abstract_instance.go
+++ internal/tofu/node_resource_abstract_instance.go
@@ -3238,6 +3238,18 @@ func (n *NodeAbstractResourceInstance) applyEphemeralResource(ctx context.Contex
 
 	keyData = evalCtx.InstanceExpander().GetResourceInstanceRepetitionData(n.ResourceInstanceAddr())
 
+	// checkDiags := evalCheckRules(
+	// 	ctx,
+	// 	addrs.ResourcePrecondition,
+	// 	n.Config.Preconditions,
+	// 	evalCtx, n.Addr, keyData,
+	// 	tfdiags.Error,
+	// )
+	// diags = diags.Append(checkDiags)
+	// if diags.HasErrors() {
+	// 	return nil, keyData, diags // failed preconditions prevent further evaluation
+	// }
+
 	var configDiags tfdiags.Diagnostics
 	configVal, _, configDiags = evalCtx.EvaluateBlock(ctx, n.Config.Config, schema, nil, keyData)
 	diags = diags.Append(configDiags)
