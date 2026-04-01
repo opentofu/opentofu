2cc8ee9e02970aa007e96119cfbd680d336ba6de
James Humphries <james@james-humphries.co.uk>
2026-04-01 10:17:57 +0100

    undo line removal
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/engine/internal/execgraph/operations_test.go
+++ internal/engine/internal/execgraph/operations_test.go
@@ -325,6 +325,7 @@ func (m *managedResourceInstanceMockProvider) UpgradeResourceState(context.Conte
 	panic("unimplemented")
 }
 
+
 // UpgradeResourceIdentity implements providers.Configured.
 func (m *managedResourceInstanceMockProvider) UpgradeResourceIdentity(context.Context, providers.UpgradeResourceIdentityRequest) providers.UpgradeResourceIdentityResponse {
 	panic("unimplemented")
