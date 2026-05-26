f2e04c287bcdefe988611a052e524c4b2ed9a701
Larry Bordowitz <laurence.bordowitz@gmail.com>
2026-05-26 11:41:12 -0500

    Encryption azure vault fix 1.12 (#4151)
    Signed-off-by: Larry Bordowitz <laurence.bordowitz@gmail.com>
    Signed-off-by: Gal Salomon <gasalomo@akamai.com>
    Co-authored-by: Gal Salomon <gasalomo@akamai.com>

--- internal/encryption/keyprovider/azure_vault/config.go
+++ internal/encryption/keyprovider/azure_vault/config.go
@@ -130,8 +130,8 @@ func (c Config) Build() (keyprovider.KeyProvider, keyprovider.KeyMeta, error) {
 		return nil, nil, &keyprovider.ErrInvalidConfiguration{Message: "key_length must be at least 1"}
 	}
 
-	environment := stringAttrDefaultEnvFallback(c.OIDCToken, "public", "ARM_ENVIRONMENT")
-	metadataHost := stringAttrEnvFallback(c.OIDCToken, "ARM_METADATA_HOST")
+	environment := stringAttrDefaultEnvFallback(c.Environment, "public", "ARM_ENVIRONMENT")
+	metadataHost := stringAttrEnvFallback(c.MetadataHost, "ARM_METADATA_HOST")
 
 	cloudConfig, _, err := auth.CloudConfigFromAddresses(
 		ctx,
@@ -176,8 +176,8 @@ func (c Config) Build() (keyprovider.KeyProvider, keyprovider.KeyMeta, error) {
 		},
 		StorageAddresses: auth.StorageAddresses{
 			CloudConfig:    cloudConfig,
-			SubscriptionID: stringAttrEnvFallback(c.OIDCToken, "ARM_SUBSCRIPTION_ID"),
-			TenantID:       stringAttrEnvFallback(c.OIDCToken, "ARM_TENANT_ID"),
+			SubscriptionID: stringAttrEnvFallback(c.SubscriptionID, "ARM_SUBSCRIPTION_ID"),
+			TenantID:       stringAttrEnvFallback(c.TenantID, "ARM_TENANT_ID"),
 		},
 		WorkloadIdentityAuthConfig: auth.WorkloadIdentityAuthConfig{
 			UseAKSWorkloadIdentity: c.UseAKS,
