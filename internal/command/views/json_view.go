69ffb949c97991f5e7f32a1b86598b40b8961028
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-07-08 17:27:31 +0300

    Remove the lint diagnostics severity and adjust the logic around lint diagnostics consolidation to work properly with only the lint diagnostic go type.
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/command/views/json_view.go
+++ internal/command/views/json_view.go
@@ -95,8 +95,6 @@ func (v *JSONView) Diagnostics(diags tfdiags.Diagnostics, metadata ...any) {
 		args = append(args, metadata...)
 
 		switch diag.Severity() {
-		case tfdiags.LintingWarning:
-			v.log.Warn(fmt.Sprintf("Linting: %s", diag.Description().Summary), args...)
 		case tfdiags.Warning:
 			v.log.Warn(fmt.Sprintf("Warning: %s", diag.Description().Summary), args...)
 		default:
