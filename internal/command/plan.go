a50f17901dbc8557729f3c96efddbfb77df0e1c9
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-07-10 08:55:28 +0300

    Add the -lint flag to the validate, plan, apply commands help text
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/command/plan.go
+++ internal/command/plan.go
@@ -319,6 +319,14 @@ Other Options:
                                imported with a relative path. When "none" is
                                selected, all the deprecation warnings will be
                                dropped.
+
+  -lint=all                    Configures the linting rules to be executed during
+                               this command. By specifying this flag, the built-in
+                               linting will be enabled, which will start issuing
+                               warning diagnostics if any included rule will be
+                               violated. For more details on the format and
+                               available linting rules, refer to the official
+                               documentation.
 `
 	return strings.TrimSpace(helpText)
 }
