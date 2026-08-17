d021d05ec017123018402840ed3b70313c3fd7ac
Martin Atkins <mart@degeneration.co.uk>
2026-08-17 13:32:04 -0700

    e2etest: replace template-provider fixture with cloudinit-provider
    The hashicorp/template provider is deprecated and has no versions with
    windows_amd64 packages, so we'll switch to using hashicorp/cloudinit as
    our arbitrary placeholder for these tests for whether "tofu init" can
    successfully install a provider from the OpenTofu Registry.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/command/e2etest/init_test.go
+++ internal/command/e2etest/init_test.go
@@ -25,13 +25,13 @@ func TestInitProviders(t *testing.T) {
 	t.Parallel()
 
 	// This test reaches out to registry.opentofu.org to download the
-	// template provider, so it can only run if network access is allowed.
+	// cloudinit provider, so it can only run if network access is allowed.
 	// We intentionally don't try to stub this here, because there's already
 	// a stubbed version of this in the "command" package and so the goal here
 	// is to test the interaction with the real repository.
 	skipIfCannotAccessNetwork(t)
 
-	fixturePath := filepath.Join("testdata", "template-provider")
+	fixturePath := filepath.Join("testdata", "cloudinit-provider")
 	tf := e2e.NewBinary(t, tofuBin, fixturePath)
 
 	stdout, stderr, err := tf.Run("init")
@@ -47,7 +47,7 @@ func TestInitProviders(t *testing.T) {
 		t.Errorf("success message is missing from output:\n%s", stdout)
 	}
 
-	if !strings.Contains(stdout, "- Installing hashicorp/template v") {
+	if !strings.Contains(stdout, "- Installing hashicorp/cloudinit v") {
 		t.Errorf("provider download message is missing from output:\n%s", stdout)
 		t.Logf("(this can happen if you have a copy of the plugin in one of the global plugin search dirs)")
 	}
--- internal/command/e2etest/version_test.go
+++ internal/command/e2etest/version_test.go
@@ -47,11 +47,11 @@ func TestVersionWithProvider(t *testing.T) {
 	t.Parallel()
 
 	// This test reaches out to registry.opentofu.org to download the
-	// template and null providers, so it can only run if network access is
+	// cloudinit provider, so it can only run if network access is
 	// allowed.
 	skipIfCannotAccessNetwork(t)
 
-	fixturePath := filepath.Join("testdata", "template-provider")
+	fixturePath := filepath.Join("testdata", "cloudinit-provider")
 	tf := e2e.NewBinary(t, tofuBin, fixturePath)
 
 	// Initial run (before "init") should work without error but will not
@@ -80,7 +80,7 @@ func TestVersionWithProvider(t *testing.T) {
 	}
 
 	// After running init, we additionally include information about the
-	// selected version of the "template" provider.
+	// selected version of the "cloudinit" provider.
 	{
 		stdout, stderr, err := tf.Run("version")
 		if err != nil {
@@ -91,7 +91,7 @@ func TestVersionWithProvider(t *testing.T) {
 			t.Errorf("unexpected stderr output:\n%s", stderr)
 		}
 
-		wantMsg := "+ provider registry.opentofu.org/hashicorp/template v" // we don't know which version we'll get here
+		wantMsg := "+ provider registry.opentofu.org/hashicorp/cloudinit v" // we don't know which version we'll get here
 		if !strings.Contains(stdout, wantMsg) {
 			t.Errorf("output does not contain provider information %q:\n%s", wantMsg, stdout)
 		}
