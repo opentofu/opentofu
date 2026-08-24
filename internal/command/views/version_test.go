91ea2e34a45ab8fa81b800fda2de8deeb54ebd83
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2026-08-24 14:39:34 +0530

    feat(command): display downloaded module versions in tofu version human and JSON output
    Signed-off-by: Vedant Madane <6527493+VedantMadane@users.noreply.github.com>

--- internal/command/views/version_test.go
+++ internal/command/views/version_test.go
@@ -25,11 +25,27 @@ func TestVersionViews(t *testing.T) {
 			viewCall: func(v Version) {
 				v.PrintVersion("0.1.0", "dev", "darwin_arm64", false, map[string]string{
 					"registry.opentofu.org/test/test": "0.2.0",
+				}, nil)
+			},
+			wantStdout: `OpenTofu v0.1.0-dev
+on darwin_arm64
++ provider registry.opentofu.org/test/test v0.2.0
+`,
+			wantStderr: "",
+		},
+		"human printVersion with module selections": {
+			viewType: arguments.ViewHuman,
+			viewCall: func(v Version) {
+				v.PrintVersion("0.1.0", "dev", "darwin_arm64", false, map[string]string{
+					"registry.opentofu.org/test/test": "0.2.0",
+				}, map[string]string{
+					"myregistry.foo.com/namespace/iam/aws": "1.4.2",
 				})
 			},
 			wantStdout: `OpenTofu v0.1.0-dev
 on darwin_arm64
 + provider registry.opentofu.org/test/test v0.2.0
++ module myregistry.foo.com/namespace/iam/aws v1.4.2
 `,
 			wantStderr: "",
 		},
@@ -38,7 +54,7 @@ on darwin_arm64
 			viewCall: func(v Version) {
 				v.PrintVersion("0.1.0", "dev", "darwin_arm64", true, map[string]string{
 					"registry.opentofu.org/test/test": "0.2.0",
-				})
+				}, nil)
 			},
 			wantStdout: `OpenTofu v0.1.0-dev
 on darwin_arm64
@@ -52,7 +68,7 @@ running in FIPS 140-3 mode (not yet supported)
 			viewCall: func(v Version) {
 				v.PrintVersion("0.1.0", "dev", "darwin_arm64", false, map[string]string{
 					"registry.opentofu.org/test/test": "0.0.0",
-				})
+				}, nil)
 			},
 			wantStdout: `OpenTofu v0.1.0-dev
 on darwin_arm64
@@ -65,7 +81,7 @@ on darwin_arm64
 			viewCall: func(v Version) {
 				v.PrintVersion("0.1.0", "dev", "darwin_arm64", false, map[string]string{
 					"registry.opentofu.org/test/test": "0.2.0",
-				})
+				}, nil)
 			},
 			wantStdout: `{
   "terraform_version": "0.1.0-dev",
@@ -77,12 +93,34 @@ on darwin_arm64
 `,
 			wantStderr: "",
 		},
+		"json printVersion with module selections": {
+			viewType: arguments.ViewJSON,
+			viewCall: func(v Version) {
+				v.PrintVersion("0.1.0", "dev", "darwin_arm64", false, map[string]string{
+					"registry.opentofu.org/test/test": "0.2.0",
+				}, map[string]string{
+					"myregistry.foo.com/namespace/iam/aws": "1.4.2",
+				})
+			},
+			wantStdout: `{
+  "terraform_version": "0.1.0-dev",
+  "platform": "darwin_arm64",
+  "provider_selections": {
+    "registry.opentofu.org/test/test": "0.2.0"
+  },
+  "module_selections": {
+    "myregistry.foo.com/namespace/iam/aws": "1.4.2"
+  }
+}
+`,
+			wantStderr: "",
+		},
 		"json printVersion with fips enabled": {
 			viewType: arguments.ViewJSON,
 			viewCall: func(v Version) {
 				v.PrintVersion("0.1.0", "dev", "darwin_arm64", true, map[string]string{
 					"registry.opentofu.org/test/test": "0.2.0",
-				})
+				}, nil)
 			},
 			wantStdout: `{
   "terraform_version": "0.1.0-dev",
@@ -100,7 +138,7 @@ on darwin_arm64
 			viewCall: func(v Version) {
 				v.PrintVersion("0.1.0", "dev", "darwin_arm64", false, map[string]string{
 					"registry.opentofu.org/test/test": "0.0.0",
-				})
+				}, nil)
 			},
 			wantStdout: `{
   "terraform_version": "0.1.0-dev",
